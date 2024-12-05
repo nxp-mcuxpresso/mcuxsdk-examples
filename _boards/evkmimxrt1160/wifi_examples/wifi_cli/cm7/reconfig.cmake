81df74624e0f923ad3b3c1b039365784b1ff2bc2
Sanjay Yadav <sanjay.yadav@nxp.com>
2024-12-05 19:23:34 +0530

    [MCUX-74316] Fixed missing config files in build of wifi_cli for evkmimxrt1160
    [Description]
    Missing below cmds in wifi_cli app for evkmimxrt1160 board:
    wlan-roaming
    wlan-rssi-low-threshold
    wlan-eu-crypto-rc4
    wlan-cloud-keep-alive
    wlan_tcp_client.
    
    [Root Cause]
    Issue was seen due to wrong config files were used for wifi_cli app on evkmimxrt1160 board.
    
    [Fix]
    Fixed missing config files in build config.
    
    [Testing]
    Dapeng tests passed successfully.
    
    Signed-off-by: Sanjay Yadav <sanjay.yadav@nxp.com>

--- _boards/evkmimxrt1160/wifi_examples/wifi_cli/cm7/reconfig.cmake
+++ _boards/evkmimxrt1160/wifi_examples/wifi_cli/cm7/reconfig.cmake
@@ -6,14 +6,13 @@
 
 mcux_add_source(
     BASE_PATH ${SdkRootDirPath}
-    SOURCES examples/_boards/${board}/wifi_examples/wifi_cli/wifi_config.h
+    SOURCES middleware/wifi_nxp/example/wifi_cli/wifi_config/wifi_config.h
             examples/_boards/${board}/wifi_examples/wifi_cli/lwipopts.h
             examples/_boards/${board}/wifi_examples/common/pin_mux.c
             examples/_boards/${board}/wifi_examples/common/pin_mux.h
-            examples/_boards/${board}/wifi_examples/wifi_cli/lwippools.h
-            examples/_boards/${board}/wifi_examples/wifi_cli/lwiphooks.h
-            examples/_boards/${board}/wifi_examples/wifi_cli/FreeRTOSConfig.h
-
+            middleware/wifi_nxp/example/wifi_cli/lwippools.h
+            middleware/wifi_nxp/example/wifi_cli/lwiphooks.h
+            middleware/wifi_nxp/example/wifi_cli/FreeRTOSConfig.h
             examples/_boards/${board}/wifi_examples/common/${core_id}/hardware_init.c
             examples/_boards/${board}/wifi_examples/common/${core_id}/app.h
             examples/_boards/${board}/sdmmc_config.c
@@ -26,15 +25,10 @@ mcux_add_source(
 
 mcux_add_include(
     BASE_PATH ${SdkRootDirPath}
-    INCLUDES examples/_boards/${board}/wifi_examples/wifi_cli
-             examples/_boards/${board}/wifi_examples/common
+    INCLUDES examples/_boards/${board}/wifi_examples/common
              examples/_boards/${board}/wifi_examples/common/${core_id}
+             middleware/wifi_nxp/example/wifi_cli/wifi_config
              examples/_boards/${board}/wifi_examples/common/app_config
-)
-
-mcux_project_remove_include(
-    BASE_PATH ${SdkRootDirPath}
-    INCLUDES middleware/wifi_nxp/example/wifi_cli
 )
 
 mcux_add_iar_configuration(
