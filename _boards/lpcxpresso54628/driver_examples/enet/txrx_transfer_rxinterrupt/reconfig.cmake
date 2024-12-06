a172bfe73a868d76f0373e2f834b6c0ac0295cc1
Stanislav Poboril <stanislav.poboril@nxp.com>
2024-12-06 17:49:29 +0100

    [MCUX-74323][lpc_enet][mcx_enet] Added missing linker flags in examples.
    Added missing linker flags (e.g. heap and stack size) in in LPC and MCX
    ENET driver examples to match the previous version of SDK.
    
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

--- _boards/lpcxpresso54628/driver_examples/enet/txrx_transfer_rxinterrupt/reconfig.cmake
+++ _boards/lpcxpresso54628/driver_examples/enet/txrx_transfer_rxinterrupt/reconfig.cmake
@@ -1,7 +1,3 @@
-mcux_add_macro(
-    CC "-DENET_PTP1588FEATURE_REQUIRED"
-)
-
 mcux_add_linker_symbol(
     SYMBOLS "__heap_size__=0x3000\
              __stack_size__=0x3000\
--- _boards/lpcxpresso54s018/driver_examples/enet/txrx_rxinterrupt/reconfig.cmake
+++ _boards/lpcxpresso54s018/driver_examples/enet/txrx_rxinterrupt/reconfig.cmake
@@ -1,4 +1,9 @@
-
 mcux_add_macro(
     AS "-DIMG_BAUDRATE=96000000"
+)
+
+mcux_add_linker_symbol(
+    SYMBOLS "__heap_size__=0x3000\
+             __stack_size__=0x3000\
+             __ram_vector_table__=1"
 )
