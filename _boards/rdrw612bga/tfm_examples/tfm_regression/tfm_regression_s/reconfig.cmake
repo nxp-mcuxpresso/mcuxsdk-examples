bf5b518450ceabc1b22a560561fd21bc161e4b88
Waqar Tahir <waqar.tahir@nxp.com>
2024-12-09 14:26:51 +0100

    [IOTDL-1892] Warnings suppressed in tf-m examples coming from third party code
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

--- _boards/rdrw612bga/tfm_examples/tfm_regression/tfm_regression_s/reconfig.cmake
+++ _boards/rdrw612bga/tfm_examples/tfm_regression/tfm_regression_s/reconfig.cmake
@@ -38,13 +38,6 @@ mcux_add_macro(
     TARGETS debug
     CC "-DNDEBUG"
 )
-#mcux_remove_armgcc_configuration(
-#    TARGETS debug
-#    CC "-O0"
-#)
-mcux_add_armgcc_configuration(
-    CC "-Wno-format"
-)
 
 #iar configurations
 mcux_remove_iar_configuration(
