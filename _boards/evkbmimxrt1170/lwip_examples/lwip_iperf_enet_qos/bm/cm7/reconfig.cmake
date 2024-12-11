b3500389e7322ae19b54cf5c00791428136ffb12
Stanislav Poboril <stanislav.poboril@nxp.com>
2024-12-11 17:12:19 +0100

    [MCUX-74181][lwip_examples] Overridden the linker files.
    The lwip_iperf_bm and lwip_iperf_enet_qos_bm examples override
    the used linker files in some build configurations on evkbmimxrt1170.
    The linker files move some frequently executed code to ITCM
    for better performance. This setting was in previous version of SDK
    but was lost for lwip_iperf_bm. Newly, lwip_iperf_enet_qos_bm
    has this setting as well on evkbmimxrt1170.
    
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

--- _boards/evkbmimxrt1170/lwip_examples/lwip_iperf_enet_qos/bm/cm7/reconfig.cmake
+++ _boards/evkbmimxrt1170/lwip_examples/lwip_iperf_enet_qos/bm/cm7/reconfig.cmake
@@ -2,3 +2,39 @@
 mcux_add_macro(
     CC "-DFSL_ETH_ENABLE_CACHE_CONTROL=1"
 )
+
+mcux_add_mdk_configuration(
+    LD "--diag_suppress=L6329W"
+)
+
+mcux_remove_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_sdram_debug flexspi_nor_sdram_release
+    LINKER devices/RT/RT1170/MIMXRT1176/iar/MIMXRT1176xxxxx_cm7_flexspi_nor_sdram.icf
+)
+mcux_remove_mdk_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_sdram_debug flexspi_nor_sdram_release
+    LINKER devices/RT/RT1170/MIMXRT1176/arm/MIMXRT1176xxxxx_cm7_flexspi_nor_sdram.scf
+)
+mcux_remove_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_sdram_debug flexspi_nor_sdram_release
+    LINKER devices/RT/RT1170/MIMXRT1176/gcc/MIMXRT1176xxxxx_cm7_flexspi_nor_sdram.ld
+)
+
+mcux_add_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_sdram_debug flexspi_nor_sdram_release
+    LINKER examples/_boards/${board}/lwip_examples/linker/lwip_tcm/MIMXRT1176xxxxx_cm7_flexspi_nor_sdram_lwiptcm.icf
+)
+mcux_add_mdk_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_sdram_debug flexspi_nor_sdram_release
+    LINKER examples/_boards/${board}/lwip_examples/linker/lwip_tcm/MIMXRT1176xxxxx_cm7_flexspi_nor_sdram_lwiptcm.scf
+)
+mcux_add_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_sdram_debug flexspi_nor_sdram_release
+    LINKER examples/_boards/${board}/lwip_examples/linker/lwip_tcm/MIMXRT1176xxxxx_cm7_flexspi_nor_sdram_lwiptcm.ld
+)
