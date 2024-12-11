a2550f0aa9f15c0f9df52c2ad19ebd2116cdaa7b
Stanislav Poboril <stanislav.poboril@nxp.com>
2024-12-11 17:01:40 +0100

    [MCUX-74181][lwip_examples] Enabled HW offload of protocol checksum validation/generation in selected examples.
    Enabled HW offload of protocol checksum validation and generation in
    lwip_iperf_bm_enet_qos example (on all platforms) and lwip_iperf_bm
    example (on evkbmimxrt1170).
    
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

--- _boards/evkbmimxrt1170/lwip_examples/lwip_iperf/bm/reconfig.cmake
+++ _boards/evkbmimxrt1170/lwip_examples/lwip_iperf/bm/reconfig.cmake
@@ -1,1 +1,19 @@
+mcux_remove_macro(
+    CC "-DCHECKSUM_GEN_UDP=1\
+        -DCHECKSUM_GEN_TCP=1\
+        -DCHECKSUM_GEN_ICMP=1\
+        -DCHECKSUM_CHECK_UDP=1\
+        -DCHECKSUM_CHECK_TCP=1\
+        -DCHECKSUM_CHECK_ICMP=1"
+)
+
+mcux_add_macro(
+    CC "-DCHECKSUM_GEN_UDP=0\
+        -DCHECKSUM_GEN_TCP=0\
+        -DCHECKSUM_GEN_ICMP=0\
+        -DCHECKSUM_CHECK_UDP=0\
+        -DCHECKSUM_CHECK_TCP=0\
+        -DCHECKSUM_CHECK_ICMP=0"
+)
+
 include(${CMAKE_CURRENT_LIST_DIR}/${core_id}/reconfig.cmake OPTIONAL)
