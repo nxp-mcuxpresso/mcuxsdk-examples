9e207ecc1d7d31b8ac92aa3269b76121b068ef51
Mike Li <mike.gang.li@nxp.com>
2024-12-11 20:02:07 +0800

    [MCUX-73751] Make multicore_hello_world@cm33 examples as POR image by default for evkmimxrt1180
    Signed-off-by: Mike Li <mike.gang.li@nxp.com>

--- _boards/evkmimxrt1180/multicore_examples/hello_world/cm33/reconfig.cmake
+++ _boards/evkmimxrt1180/multicore_examples/hello_world/cm33/reconfig.cmake
@@ -3,6 +3,30 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+mcux_remove_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    CC "-DXIP_BOOT_HEADER_ENABLE=0"
+)
+
+mcux_remove_mdk_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    LD "--predefine=\"-DXIP_BOOT_HEADER_ENABLE=0\""
+)
+
+mcux_add_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    CC "-DXIP_BOOT_HEADER_ENABLE=1"
+)
+
+mcux_add_mdk_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    LD "--predefine=\"-DXIP_BOOT_HEADER_ENABLE=1\""
+)
+
 mcux_add_mdk_configuration(
         CC "-DCORE1_IMAGE_COPY_TO_RAM"
         LD "--keep=*(*core1_code)"
