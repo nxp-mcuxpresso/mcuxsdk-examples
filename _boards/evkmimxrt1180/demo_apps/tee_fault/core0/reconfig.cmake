bf53ab2f6474a15cdc7aa277373951db182c85ec
Eric Sun <jian.sun@nxp.com>
2024-12-12 10:54:12 +0800

    [MCUX-74466][RT1180] Fix tee_fault example secondary image core copy problem
    Signed-off-by: Eric Sun <jian.sun@nxp.com>

--- _boards/evkmimxrt1180/demo_apps/tee_fault/core0/reconfig.cmake
+++ _boards/evkmimxrt1180/demo_apps/tee_fault/core0/reconfig.cmake
@@ -1,3 +1,7 @@
+#
+# Copyright 2024 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
 
 mcux_add_source(
     BASE_PATH ${SdkRootDirPath}
@@ -7,9 +11,17 @@ mcux_add_source(
 )
 
 mcux_add_mdk_configuration(
+    CC "-DCORE1_IMAGE_COPY_TO_RAM"
     LD "--keep=*(*core1_code)"
 )
 
+mcux_add_iar_configuration(
+        CC "-DCORE1_IMAGE_COPY_TO_RAM"
+)
+
+mcux_add_armgcc_configuration(
+        CC "-DCORE1_IMAGE_COPY_TO_RAM"
+)
 mcux_add_macro(
     CC "-D__SEMIHOST_HARDFAULT_DISABLE=1"
 )
