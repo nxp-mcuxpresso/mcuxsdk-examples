d9ac6daaba0c102ae29e12df5698c1def9e4b439
Stanislav Poboril <stanislav.poboril@nxp.com>
2024-12-12 17:00:34 +0100

    [MCUX-74381][lwip_ping_freertos] Fixed PHY initialization failure.
    Fixed PHY initialization failure of lwip_ping_freertos example
    on evkmimxrt1020 and evkmimxrt1024 boards by disabling link
    status detection by GPIO interrupt and reverted to polling.
    
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

--- _boards/evkmimxrt1020/lwip_examples/lwip_ping/freertos/reconfig.cmake
+++ _boards/evkmimxrt1020/lwip_examples/lwip_ping/freertos/reconfig.cmake
@@ -1,7 +1,6 @@
 # Add additional configuration
 mcux_add_macro(
-    CC "-DFSL_FEATURE_PHYKSZ8081_USE_RMII50M_MODE\
-        -DETH_LINK_POLLING_INTERVAL_MS=0"
+    CC "-DFSL_FEATURE_PHYKSZ8081_USE_RMII50M_MODE"
 )
 mcux_add_macro(
     TOOLCHAINS iar mdk armgcc
