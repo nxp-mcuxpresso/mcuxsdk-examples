efe244bcb68b230c2ba95652eb72e78e5ee3cb7a
Waqar Tahir <waqar.tahir@nxp.com>
2024-12-12 11:12:03 +0100

    [IOTDL-1899] compiler opt updated for tf-m examples for N10 armgcc-debug mode. Aligned with other toolchains. duplicate, unused files are removed for frdmmcxn947 regression example.
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

--- _boards/frdmmcxn947/tfm_examples/tfm_psatest/tfm_psatest_ns/reconfig.cmake
+++ _boards/frdmmcxn947/tfm_examples/tfm_psatest/tfm_psatest_ns/reconfig.cmake
@@ -4,3 +4,14 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 include(${SdkRootDirPath}/${board_root}/${board}/tfm_examples/reconfig.cmake OPTIONAL)
+
+mcux_remove_armgcc_configuration(
+    TARGETS debug
+    CC "-O0"
+    CX "-O0"
+)
+mcux_add_armgcc_configuration(
+    TARGETS debug
+    CC "-Os"
+    CX "-Os"
+)
