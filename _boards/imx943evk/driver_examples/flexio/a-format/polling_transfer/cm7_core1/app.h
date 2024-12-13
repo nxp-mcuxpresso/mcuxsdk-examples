c41a4cb540b967472a2b1661e37feac6cfa2645f
JianchaoWang <Jianchao.wang_1@nxp.com>
2024-12-13 20:02:35 +0900

    [INDLINUX-4611][i.MX943] Adding the hardware initialization code
    - app.h
    - hardware_init.c
    - pin_mux.c/h
    
    Signed-off-by: JianchaoWang <Jianchao.wang_1@nxp.com>

---
+++ _boards/imx943evk/driver_examples/flexio/a-format/polling_transfer/cm7_core1/app.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2024 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _APP_H_
+#define _APP_H_
+
+#include "board.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define BOARD_FLEXIO_BASE      FLEXIO1
+
+#if BOARD_IMX943_TYPE == BOARD_IMX943_EMULATOR
+#warning "For Emulator"
+#define FLEXIO_A_FORMAT_DR_PIN    9U
+#define FLEXIO_A_FORMAT_TX_PIN    11U
+#define FLEXIO_A_FORMAT_RX_PIN    8U
+#else
+#define FLEXIO_A_FORMAT_DR_PIN    1U
+#define FLEXIO_A_FORMAT_TX_PIN    2U
+#define FLEXIO_A_FORMAT_RX_PIN    3U
+#endif
+
+#define A_FORMAT_TX_SHIFTER_INDEX 0U
+#define A_FORMAT_RX_SHIFTER_INDEX 1U
+#define A_FORMAT_DR_TIMER_INDEX   0U
+#define A_FORMAT_TX_TIMER_INDEX   1U
+#define A_FORMAT_RX_TIMER_INDEX   2U
+#define DEMO_DFS_DIV2_CLOCK       (hal_clock_syspll1dfs0div2)
+#define DEMO_FLEXIO_CLOCK         (hal_clock_flexio1)
+#define FLEXIO_CLOCK_FREQUENCY    HAL_ClockGetRate(DEMO_FLEXIO_CLOCK)
+/*${macro:end}*/
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */
