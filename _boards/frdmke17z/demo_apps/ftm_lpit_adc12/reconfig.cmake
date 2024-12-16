d2a9a9ff460a8371e6c5cf98832766a2a62a1aa3
William Tang <william.tang@nxp.com>
2024-12-16 18:17:09 +0800

    [MCUX-74834][FTM]Fix ftm_lpit_adc12 assert error
     - Example ftm_lpit_adc12 use example specific clock_config.c and
       clock_config.h, so system core clock is not default.
     - Default system core clock is greater than lpit source clock,
       which results assert error.
     - Use clock_customize_folder project segment to add individual
       clock configuration files.
    
    Signed-off-by: William Tang <william.tang@nxp.com>

--- _boards/frdmke17z/demo_apps/ftm_lpit_adc12/reconfig.cmake
+++ _boards/frdmke17z/demo_apps/ftm_lpit_adc12/reconfig.cmake
@@ -3,3 +3,14 @@ mcux_add_macro(
     CC "-DFRDM_KE17Z\
        -DFREEDOM"
 )
+
+mcux_add_source(
+    BASE_PATH ${SdkRootDirPath}
+    SOURCES ${board_root}/${board}/demo_apps/ftm_lpit_adc12/clock_config.c
+    SOURCES ${board_root}/${board}/demo_apps/ftm_lpit_adc12/clock_config.h
+)
+
+mcux_add_include(
+    BASE_PATH ${SdkRootDirPath} 
+    INCLUDES ${board_root}/${board}/demo_apps/ftm_lpit_adc12
+)
