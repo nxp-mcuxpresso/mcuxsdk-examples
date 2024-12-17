cf0c1339d1b4ee503c36d32fb061479a26f8f108
Chay Guo <changyi.guo@nxp.com>
2024-12-17 12:47:16 +0800

    [MCUX-75067][RT700]Generated littlefs_shell/peripherals code using MCUXpresso Config tools.
    - Regenerated code fixing import issue.
    
    Signed-off-by: Chay Guo <changyi.guo@nxp.com>

--- _boards/mimxrt700evk/littlefs_examples/littlefs_shell/cm33_core0/peripherals.h
+++ _boards/mimxrt700evk/littlefs_examples/littlefs_shell/cm33_core0/peripherals.h
@@ -1,6 +1,5 @@
 /*
  * Copyright 2024 NXP
- * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -14,26 +13,15 @@
 #define _PERIPHERALS_H_
 
 /***********************************************************************************************************************
- * User includes
- **********************************************************************************************************************/
-#include "lfs_mflash.h"
-
-
-/***********************************************************************************************************************
  * Included files
  **********************************************************************************************************************/
 #include "fsl_common.h"
 #include "lfs.h"
+#include "lfs_mflash.h"
 
 #if defined(__cplusplus)
 extern "C" {
 #endif /* __cplusplus */
-
-/***********************************************************************************************************************
- * User definitions
- **********************************************************************************************************************/
-#define LITTLEFS_START_ADDR 0x00400000
-
 
 /***********************************************************************************************************************
  * Definitions
@@ -45,10 +33,16 @@ extern "C" {
 #define LITTLEFS_PROG_SIZE 256
 /* Erasable block size definition */
 #define LITTLEFS_BLOCK_SIZE 4096
+/* Block count definition */
+#define LITTLEFS_BLOCK_COUNT 1024
+/* Block cycles definition */
+#define LITTLEFS_BLOCK_CYCLES 100
 /* Minimum block cache size definition */
 #define LITTLEFS_CACHE_SIZE 256
 /* Minimum lookahead buffer size definition */
 #define LITTLEFS_LOOKAHEAD_SIZE 16
+/* Block starting address definition */
+#define LITTLEFS_START_ADDR 0x00400000
 
 /***********************************************************************************************************************
  * Global variables
