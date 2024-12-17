e7ae43ffec6db21ae00d7fdbe6bd3e18537aae24
Andrej Trnkóci <andrej.trnkoci@nxp.com>
2024-12-17 23:25:57 +0100

    [MCUX-74240] Increased heap size also in iar mdk
    The heap size was increased also for iar and mdk builds.
    
    Signed-off-by: Andrej Trnkoci andrej.trnkoci@nxp.com

--- _boards/lpcxpresso55s36/littlefs_examples/littlefs_shell/reconfig.cmake
+++ _boards/lpcxpresso55s36/littlefs_examples/littlefs_shell/reconfig.cmake
@@ -5,7 +5,34 @@ mcux_add_source(
       examples/_boards/${board}/littlefs_examples/littlefs_shell/peripherals.h
 )
 
+mcux_add_mdk_configuration(
+  CC "-mno-unaligned-access"
+)
 
 mcux_add_armgcc_configuration(
     LD "-Xlinker --defsym=__stack_size__=0x1000 -Xlinker --defsym=__heap_size__=0x1000"
+)
+
+mcux_remove_iar_configuration(
+    LD "--config_def=__heap_size__=0x400"
+)
+
+mcux_add_iar_configuration(
+  LD "--config_def=__heap_size__=0x1000"
+)
+
+mcux_add_iar_configuration(
+  LD "--config_def=__stack_size__=0x4000"
+)
+
+mcux_remove_mdk_configuration(
+    LD "--predefine=\"-D__stack_size__=0x1000\" --predefine=\"-D__heap_size__=0x400\""
+)
+mcux_add_mdk_configuration(
+    LD "--predefine=\"-D__stack_size__=0x2000\" --predefine=\"-D__heap_size__=0x2000\""
+)
+
+# Add additional configuration
+mcux_add_mdk_configuration(
+    LD "--diag_suppress=L6329W"
 )
