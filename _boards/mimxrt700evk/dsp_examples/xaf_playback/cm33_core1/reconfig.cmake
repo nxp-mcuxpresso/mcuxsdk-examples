f9368e7774d2e7ace4adccffe65f1af4e531427e
Tomas Barak (nxf33034) <tomas.barak@nxp.com>
2024-12-18 16:40:11 +0100

    [MCUX-75190] Move XAF examples to the internal repository
    - XAF examples will be distributed just in the KEX packages
    
    Signed-off-by: Tomas Barak (nxf33034) <tomas.barak@nxp.com>

--- _boards/mimxrt700evk/dsp_examples/xaf_playback/cm33_core1/reconfig.cmake
+++
@@ -1,4 +0,0 @@
-
-mcux_add_macro(
-    CC "-DXA_SBC_DECODER=1 -DXA_PCM_GAIN=1 -DXA_SRC_PP_FX=1"
-)
--- dsp_examples/xaf_usb_demo/cm/sysbuild.cmake
+++
@@ -1,6 +0,0 @@
-#
-# Copyright 2024 NXP
-#
-# SPDX-License-Identifier: BSD-3-Clause
-
-include(${SdkRootDirPath}/examples/_boards/${board}/dsp_examples/xaf_usb_demo/${core_id}/sysbuild.cmake)
