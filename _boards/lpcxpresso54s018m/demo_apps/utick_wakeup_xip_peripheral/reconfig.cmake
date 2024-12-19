4cbd893d455b605e40030184e1b6c21014be8b76
Peter Wang <chaoyi.wang@nxp.com>
2024-12-19 13:40:57 +0800

    [MCUX-75224][LPC54S018M] add missing XIP_IMAGE link flag for XIP example
    1. the XIP_IMAGE link flag must be defined so that the image could locate into SPIFI flash
    2. MDK, ARMGCC and MCUXpress IDE all missed the XIP_IMAGE link flag for XIP examples
    
    Signed-off-by: Peter Wang <chaoyi.wang@nxp.com>

--- _boards/lpcxpresso54s018m/demo_apps/utick_wakeup_xip_peripheral/reconfig.cmake
+++ _boards/lpcxpresso54s018m/demo_apps/utick_wakeup_xip_peripheral/reconfig.cmake
@@ -8,6 +8,12 @@ mcux_add_source(
 mcux_add_iar_configuration(
     LD "--config_def XIP_IMAGE=1"
 )
+mcux_add_mdk_configuration(
+    LD "--predefine=\"-DXIP_IMAGE=1\""
+)
+mcux_add_armgcc_configuration(
+    LD "-Xlinker --defsym=XIP_IMAGE=1"
+)
 
 mcux_add_macro(
     TOOLCHAINS armgcc iar mdk
