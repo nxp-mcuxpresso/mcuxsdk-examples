578a59a587b226a8d004c2115fc379d1ae5b88b2
Peter Wang <chaoyi.wang@nxp.com>
2024-12-20 12:34:22 +0800

    [MCUX-75247][lpc55s36] update mbedtls example to reserve srama for PKC
    1. the SRAM A(16K) is reserved for PKC, when PKC is used, customer cannot use SRAM A
    2. Any example which use the PKC should reserve the SRAM A
    
    Signed-off-by: Peter Wang <chaoyi.wang@nxp.com>

--- _boards/lpcxpresso55s36/mbedtls_examples/mbedtls_benchmark/reconfig.cmake
+++ _boards/lpcxpresso55s36/mbedtls_examples/mbedtls_benchmark/reconfig.cmake
@@ -7,6 +7,16 @@ mcux_add_armgcc_configuration(
        -fno-strict-aliasing"
 )
 
+mcux_add_iar_configuration(
+    LD "--config_def __pkc__=1"
+)
+mcux_add_mdk_configuration(
+    LD "--predefine=\"-D__pkc__=1\""
+)
+mcux_add_armgcc_configuration(
+    LD "-Xlinker --defsym=__pkc__=1"
+)
+
 mcux_add_macro(
     CC "-DMBEDTLS_CONFIG_FILE=\\\"els_pkc_mbedtls_config.h\\\"\
        -DPRINTF_ADVANCED_ENABLE=1\
