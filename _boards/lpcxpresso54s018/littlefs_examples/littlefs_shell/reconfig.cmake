4cbb9cb7783fc308ff4f892d988090230f530f78
Peter Wang <chaoyi.wang@nxp.com>
2024-12-20 15:44:48 +0800

    [MCUX-75095,MCUX-75098][lpc55s36,mcxn947] extend the stack/heap size for littlefs_shell example
    1. the default 0x400 heap size does not work for lpc55s36,mcxn947,lpc54s018
    
    Signed-off-by: Peter Wang <chaoyi.wang@nxp.com>

--- _boards/lpcxpresso54s018/littlefs_examples/littlefs_shell/reconfig.cmake
+++ _boards/lpcxpresso54s018/littlefs_examples/littlefs_shell/reconfig.cmake
@@ -4,3 +4,27 @@ mcux_add_source(
       examples/_boards/${board}/littlefs_examples/littlefs_shell/peripherals.c
       examples/_boards/${board}/littlefs_examples/littlefs_shell/peripherals.h
 )
+
+mcux_remove_iar_configuration(
+    LD "--config_def=__stack_size__=0x1000 --config_def=__heap_size__=0x400"
+)
+
+mcux_remove_mdk_configuration(
+    LD "--predefine=\"-D__stack_size__=0x1000\" --predefine=\"-D__heap_size__=0x400\""
+)
+
+mcux_remove_armgcc_configuration(
+    LD "-Xlinker --defsym=__stack_size__=0x1000 -Xlinker --defsym=__heap_size__=0x400"
+)
+
+mcux_add_iar_configuration(
+    LD "--config_def=__stack_size__=0x2000 --config_def=__heap_size__=0x1000"
+)
+
+mcux_add_mdk_configuration(
+    LD "--predefine=\"-D__stack_size__=0x2000\" --predefine=\"-D__heap_size__=0x1000\""
+)
+
+mcux_add_armgcc_configuration(
+    LD "-Xlinker --defsym=__stack_size__=0x1000 -Xlinker --defsym=__heap_size__=0x1000"
+)
