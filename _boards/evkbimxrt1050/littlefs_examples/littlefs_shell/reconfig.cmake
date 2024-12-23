e42046218b0cfc3b91aaed45f46b3543dc4f6544
Andrej Trnkóci <andrej.trnkoci@nxp.com>
2024-12-23 01:46:28 +0100

    [MCUX-74387] littlefs_shell heap size increased
    The heap size was increased for littlefs_shell example on evkbimxrt1050
    board to fix mounting and formating (the app would run out of memory
    when executing the commands)
    
    Signed-off-by: Andrej Trnkóci <andrej.trnkoci@nxp.com>

--- _boards/evkbimxrt1050/littlefs_examples/littlefs_shell/reconfig.cmake
+++ _boards/evkbimxrt1050/littlefs_examples/littlefs_shell/reconfig.cmake
@@ -6,8 +6,24 @@ mcux_add_source(
             examples/_boards/${board}/littlefs_examples/littlefs_shell/peripherals.h
 )
 
+mcux_add_armgcc_configuration(
+    LD "-Xlinker --defsym=__stack_size__=0x1000 -Xlinker --defsym=__heap_size__=0x1000"
+)
 
+mcux_remove_iar_configuration(
+    LD "--config_def=__heap_size__=0x400"
+)
 
+mcux_add_iar_configuration(
+  LD "--config_def=__heap_size__=0x1000"
+)
+
+mcux_remove_mdk_configuration(
+    LD "--predefine=\"-D__stack_size__=0x1000\" --predefine=\"-D__heap_size__=0x400\""
+)
+mcux_add_mdk_configuration(
+    LD "--predefine=\"-D__stack_size__=0x1000\" --predefine=\"-D__heap_size__=0x1000\""
+)
 
 # Add additional configuration
 mcux_add_macro(
