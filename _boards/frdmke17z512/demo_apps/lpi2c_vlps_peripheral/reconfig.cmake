91c1eec6603968e61c5dcce8f48f9ddd56d14f8b
Chay Guo <changyi.guo@nxp.com>
2024-12-24 15:39:33 +0800

    [MCUX-75167][KE1xZ]Fixed lpi2c_vlps_peripheral demo
    - The demo used customized clock_config.c. Corrected the clock_config files used for the demo.
    
    Signed-off-by: Chay Guo <changyi.guo@nxp.com>

--- _boards/frdmke17z512/demo_apps/lpi2c_vlps_peripheral/reconfig.cmake
+++ _boards/frdmke17z512/demo_apps/lpi2c_vlps_peripheral/reconfig.cmake
@@ -1,8 +1,13 @@
+# Copyright 2024 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
 
 mcux_add_source(
     BASE_PATH ${SdkRootDirPath}
     SOURCES ${board_root}/${board}/demo_apps/lpi2c_vlps_peripheral/peripherals.c
             ${board_root}/${board}/demo_apps/lpi2c_vlps_peripheral/peripherals.h
+            ${board_root}/${board}/demo_apps/lpi2c_vlps_peripheral/clock_config.c
+            ${board_root}/${board}/demo_apps/lpi2c_vlps_peripheral/clock_config.h
 )
 
 mcux_add_macro(
