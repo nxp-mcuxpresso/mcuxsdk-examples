29411c580b51ca2f3f3621993126113417bd2571
JianchaoWang <Jianchao.wang_1@nxp.com>
2025-01-03 18:26:02 +0900

    [INDLINUX-4612][T-format] Updating the T-format example in the polling mode
    Added two test demos:
    - Getting the encoder ID
    - Getting the encoder temperature
    
    Signed-off-by: JianchaoWang <Jianchao.wang_1@nxp.com>

--- driver_examples/flexio/t-format/polling_transfer/flexio_t-format_polling_transfer.c
+++ driver_examples/flexio/t-format/polling_transfer/flexio_t-format_polling_transfer.c
@@ -27,12 +27,12 @@
  ******************************************************************************/
 encoder_T_format encoder;
 status_t status;
-static bool cmdFlag = false;
 
 /*******************************************************************************
  * Code
  ******************************************************************************/
 #if (MODE == MODE_INT)
+static bool cmdFlag = false;
 void FLEXIO_T_Format_UserCallback(FLEXIO_T_FORMAT_Type *base,
        	                          flexio_t_format_handle_t *handle,
                                   status_t status, void *userData)
@@ -64,7 +64,7 @@ int main(void)
     FLEXIO_T_FORMAT_Type encDev;
     encoder_all_info_t enc_abs, abs_save;
     uint8_t enc_id;
-    int8_t temp, temp_save;
+    int8_t temp = 0, temp_save = 0;
     uint64_t time = 0;
 
     BOARD_InitHardware();
@@ -121,20 +121,29 @@ int main(void)
 
 #if (MODE == MODE_POLL)
     status = T_Format_Get_Encoder_ID(&encoder, &enc_id);
-    if (status != kStatus_Success)
+    if (status == kStatus_FLEXIO_T_FORMAT_FrameErr)
     {
-    }/**/
+        PRINTF("[T-format] The frame of getting encoder ID is error!\r\n");
+    }
+    else
+    {
+        PRINTF("[T-format] Encoder ID: 0x%02X\r\n\t%s\r\n", enc_id, T_Format_GetStatusFlag(status));
+    }
+
     status = T_Format_Readout_ABS_ABM(&encoder, &enc_abs);
-    if (status != kStatus_Success)
+    if (status == kStatus_FLEXIO_T_FORMAT_FrameErr)
     {
-//        DiagnosisInfoOutput(status);
-//        return status;
+        PRINTF("[T-format] The frame of getting ABS and ABM is error!\r\n");
+    }
+    else
+    {
+        PRINTF("[T-format] Multi-turn data: %d, single-turn data: %d\r\n\t%s\r\n", enc_abs.multiTurn, enc_abs.singleTurn, T_Format_GetStatusFlag(status));
     }
     status = T_Format_Get_Temperature(&encoder, &temp);
-    if (status != kStatus_Success)
+    if (status == kStatus_Success)
     {
-    }/**/
-    PRINTF("[0.000s] Encoder ID: %d\r\n\t Multi-turn data: %d, single-turn data: %ld\r\n\t Temperature: %f\r\n", enc_id, enc_abs.multiTurn, enc_abs.singleTurn, temp);
+        PRINTF("[T-format] Temperature: %d\r\n", temp);
+    }
 
     abs_save  = enc_abs;
     temp_save = temp;
@@ -143,12 +152,16 @@ int main(void)
         /* Delay at least 90 ms. */
         SDK_DelayAtLeastUs(90000, SDK_DEVICE_MAXIMUM_CPU_CLOCK_FREQUENCY);
         time++;
-	T_Format_Readout_ABS_ABM(&encoder, &enc_abs);
+	status = T_Format_Readout_ABS_ABM(&encoder, &enc_abs);
         SDK_DelayAtLeastUs(10000, SDK_DEVICE_MAXIMUM_CPU_CLOCK_FREQUENCY);
+        if (status == kStatus_FLEXIO_T_FORMAT_FrameErr)
+        {
+            PRINTF("[%.2fs] The frame of getting ABS and ABM is error!\r\n", time/10.0);
+        }
 	T_Format_Get_Temperature(&encoder, &temp);
-        if ((abs((int)enc_abs.multiTurn - (int)abs_save.multiTurn) > 10) || //(fabs(enc_abs.singleTurn - abs_save.singleTurn) > 10) ||
+        if (/*(abs((int)enc_abs.multiTurn - (int)abs_save.multiTurn) > 10) || */(abs(enc_abs.singleTurn - abs_save.singleTurn) > 500) ||
             (abs(temp - temp_save) > 1)) {
-            PRINTF("[%.2fs] Encoder ID: 0x%X\n\r\t Multi-turn data: %d, single-turn data: %ld\r\n\t Temperature: %f\r\n", time/10.0, enc_id, enc_abs.multiTurn, enc_abs.singleTurn, temp);
+            PRINTF("[%.2fs] Encoder ID: 0x%X\n\r\t Multi-turn data: %d, single-turn data: %d\r\n\t Temperature: %d\r\n", time/10.0, enc_id, enc_abs.multiTurn, enc_abs.singleTurn, temp);
             abs_save = enc_abs;
             temp_save = temp;
         }
