cd100cb2eda014c9e6618aa1cca786082b81748b
Biwen <biwen.li@nxp.com>
2025-01-07 09:35:54 +0900

    [MCUX-64655][flexcan] change can instance to can1
    Signed-off-by: Biwen <biwen.li@nxp.com>

--- _boards/imx943evk/driver_examples/flexcan/loopback/cm33_core1/app.h
+++ _boards/imx943evk/driver_examples/flexcan/loopback/cm33_core1/app.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 NXP
+ * Copyright 2025 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -13,13 +13,13 @@
  * Definitions
  ******************************************************************************/
 /*${macro:start}*/
-#define EXAMPLE_CAN                CAN3
-#define EXAMPLE_FLEXCAN_IRQn       CAN3_IRQn
-#define EXAMPLE_FLEXCAN_IRQHandler CAN3_IRQHandler
+#define EXAMPLE_CAN                CAN1
+#define EXAMPLE_FLEXCAN_IRQn       CAN1_IRQn
+#define EXAMPLE_FLEXCAN_IRQHandler CAN1_IRQHandler
 #define RX_MESSAGE_BUFFER_NUM      (1)
 #define TX_MESSAGE_BUFFER_NUM      (0)
 
-#define FLEXCAN_CLOCK_ROOT   (hal_clock_can3)
+#define FLEXCAN_CLOCK_ROOT   (hal_clock_can1)
 #define EXAMPLE_CAN_CLK_FREQ (HAL_ClockGetRate(FLEXCAN_CLOCK_ROOT))
 /* Set USE_IMPROVED_TIMING_CONFIG macro to use api to calculates the improved CAN / CAN FD timing values. */
 #define USE_IMPROVED_TIMING_CONFIG (1U)
--- _boards/imx943evk/driver_examples/flexcan/loopback_transfer/cm33_core1/hardware_init.c
+++ _boards/imx943evk/driver_examples/flexcan/loopback_transfer/cm33_core1/hardware_init.c
@@ -6,13 +6,13 @@
 /*${header:start}*/
 #include "app.h"
 #include "board.h"
+#include "pin_mux.h"
 #include "sm_platform.h"
 /*${header:end}*/
 
 /*${function:start}*/
 void BOARD_InitHardware(void)
 {
-    pca6416a_handle_t handle;
     /* clang-format off */
     hal_clk_t hal_flexcanClkCfg = {
         .clk_id = FLEXCAN_CLOCK_ROOT,
@@ -20,14 +20,8 @@ void BOARD_InitHardware(void)
         .clk_round_opt = hal_clk_round_auto,
         .rate = 24000000UL,
     };
-    hal_clk_t hal_lpi2cClkCfg = {
-        .clk_id = BOARD_PCA6416A_I2C_CLOCK_ROOT,
-        .pclk_id = hal_clock_osc24m,
-        .clk_round_opt = hal_clk_round_auto,
-        .rate = 24000000UL,
-    };
-
     /* clang-format on */
+
     SM_Platform_Init();
     BOARD_InitDebugConsolePins();
     BOARD_InitBootPins();
@@ -36,20 +30,18 @@ void BOARD_InitHardware(void)
 
     HAL_ClockSetRate(&hal_flexcanClkCfg);
     HAL_ClockEnable(&hal_flexcanClkCfg);
-    HAL_ClockSetRate(&hal_lpi2cClkCfg);
-    HAL_ClockEnable(&hal_lpi2cClkCfg);
 
-    BOARD_InitPCA6416A(&handle);
-
-    PCA6416A_SetDirection(&handle, (1 << BOARD_PCA6416A_CAN3_EN), kPCA6416A_Output);
-    PCA6416A_SetDirection(&handle, (1 << BOARD_PCA6416A_CAN3_STBY_B), kPCA6416A_Output);
+    BOARD_EXPANDER_SetPinAsOutput(BOARD_PCA6416_I2C6_S1_ID, CAN1_ENABLE);
+    BOARD_EXPANDER_SetPinAsOutput(BOARD_PCA6416_I2C6_S1_ID, CAN1_STBY_B);
 
     /* Initialize TJA1463AT with normal mode */
     /* Clear CAN STBY_N and EN signal of TJA1463AT */
-    PCA6416A_ClearPins(&handle, (1 << BOARD_PCA6416A_CAN3_STBY_B) | (1 << BOARD_PCA6416A_CAN3_EN));
+    BOARD_EXPANDER_SetPinToLow(BOARD_PCA6416_I2C6_S1_ID, CAN1_ENABLE);
+    BOARD_EXPANDER_SetPinToLow(BOARD_PCA6416_I2C6_S1_ID, CAN1_STBY_B);
     SDK_DelayAtLeastUs(100U, SystemCoreClock);
 
     /* Setup CAN EN and STBY_N signal of TJA1436AT */
-    PCA6416A_SetPins(&handle, (1 << BOARD_PCA6416A_CAN3_STBY_B) | (1 << BOARD_PCA6416A_CAN3_EN));
+    BOARD_EXPANDER_SetPinToHigh(BOARD_PCA6416_I2C6_S1_ID, CAN1_ENABLE);
+    BOARD_EXPANDER_SetPinToHigh(BOARD_PCA6416_I2C6_S1_ID, CAN1_STBY_B);
 }
 /*${function:end}*/
