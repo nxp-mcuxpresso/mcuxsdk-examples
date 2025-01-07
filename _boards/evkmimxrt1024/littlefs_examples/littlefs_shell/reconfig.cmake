2a2d43435dbf18542494965e43ac55b9caeae6f2
Andrej Trnkóci <andrej.trnkoci@nxp.com>
2025-01-07 11:08:26 +0100

    [MCUX-75110] littlefs_shell rt1024_evk armgcc fix
    For evkmimxrt1024 the armgcc debug/release target (ram) the txt region
    was placed to ocram, because the itcm region is not big enough. For
    sdram_debug/sdram_release the example was disabled (at least for now).
    It seems there might be some linker file issue for sdram.
    
    Signed-off-by: Andrej Trnkóci <andrej.trnkoci@nxp.com>

--- _boards/evkmimxrt1024/littlefs_examples/littlefs_shell/reconfig.cmake
+++ _boards/evkmimxrt1024/littlefs_examples/littlefs_shell/reconfig.cmake
@@ -22,18 +22,15 @@ mcux_add_mdk_configuration(
     LD "--diag_suppress=L6329W"
 )
 
-# Add or remove Linker File Configurations
-mcux_remove_armgcc_linker_script(
-        BASE_PATH ${SdkRootDirPath}
-        LINKER devices/RT/RT1020/MIMXRT1024/gcc/MIMXRT1024xxxxx_flexspi_nor.ld
-)
 
 mcux_remove_armgcc_linker_script(
-        BASE_PATH ${SdkRootDirPath}
-        LINKER devices/RT/RT1020/MIMXRT1024/gcc/MIMXRT1024xxxxx_ram.ld
+    TARGETS release debug
+    BASE_PATH ${SdkRootDirPath}
+    LINKER devices/RT/RT1020/MIMXRT1024/gcc/MIMXRT1024xxxxx_ram.ld
 )
 
 mcux_add_armgcc_linker_script(
+    TARGETS release debug
     BASE_PATH ${SdkRootDirPath}
-    LINKER examples/_boards/${board}/littlefs_examples/littlefs_shell/linker/MIMXRT1021xxxxx_flexspi_nor.ld
+    LINKER examples/_boards/${board}/littlefs_examples/littlefs_shell/linker/MIMXRT1024xxxxx_ram_txt_in_ocram.ld
 )
