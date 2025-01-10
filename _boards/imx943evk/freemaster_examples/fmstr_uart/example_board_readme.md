b85e0afec98ed710250bd3e152c045145b86f3ce
Tomas Kianica <tomas.kianica@nxp.com>
2025-01-10 13:45:41 +0100

    [MCUX-75696][imx943evk][FreeMASTER] Added UART example
    Signed-off-by: Tomas Kianica <tomas.kianica@nxp.com>

---
+++ _boards/imx943evk/freemaster_examples/fmstr_uart/example_board_readme.md
@@ -0,0 +1,34 @@
+Hardware requirements
+=====================
+- Micro USB cable
+- Target microcontroller board
+- J-Link Debug Probe
+- 12V~20V power supply
+- Personal Computer
+- FreeMASTER 2.5 or later installed on PC
+
+Board settings
+============
+Connect 12V~20V power supply and J-Link Debug Probe to the board, switch SW301 to power on the board.
+By default this demo communicates over a virtual serial line interface of the debugger USB connection. 
+With a proper RS232 transceiver and pin configuration, it may also connect to microcontroller UART peripheral.
+
+Prepare the demo
+===============
+1.  Connect a USB cable between the host PC and the J1401 Debug USB port on the target board.
+2.  Compile and download the program to the target microcontroller.
+3.  Run and resume application execution when debugger stops in the main() function.
+
+Connect with FreeMASTER
+=======================
+4.  Run FreeMASTER, use the Connection Wizard or open Project Options.
+5.  Select serial communication, choose correct COMx port and connect at speed 115200 bps.
+6.  Start communication, FreeMASTER loads the initial TSA Active Content links in the Welcome page.
+7.  Click the "FreeMASTER Demonstration Project (embedded in device)" in the Welcome page.
+8.  The demo is now running, you should be able to watch variable values and graphs.
+
+More information
+================
+Read more information about FreeMASTER tool at http://www.nxp.com/freemaster.
+Feel free to ask questions and report issues at FreeMASTER's 
+community page at https://community.nxp.com/community/freemaster
---
+++ _boards/imx943evk/freemaster_examples/fmstr_uart/main.c
@@ -0,0 +1,143 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * FreeMASTER Communication Driver - Example Application
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+
+
+#include "pin_mux.h"
+#include "fsl_lpuart.h"
+#include "fsl_common.h"
+#include "sm_platform.h"
+#include "board.h"
+
+#include "freemaster.h"
+#include "freemaster_serial_lpuart.h"
+
+#include "freemaster_example.h"
+
+////////////////////////////////////////////////////////////////////////////////
+// Local macros define
+////////////////////////////////////////////////////////////////////////////////
+
+#define _FMSTR_LPUART_BASE_ADDR(x)   LPUART##x
+#define _FMSTR_LPUART_IRQn(x)        LPUART##x##_IRQn
+#define _FMSTR_LPUART_IRQ_HANDLER(x) LPUART##x##_IRQHandler
+
+#define FMSTR_LPUART_BASE_ADDR(x)    _FMSTR_LPUART_BASE_ADDR(x)
+#define FMSTR_LPUART_IRQn(x)         _FMSTR_LPUART_IRQn(x)
+#define FMSTR_LPUART_IRQ_HANDLER(x)  _FMSTR_LPUART_IRQ_HANDLER(x)
+
+////////////////////////////////////////////////////////////////////////////////
+// Variables
+////////////////////////////////////////////////////////////////////////////////
+
+//! Note: All global variables accessed by FreeMASTER are defined in a shared
+//! freemaster_example.c file
+
+////////////////////////////////////////////////////////////////////////////////
+// Prototypes
+////////////////////////////////////////////////////////////////////////////////
+
+static void init_freemaster_lpuart(void);
+
+////////////////////////////////////////////////////////////////////////////////
+// Code
+////////////////////////////////////////////////////////////////////////////////
+
+int main(void)
+{
+    /* Board initialization */
+    SM_Platform_Init();
+    BOARD_InitDebugConsolePins();
+    BOARD_InitBootPins();
+    BOARD_BootClockRUN();
+
+    /* FreeMASTER communication layer initialization */
+    init_freemaster_lpuart();
+
+    /* This example uses shared code from FreeMASTER generic example application */
+    FMSTR_Example_Init();
+
+    while (1)
+    {
+        /* FreeMASTER example increments several variables periodically,
+           use the FreeMASTER PC Host tool to visualize the variables */
+        FMSTR_Example_Poll();
+    }
+}
+
+/*!
+ * @brief LPUART Module initialization (LPUART is a the standard block included e.g. in K66F)
+ */
+static void init_freemaster_lpuart(void)
+{
+    lpuart_config_t config;
+
+    /* clang-format off */
+    hal_clk_t hal_clk = {
+        .clk_id = hal_clock_invalid,
+        .clk_round_opt = hal_clk_round_auto,
+        .rate = 24000000UL,
+    };
+    /* clang-format on */
+
+    hal_clk.clk_id = (hal_clk_id_e)BOARD_GetUartClkId(BOARD_DEBUG_CONSOLE_PORT);
+
+    HAL_ClockSetRate(&hal_clk);
+    HAL_ClockEnable(&hal_clk);
+
+    /*
+     * config.baudRate_Bps = 115200U;
+     * config.parityMode = kUART_ParityDisabled;
+     * config.stopBitCount = kUART_OneStopBit;
+     * config.txFifoWatermark = 0;
+     * config.rxFifoWatermark = 1;
+     * config.enableTx = false;
+     * config.enableRx = false;
+     */
+    LPUART_GetDefaultConfig(&config);
+    config.baudRate_Bps = 115200U;
+    config.enableTx     = false;
+    config.enableRx     = false;
+
+    LPUART_Init(FMSTR_LPUART_BASE_ADDR(BOARD_DEBUG_UART_INSTANCE), &config, HAL_ClockGetRate(hal_clk.clk_id));
+
+    /* Register communication module used by FreeMASTER driver. */
+    FMSTR_SerialSetBaseAddress(FMSTR_LPUART_BASE_ADDR(BOARD_DEBUG_UART_INSTANCE));
+
+#if FMSTR_SHORT_INTR || FMSTR_LONG_INTR
+    /* Enable UART interrupts. */
+    EnableIRQ(FMSTR_LPUART_IRQn(BOARD_DEBUG_UART_INSTANCE));
+    EnableGlobalIRQ(0);
+#endif
+}
+
+#if FMSTR_SHORT_INTR || FMSTR_LONG_INTR
+/*
+ *   Application interrupt handler of communication peripheral used in interrupt modes
+ *   of FreeMASTER communication.
+ *
+ *   NXP MCUXpresso SDK framework defines interrupt vector table as a part of "startup_XXXXXX.x"
+ *   assembler/C file. The table points to weakly defined symbols, which may be overwritten by the
+ *   application specific implementation. FreeMASTER overrides the original weak definition and
+ *   redirects the call to its own handler.
+ *
+ */
+
+void FMSTR_LPUART_IRQ_HANDLER(BOARD_DEBUG_UART_INSTANCE)(void)
+{
+    /* Call FreeMASTER Interrupt routine handler */
+    FMSTR_SerialIsr();
+}
+#endif
+
+////////////////////////////////////////////////////////////////////////////////
+// EOF
+/////////////////////////////////////////////////////////////////////////////////
