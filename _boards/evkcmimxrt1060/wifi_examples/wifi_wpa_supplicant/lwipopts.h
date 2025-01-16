5c1632ca1d14774602c5d8cbbf0f7bc600238fca
Manav Chotalia <manavashwinbhai.chotalia@nxp.com>
2025-01-16 18:12:37 +0530

    Issue: WSW-54849 Unable to transmit Multicast Tx data from DUTAP
    [Description]
    Unable to transmit Multicast Tx data from DUTAP.
    
    [Root Cause]
    For Tx multicast case routing failed as lwip_hook_ip4_route_src not enabled.
    
    [Fix]
    Updated config for routing from lwip_hook_ip4_route_src
    
    [Testing]
    Validated UDP-TX for mentioned category HT20,HT40 etc.
    
    [Open Issues/Todo]
    None
    
    [Reviewers]
    Sandip Dalvi, Supriya Inamdar, Abhinav Kulkarni, keval Thakker
    
    Change-Id: 57611234-d3eb-11ef-b1fe-af9977d05d9c
    
    Signed-off-by: Manav Chotalia manavashwinbhai.chotalia@nxp.com

--- _boards/evkcmimxrt1060/wifi_examples/wifi_wpa_supplicant/lwipopts.h
+++ _boards/evkcmimxrt1060/wifi_examples/wifi_wpa_supplicant/lwipopts.h
@@ -528,7 +528,12 @@ u32_t lwip_rand(void);
 
 #ifndef LWIP_HOOK_FILENAME
 #define LWIP_HOOK_FILENAME                               "lwiphooks.h"
+#endif
+
+#if CONFIG_CLOUD_KEEP_ALIVE
 #define LWIP_HOOK_TCP_OUT_ADD_TCPOPTS(p, hdr, pcb, opts) lwip_hook_tcp_out_add_tcpopts(p, hdr, pcb, opts)
 #endif
+
+#define LWIP_HOOK_IP4_ROUTE_SRC(src, dest) lwip_hook_ip4_route_src(src, dest)
 
 #endif /* __LWIPOPTS_H__ */
