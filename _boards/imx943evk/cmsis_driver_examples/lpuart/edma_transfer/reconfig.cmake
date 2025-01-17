b7bdf7170ed3263b2e7623071b195df74e83b4ec
Qiang Zhao <qiang.zhao@nxp.com>
2025-01-17 07:49:39 +0530

    [MCUX-64655][imx943evk][cmsis_lpuart] fix compile issue
    The path is not correct, correct the path
    
    Signed-off-by: Qiang Zhao <qiang.zhao@nxp.com>

--- _boards/imx943evk/cmsis_driver_examples/lpuart/edma_transfer/reconfig.cmake
+++ _boards/imx943evk/cmsis_driver_examples/lpuart/edma_transfer/reconfig.cmake
@@ -2,11 +2,11 @@
 
 mcux_add_source(
     BASE_PATH ${SdkRootDirPath}
-    SOURCES examples/${board}/cmsis_driver_examples/lpuart/edma_transfer/RTE_Device.h
+    SOURCES ${board_root}/${board}/cmsis_driver_examples/lpuart/edma_transfer/RTE_Device.h
 )
 
 
 mcux_add_include(
     BASE_PATH ${SdkRootDirPath}
-    INCLUDES examples/${board}/cmsis_driver_examples/lpuart/edma_transfer
+    INCLUDES ${board_root}/${board}/cmsis_driver_examples/lpuart/edma_transfer
 )
--- _boards/imx943evk/cmsis_driver_examples/lpuart/interrupt_transfer/reconfig.cmake
+++ _boards/imx943evk/cmsis_driver_examples/lpuart/interrupt_transfer/reconfig.cmake
@@ -2,11 +2,11 @@
 
 mcux_add_source(
     BASE_PATH ${SdkRootDirPath}
-    SOURCES examples/${board}/cmsis_driver_examples/lpuart/interrupt_transfer/RTE_Device.h
+    SOURCES ${board_root}/${board}/cmsis_driver_examples/lpuart/interrupt_transfer/RTE_Device.h
 )
 
 
 mcux_add_include(
     BASE_PATH ${SdkRootDirPath}
-    INCLUDES examples/${board}/cmsis_driver_examples/lpuart/interrupt_transfer
+    INCLUDES ${board_root}/${board}/cmsis_driver_examples/lpuart/interrupt_transfer
 )
