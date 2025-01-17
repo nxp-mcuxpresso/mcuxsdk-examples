2fc5dc85848f5b3c6f63c1692501353f427e9fdf
JianchaoWang <Jianchao.wang_1@nxp.com>
2025-01-17 18:05:53 +0800

    [INDLINUX-4611][A-format] Adding the example in the interrupt mode for i.MX943
    Signed-off-by: JianchaoWang <Jianchao.wang_1@nxp.com>

---
+++ _boards/imx943evk/driver_examples/flexio/a-format/interrupt_transfer/cm33_core1/hardware_init.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*${header:start}*/
+#include "app.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+#include "sm_platform.h"
+/*${header:end}*/
+
+/*${function:start}*/
+void BOARD_InitHardware(void)
+{
+    /* clang-format off */
+    hal_clk_t hal_flexioClkCfg = {
+        .clk_id = DEMO_FLEXIO_CLOCK,
+        .pclk_id = hal_clock_syspll1dfs1div2, /* 400 MHz */
+        .clk_round_opt = hal_clk_round_auto,
+        .rate = 80000000UL, /* 80 MHz */
+    };
+    /* clang-format on */
+
+    SM_Platform_Init();
+    BOARD_InitDebugConsolePins();
+    BOARD_InitBootPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+
+    HAL_ClockSetParent(&hal_flexioClkCfg);
+    HAL_ClockSetRate(&hal_flexioClkCfg);
+    HAL_ClockEnable(&hal_flexioClkCfg);
+}
+/*${function:end}*/
