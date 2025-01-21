1fb094e9dd838a0b4b17b823b1da5602a19bcbff
Ruijia Wang <ruijia.wang@nxp.com>
2025-01-21 14:52:55 +0800

    [MCUX-75128] Reduced I3C controller baud rate
    [Status]
    - The Tx data can't be sent out from FIFO with unknown reason in high
      baud rate. The optimization of code also has effect on this. Reduced
      buad rate first to bypass this.
    
    Signed-off-by: Ruijia Wang <ruijia.wang@nxp.com>

--- _boards/lpcxpresso860max/component_examples/i3c_bus/master/app.h
+++ _boards/lpcxpresso860max/component_examples/i3c_bus/master/app.h
@@ -12,8 +12,9 @@
  ******************************************************************************/
 /*${macro:start}*/
 #define EXAMPLE_MASTER           I3C0
-#define EXAMPLE_I3C_OD_BAUDRATE  1000000U
-#define EXAMPLE_I3C_PP_BAUDRATE  2000000U
+#define EXAMPLE_I2C_BAUDRATE     100000
+#define EXAMPLE_I3C_OD_BAUDRATE  375000
+#define EXAMPLE_I3C_PP_BAUDRATE  750000
 #define I3C_CLOCK_FREQUENCY      CLOCK_GetI3cClkFreq()
 #define I3C_SLOW_CLOCK_FREQUENCY CLOCK_GetLpOscFreq()
 #define FSL_FEATURE_I3C_HAS_IBI_PAYLOAD_SIZE_OPTIONAL_BYTE (1)
