ea8e85c1ef013a7031d1971523543dbeb2860aa6
Biwen <biwen.li@nxp.com>
2025-01-24 23:34:50 +0900

    [MCUX-76066][imx943evk] fix a bug(Duplicated id in the manifest) that failed to generate zip package
    - Fix a bug that failed to generate zip package
    
    Signed-off-by: Biwen <biwen.li@nxp.com>

--- _boards/imx943evk/multicore_examples/rpmsg_lite_pingpong_rtos_no_mcmgr/reconfig.cmake
+++ _boards/imx943evk/multicore_examples/rpmsg_lite_pingpong_rtos_no_mcmgr/reconfig.cmake
@@ -1,8 +1,14 @@
 #
-# Copyright 2024 NXP
+# Copyright 2025 NXP
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
 mcux_add_include(
     INCLUDES .
+)
+
+mcux_add_source(
+    BASE_PATH ${SdkRootDirPath}
+    SOURCES
+            ${board_root}/${board}/multicore_examples/rpmsg_lite_pingpong_rtos_no_mcmgr/rpmsg_config.h
 )
