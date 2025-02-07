249a0ab8f129c4d19c2ff05bb281faa8352de822
Biwen <biwen.li@nxp.com>
2025-02-07 12:46:23 +0900

    [MCUX-76086][imx943evk] fix lwip build issue of zip pacakge(lwip_netc_port.h: No such file)
    - Fix lwip build issue of zip pacakge(lwip_netc_port.h: No such file)
    
    Signed-off-by: Biwen <biwen.li@nxp.com>

--- _boards/imx943evk/lwip_examples/lwip_ping/freertos/reconfig.cmake
+++ _boards/imx943evk/lwip_examples/lwip_ping/freertos/reconfig.cmake
@@ -1,5 +1,15 @@
-# Add additional configuration
 mcux_add_macro(
-    CC "-DUSE_RTOS=1\
-       -DPRINTF_ADVANCED_ENABLE=1"
+    CC "-DNETC_PORT_USE_INCLUDES=1"
 )
+
+mcux_add_source(
+    BASE_PATH ${SdkRootDirPath}
+    SOURCES ${board_root}/${board}/lwip_examples/lwip_netc_port.h
+)
+
+mcux_add_include(
+    BASE_PATH ${SdkRootDirPath}
+    INCLUDES ${board_root}/${board}/lwip_examples
+)
+
+
