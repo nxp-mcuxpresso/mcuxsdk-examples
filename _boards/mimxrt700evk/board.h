1d7b69573fc59df896b1254dcfaecb1a8273cc1d
Chay Guo <changyi.guo@nxp.com>
2025-02-18 15:34:43 +0800

    [MCUX-74717][example]Updated sai_interrut example to call codec I2c release if provided.
    - Call BOARD_Codec_I2C_ReleaseBus before operating on the Codec I2C in case it's hang.
    
    Signed-off-by: Chay Guo <changyi.guo@nxp.com>

--- _boards/mimxrt700evk/board.h
+++ _boards/mimxrt700evk/board.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023-2024 NXP
+ * Copyright 2023-2025 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -310,6 +310,10 @@ void BOARD_I2c2RecoverBus(void);
         BOARD_RestoreI2c##n##PinMux(); \
     } while (0);
 
+/*!
+ * @brief Release Codec I2C bus. This macro is used to release the onboard codec I2C.
+ */
+#define BOARD_Codec_I2C_ReleaseBus() BOARD_I2C_ReleaseBus(2)
 #endif
 
 #if defined(SDK_I2C_BASED_COMPONENT_USED) && SDK_I2C_BASED_COMPONENT_USED
