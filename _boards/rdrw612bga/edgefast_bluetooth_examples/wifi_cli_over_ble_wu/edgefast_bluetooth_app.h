c647cc6227db46c181f2059dd1ebfea47695b545
Jiawei Yang <jiawei.yang_1@nxp.com>
2025-02-18 16:36:39 +0800

    [MCUX-76325][edgefast]Add components for wifi_cli_over_ble_wu on RW61X
    Since the app_config.h is no longer supported, the relevant content has
    been moved to wifi_bt_module_config.h.
    Now, wifi_config.h has to include wifi_bt_module_config.h which needs to
    be loaded through a specific component, otherwise a build error will
    occur.
    
    Enable CONFIG_MCUX_COMPONENT_component.wifi_bt_module.config for
    wifi_cli_over_ble_wu on RW61X to load wifi_bt_module_config.h into
    project.
    
    Signed-off-by: Jiawei Yang <jiawei.yang_1@nxp.com>

--- _boards/rdrw612bga/edgefast_bluetooth_examples/wifi_cli_over_ble_wu/edgefast_bluetooth_app.h
+++ _boards/rdrw612bga/edgefast_bluetooth_examples/wifi_cli_over_ble_wu/edgefast_bluetooth_app.h
@@ -24,18 +24,6 @@
 #define BOARD_USER_BUTTON_IRQ  GPIO_INTA_IRQn
 #define BOARD_USER_BUTTON_NAME "SW2"
 
-#ifndef WIFI_BOARD_RW610
-#define WIFI_BOARD_RW610
-#endif
-
-#if defined(WIFI_BOARD_RW610)
-#define RW610
-#define WIFI_BT_USE_IMU_INTERFACE
-#define WIFI_BT_TX_PWR_LIMITS "wlan_txpwrlimit_cfg_WW_rw610.h"
-#else
-#error "Please define macro for RW610 board"
-#endif
-
 #include "wifi_config.h"
 /* Due to the changes in wifi configuration,
    please do not use '#undef' to diasble wifi uart task. */
