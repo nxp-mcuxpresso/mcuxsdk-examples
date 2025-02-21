7f9b74623d105842c20260f9f76f4dc2228014a5
Patrik Nemeth (nxf98608) <patrik.nemeth@nxp.com>
2025-02-21 15:05:51 +0100

    [MCUX-76577][mbedtls3x_examples] Fix file organization and inclusion
    in the psa_crypto_opaque_key_examples example.
    This also fixes build failures for newly added MCXN and RDRW612.
    
    Signed-off-by: Patrik Nemeth

--- _boards/evkmimxrt1180/mbedtls3x_examples/psa_crypto_opaque_key_examples/cm33/reconfig.cmake
+++ _boards/evkmimxrt1180/mbedtls3x_examples/psa_crypto_opaque_key_examples/cm33/reconfig.cmake
@@ -1,11 +1,10 @@
 
 mcux_add_source(
     BASE_PATH ${SdkRootDirPath}
-    SOURCES examples/mbedtls3x_examples/psa_crypto_opaque_key_examples/ffconf.h
-            examples/mbedtls3x_examples/psa_crypto_opaque_key_examples/mcux_mbedtls_psa_crypto_opaque_config.h
-            ${board_root}/${board}/sdmmc_config.c
+    SOURCES ${board_root}/${board}/sdmmc_config.c
             ${board_root}/${board}/sdmmc_config.h
             ${board_root}/${board}/mbedtls3x_examples/psa_crypto_opaque_key_examples/mbedtls_user_config.h
+            ${board_root}/${board}/mbedtls3x_examples/psa_crypto_opaque_key_examples/ffconf.h
 )
 
 mcux_add_include(
