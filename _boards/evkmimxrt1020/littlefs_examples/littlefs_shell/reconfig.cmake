54c1539c39fce9f6d103ade2f6bf560c8e7dd3d0
David Jurajda <david.jurajda@nxp.com>
2025-02-21 09:47:47 +0100

    [MCUX-75143] Remove specific linker file from littlefs_shell on evkmimxrt1020. 	- consolidate example to use default linker files only
    Signed-off-by: David Jurajda <david.jurajda@nxp.com>

--- _boards/evkmimxrt1020/littlefs_examples/littlefs_shell/reconfig.cmake
+++ _boards/evkmimxrt1020/littlefs_examples/littlefs_shell/reconfig.cmake
@@ -7,7 +7,7 @@ mcux_add_source(
 mcux_add_macro(
     CC "-DLFS_NO_INTRINSICS=1\
        -DSDK_DEBUGCONSOLE_UART\
-	   -DSKIP_SYSCLK_INIT\
+       -DSKIP_SYSCLK_INIT\
        -DXIP_BOOT_HEADER_DCD_ENABLE=1"
 )
 
@@ -19,22 +19,4 @@ mcux_add_macro(
 
 mcux_add_mdk_configuration(
     LD "--diag_suppress=L6329W"
-)
-
-mcux_remove_armgcc_linker_script(
-		TARGETS debug release sdram_debug sdram_release
-        BASE_PATH ${SdkRootDirPath}
-        LINKER ${device_root}/RT/RT1020/MIMXRT1021/gcc/MIMXRT1021xxxxx_flexspi_nor.ld
-)
-
-mcux_remove_armgcc_linker_script(
-		TARGETS debug release sdram_debug sdram_release
-        BASE_PATH ${SdkRootDirPath}
-        LINKER ${device_root}/RT/RT1020/MIMXRT1021/gcc/MIMXRT1021xxxxx_ram.ld
-)
-
-mcux_add_armgcc_linker_script(
-    TARGETS debug release sdram_debug sdram_release
-    BASE_PATH ${SdkRootDirPath}
-    LINKER examples/_boards/${board}/littlefs_examples/littlefs_shell/linker/MIMXRT1021xxxxx_sdram.ld
 )
