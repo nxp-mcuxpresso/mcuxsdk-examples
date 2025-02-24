61b8fac5c5b5b62663b6c4b8e07fa02950c12356
Michal Princ <michal.princ@nxp.com>
2025-02-24 15:05:03 +0100

    [MCUX-76612] Update BOARD_SW_IRQ macro for frdmk32l3a6/freertos_tickless/cm0plus example to avoid armgcc -Warray-bounds build warning
    - INTMUX1 is used for PORTA_IRQ handling
    
    Signed-off-by: Michal Princ <michal.princ@nxp.com>

--- _boards/frdmk32l3a6/freertos_examples/freertos_tickless/cm0plus/app.h
+++ _boards/frdmk32l3a6/freertos_examples/freertos_tickless/cm0plus/app.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 NXP
+ * Copyright 2017,2025 NXP
  * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -14,7 +14,7 @@
 #define BOARD_SW_GPIO        BOARD_SW2_GPIO
 #define BOARD_SW_PORT        BOARD_SW2_PORT
 #define BOARD_SW_GPIO_PIN    BOARD_SW2_GPIO_PIN
-#define BOARD_SW_IRQ         BOARD_SW2_IRQ
+#define BOARD_SW_IRQ         INTMUX1_0_IRQn
 #define BOARD_SW_IRQ_HANDLER BOARD_SW2_IRQ_HANDLER
 #define BOARD_SW_NAME        BOARD_SW2_NAME
 /*${macro:end}*/
