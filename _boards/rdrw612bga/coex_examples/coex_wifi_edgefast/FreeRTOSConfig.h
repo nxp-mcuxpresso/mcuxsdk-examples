1e7b1e91b30e300e62671ba998c9c7c952529c1c
author you <author.you@nxp.com>
2025-02-24 07:57:50 +0100

    [WSW-55932] [coex_edgefast] Fox multiple definition error of 'ucHeap'
    Disable configAPPLICATION_ALLOCATED_HEAP and CONFIG_FSL_OSA_ALLOCATED_HEAP
    
    Signed-off-by: author you <author.you@nxp.com>

--- _boards/rdrw612bga/coex_examples/coex_wifi_edgefast/FreeRTOSConfig.h
+++ _boards/rdrw612bga/coex_examples/coex_wifi_edgefast/FreeRTOSConfig.h
@@ -78,7 +78,7 @@
 #define configSUPPORT_STATIC_ALLOCATION         1
 #define configSUPPORT_DYNAMIC_ALLOCATION        1
 #define configTOTAL_HEAP_SIZE                   ((size_t)(300 * 1024))
-#define configAPPLICATION_ALLOCATED_HEAP        1
+#define configAPPLICATION_ALLOCATED_HEAP        0
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                     1
