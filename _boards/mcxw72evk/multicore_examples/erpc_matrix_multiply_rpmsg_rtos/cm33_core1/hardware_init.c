ec450561e8cd97f988709348b75648a6a330d467
Michal Princ <michal.princ@nxp.com>
2025-02-25 16:59:29 +0100

    [RPSDK-1352] Set correct SystemCoreClock for all kw47/mcxw72 secondary core example projects
    Signed-off-by: Michal Princ <michal.princ@nxp.com>

--- _boards/mcxw72evk/multicore_examples/erpc_matrix_multiply_rpmsg_rtos/cm33_core1/hardware_init.c
+++ _boards/mcxw72evk/multicore_examples/erpc_matrix_multiply_rpmsg_rtos/cm33_core1/hardware_init.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 NXP
+ * Copyright 2024-2025 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -11,11 +11,16 @@
 #include "mcmgr.h"
 /*${header:end}*/
 
+static const uint32_t froPostDivFreq[] = {
+    16000000U, 24000000U, 32000000U, 48000000U, 64000000U
+};
 /*${function:start}*/
 void BOARD_InitHardware(void)
 {
     BOARD_InitPins();
-    BOARD_InitBootClocks();
+
+    uint32_t froPostDivSel = (FRO192M0->FROCCSR & FRO192M_FROCCSR_POSTDIV_SEL_MASK) >> FRO192M_FROCCSR_POSTDIV_SEL_SHIFT;
+    SystemCoreClock = froPostDivFreq[froPostDivSel];
 }
 
 /*!
--- _boards/mcxw72evk/multicore_examples/hello_world/cm33_core1/hardware_init.c
+++ _boards/mcxw72evk/multicore_examples/hello_world/cm33_core1/hardware_init.c
@@ -1,21 +1,25 @@
 /*
- * Copyright 2024 NXP
+ * Copyright 2024-2025 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
 /*${header:start}*/
 #include "pin_mux.h"
-#include "clock_config.h"
 #include "board.h"
 #include "mcmgr.h"
 /*${header:end}*/
 
+static const uint32_t froPostDivFreq[] = {
+    16000000U, 24000000U, 32000000U, 48000000U, 64000000U
+};
 /*${function:start}*/
 void BOARD_InitHardware(void)
 {
     BOARD_InitPins();
-    BOARD_InitBootClocks();
+
+    uint32_t froPostDivSel = (FRO192M0->FROCCSR & FRO192M_FROCCSR_POSTDIV_SEL_MASK) >> FRO192M_FROCCSR_POSTDIV_SEL_SHIFT;
+    SystemCoreClock = froPostDivFreq[froPostDivSel];
 }
 
 /*!
