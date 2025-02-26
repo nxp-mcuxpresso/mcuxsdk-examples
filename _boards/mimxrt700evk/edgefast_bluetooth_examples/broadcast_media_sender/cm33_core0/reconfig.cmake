7bd7f21b66a54188f686b8108040e834077a8e8c
Chang An <chang.an_1@nxp.com>
2025-02-26 14:43:26 +0800

    [MCUX-76631][LE Audio] Put hifi_lc3 demo into examples_int.
    Move hifi_lc3 into examples_int.
    
    Signed-off-by: Chang An <chang.an_1@nxp.com>

--- _boards/mimxrt700evk/edgefast_bluetooth_examples/broadcast_media_sender/cm33_core0/reconfig.cmake
+++ _boards/mimxrt700evk/edgefast_bluetooth_examples/broadcast_media_sender/cm33_core0/reconfig.cmake
@@ -127,7 +127,7 @@ mcux_add_source(
 )
 
 mcux_add_source(
-    BASE_PATH ${SdkRootDirPath}/examples/_boards/${board}/edgefast_bluetooth_examples/hifi_lc3/hifi4/binary/
+    BASE_PATH ${SdkRootDirPath}/examples/_boards/${board}/edgefast_bluetooth_examples/broadcast_media_sender/hifi4/binary/
     TARGETS flash_debug flash_release
     SOURCES dsp_literal_release.bin
             dsp_text_release.bin
@@ -135,17 +135,17 @@ mcux_add_source(
 )
 
 mcux_add_include(
-    BASE_PATH ${SdkRootDirPath}/examples/_boards/${board}/edgefast_bluetooth_examples/hifi_lc3/hifi4/binary/
+    BASE_PATH ${SdkRootDirPath}/examples/_boards/${board}/edgefast_bluetooth_examples/broadcast_media_sender/hifi4/binary/
     INCLUDES ./
 )
 
 mcux_add_iar_configuration(
     TARGETS flash_debug flash_release
-    LD "--image_input=${SdkRootDirPath}/examples/_boards/${board}/edgefast_bluetooth_examples/hifi_lc3/hifi4/binary/dsp_literal_release.bin,__dsp_literal_bin,__dsp_literal_section,4\
+    LD "--image_input=${SdkRootDirPath}/examples/_boards/${board}/edgefast_bluetooth_examples/broadcast_media_sender/hifi4/binary/dsp_literal_release.bin,__dsp_literal_bin,__dsp_literal_section,4\
         --keep=__dsp_literal_bin\
-        --image_input=${SdkRootDirPath}/examples/_boards/${board}/edgefast_bluetooth_examples/hifi_lc3/hifi4/binary/dsp_text_release.bin,__dsp_text_bin,__dsp_text_section,4\
+        --image_input=${SdkRootDirPath}/examples/_boards/${board}/edgefast_bluetooth_examples/broadcast_media_sender/hifi4/binary/dsp_text_release.bin,__dsp_text_bin,__dsp_text_section,4\
         --keep=__dsp_text_bin\
-        --image_input=${SdkRootDirPath}/examples/_boards/${board}/edgefast_bluetooth_examples/hifi_lc3/hifi4/binary/dsp_data_release.bin,__dsp_data_bin,__dsp_data_section,4\
+        --image_input=${SdkRootDirPath}/examples/_boards/${board}/edgefast_bluetooth_examples/broadcast_media_sender/hifi4/binary/dsp_data_release.bin,__dsp_data_bin,__dsp_data_section,4\
         --keep=__dsp_data_bin"
 )
 
