fd521c8a558a29b6b7576502542ba4dd0388476b
author you <author.you@nxp.com>
2025-02-26 03:09:21 +0100

    [MCUX-76595][ncp]Update Edgefast include file due to Kconfig changes
    Reduce the number of generated header files to simplify the use at the app layer.
    
    Signed-off-by: author you <author.you@nxp.com>

--- ncp_examples/ncp_device/ble/ble_config.h
+++ ncp_examples/ncp_device/ble/ble_config.h
@@ -1,13 +1,11 @@
 /*
- *  Copyright 2024 NXP
+ *  Copyright 2024-2025 NXP
  *  All rights reserved.
  *
  *  SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include "edgefast_bluetooth_config_Gen.h"
-#include "edgefast_bluetooth_debug_config_Gen.h"
-#include "edgefast_bluetooth_extension_config_Gen.h"
+#include "edgefast_bluetooth_config.h"
 
 /* Task priority */
 #if (CONFIG_NCP_BLE)
