1dbe24c43bf890d78d0b5a010f100cb7e8fd9023
Michal Chvatal (nxf51207) <michal.chvatal@nxp.com>
2025-02-28 20:50:35 +0100

    [MCUX-76558] Increase volume in audio_demo_bm on RT595
    Signed-off-by: Michal Chvatal (nxf51207) <michal.chvatal@nxp.com>

--- _boards/evkmimxrt595/dsp_examples/audio_demo_bm/cm33/hardware_init.c
+++ _boards/evkmimxrt595/dsp_examples/audio_demo_bm/cm33/hardware_init.c
@@ -83,7 +83,7 @@ int BOARD_CODEC_Init(void)
     }
 
     /* Initial volume kept low for hearing safety. */
-    if (CODEC_SetVolume(&g_codecHandle, kCODEC_PlayChannelHeadphoneLeft | kCODEC_PlayChannelHeadphoneRight, 0x0020) !=
+    if (CODEC_SetVolume(&g_codecHandle, kCODEC_PlayChannelHeadphoneLeft | kCODEC_PlayChannelHeadphoneRight, 0x0050) !=
         kStatus_Success)
     {
         return -1;
