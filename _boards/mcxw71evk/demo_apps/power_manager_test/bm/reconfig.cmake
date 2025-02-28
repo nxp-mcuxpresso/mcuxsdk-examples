c602311dfea49192018cc1fd399e5e3bd12d9dd7
Allen Zhang <chunfeng.zhang@nxp.com>
2025-02-28 17:36:46 +0800

    [MCUX-73369] Added the missing power_manager_test example for MCX-W71-EVK
    Signed-off-by: AllenZhang <chunfeng.zhang@nxp.com>

---
+++ _boards/mcxw71evk/demo_apps/power_manager_test/bm/reconfig.cmake
@@ -0,0 +1,32 @@
+
+mcux_add_macro(
+    CC "-DGENERIC_LIST_LIGHT=1\
+       -DFSL_PM_SUPPORT_ALWAYS_ON_SECTION=1"
+)
+
+mcux_add_armgcc_configuration(
+    CC "-DAPP_BYPASS_ECC_INIT=1"
+)
+
+mcux_remove_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/MCX/MCXW/MCXW716C/iar/mcxw716_flash.icf
+)
+mcux_remove_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/MCX/MCXW/MCXW716C/gcc/mcxw716_flash.ld
+)
+
+
+mcux_add_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/MCX/MCXW/MCXW716C/iar/mcxw716_flash_rom_boot.icf
+)
+mcux_add_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/MCX/MCXW/MCXW716C/gcc/mcxw716_flash_rom_boot.ld
+)
