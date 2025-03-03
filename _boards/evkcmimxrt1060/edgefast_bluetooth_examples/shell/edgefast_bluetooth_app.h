337f76af8589e9fe82e9e996c0ea97b2f5de884f
Jiawei Yang <jiawei.yang_1@nxp.com>
2025-03-03 15:09:30 +0800

    [MCUX-76790]Fix destroyed dependency when mixing two build system
    Mixing Kconfig and app.h causes the dependency between configs to be
    destroyed.
    
    When remove BT_CLASSIC, remove other configs that depend on it
    simultaneously.
    
    Signed-off-by: Jiawei Yang <jiawei.yang_1@nxp.com>

--- _boards/evkcmimxrt1060/edgefast_bluetooth_examples/shell/edgefast_bluetooth_app.h
+++ _boards/evkcmimxrt1060/edgefast_bluetooth_examples/shell/edgefast_bluetooth_app.h
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2023 - 2024 NXP
+ *  Copyright 2023 - 2025 NXP
  *  All rights reserved.
  *
  *  SPDX-License-Identifier: BSD-3-Clause
@@ -18,10 +18,32 @@
 
 #ifdef WIFI_IW610_BOARD_MURATA_2LL_M2
 #undef CONFIG_BT_CLASSIC
-#undef CONFIG_BT_HFP_HF
 #undef CONFIG_BT_IND_RESET
 #undef CONFIG_BT_IND_RESET_OOB
-#endif
+#endif /* #ifdef WIFI_IW610_BOARD_MURATA_2LL_M2 */
+
+#ifndef CONFIG_BT_CLASSIC
+#undef CONFIG_BT_A2DP
+#undef CONFIG_BT_A2DP_CP_SERVICE
+#undef CONFIG_BT_A2DP_DR_SERVICE
+#undef CONFIG_BT_A2DP_SINK
+#undef CONFIG_BT_A2DP_SOURCE
+#undef CONFIG_BT_AVRCP
+#undef CONFIG_BT_AVRCP_BROWSING
+#undef CONFIG_BT_AVRCP_COVER_ART
+#undef CONFIG_BT_AVRCP_COVER_ART_INITIATOR
+#undef CONFIG_BT_AVRCP_COVER_ART_RESPONDER
+#undef CONFIG_BT_AVRCP_CT
+#undef CONFIG_BT_AVRCP_TG
+#undef CONFIG_BT_HFP_HF
+#undef CONFIG_BT_MAP
+#undef CONFIG_BT_MAP_MCE
+#undef CONFIG_BT_MAP_MSE
+#undef CONFIG_BT_PBAP
+#undef CONFIG_BT_PBAP_PCE
+#undef CONFIG_BT_PBAP_PSE
+#undef CONFIG_BT_RFCOMM
+#endif /* #ifndef CONFIG_BT_CLASSIC */
 
 /* To enable the BT IND Reset. */
 /* If ID Reset is enabled, use BT UART to download controller firmware. */
