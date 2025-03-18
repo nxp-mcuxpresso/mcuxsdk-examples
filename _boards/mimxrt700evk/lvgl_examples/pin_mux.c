a4ea743b26ec83baa04175ac13e3d91bbb057700
Chay Guo <changyi.guo@nxp.com>
2025-03-18 10:47:53 +0800

    [MCUX-77462][RT700]Fix signal ID in pin_mux.c for GPIO and SAI.
    Signed-off-by: Chay Guo <changyi.guo@nxp.com>

--- _boards/mimxrt700evk/lvgl_examples/pin_mux.c
+++ _boards/mimxrt700evk/lvgl_examples/pin_mux.c
@@ -45,10 +45,10 @@ void BOARD_InitBootPins(void)
 BOARD_InitMipiPanelPins:
 - options: {callFromInitBoot: 'false', coreID: cm33_core0, enableClock: 'true'}
 - pin_list:
-  - {pin_num: K6, peripheral: GPIO3, signal: 'IO, 4', pin_signal: PIO3_4}
-  - {pin_num: K7, peripheral: GPIO3, signal: 'IO, 5', pin_signal: PIO3_5, input_buffer: enable}
-  - {pin_num: T2, peripheral: GPIO1, signal: 'IO, 10', pin_signal: PIO1_10}
-  - {pin_num: P3, peripheral: GPIO1, signal: 'IO, 14', pin_signal: PIO1_14}
+  - {pin_num: K6, peripheral: GPIO3, signal: 'GPIO, 4', pin_signal: PIO3_4}
+  - {pin_num: K7, peripheral: GPIO3, signal: 'GPIO, 5', pin_signal: PIO3_5, input_buffer: enable}
+  - {pin_num: T2, peripheral: GPIO1, signal: 'GPIO, 10', pin_signal: PIO1_10}
+  - {pin_num: P3, peripheral: GPIO1, signal: 'GPIO, 14', pin_signal: PIO1_14}
  * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS ***********
  */
 /* clang-format on */
@@ -226,9 +226,9 @@ BOARD_InitFlexIOPanelPins:
   - {pin_num: F3, peripheral: FLEXIO, signal: 'IO, 11', pin_signal: PIO2_11, slew_rate: slow, input_buffer: enable, pull_enable: enable}
   - {pin_num: E3, peripheral: FLEXIO, signal: 'IO, 12', pin_signal: PIO2_12, slew_rate: slow, input_buffer: enable, pull_enable: enable}
   - {pin_num: E4, peripheral: FLEXIO, signal: 'IO, 13', pin_signal: PIO2_13, slew_rate: slow, input_buffer: enable, pull_enable: enable}
-  - {pin_num: H2, peripheral: GPIO2, signal: 'IO, 0', pin_signal: PIO2_0, direction: OUTPUT, gpio_direction: out}
-  - {pin_num: G2, peripheral: GPIO2, signal: 'IO, 1', pin_signal: PIO2_1, direction: OUTPUT, gpio_direction: out}
-  - {pin_num: D3, peripheral: GPIO2, signal: 'IO, 15', pin_signal: PIO2_15, direction: OUTPUT, gpio_direction: out}
+  - {pin_num: H2, peripheral: GPIO2, signal: 'GPIO, 0', pin_signal: PIO2_0, direction: OUTPUT, gpio_direction: out}
+  - {pin_num: G2, peripheral: GPIO2, signal: 'GPIO, 1', pin_signal: PIO2_1, direction: OUTPUT, gpio_direction: out}
+  - {pin_num: D3, peripheral: GPIO2, signal: 'GPIO, 15', pin_signal: PIO2_15, direction: OUTPUT, gpio_direction: out}
   - {pin_num: G1, peripheral: FLEXIO, signal: 'IO, 3', pin_signal: PIO2_3, input_buffer: enable}
   - {pin_num: G3, peripheral: FLEXIO, signal: 'IO, 4', pin_signal: PIO2_4, slew_rate: slow, input_buffer: enable, pull_select: up, pull_enable: enable}
  * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS ***********
