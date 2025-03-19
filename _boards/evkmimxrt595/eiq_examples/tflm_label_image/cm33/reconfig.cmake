7f4f7337ae75e68b7a03cf705b5f9ffb84971085
Xiaodong Xie <xiaodong.xie@nxp.com>
2025-03-19 13:33:35 +0800

    [AIR-12782] tflm_examples: update hyperram config for rt1180
      - Update RT1180 hyperram config
      - Correct model path for RT500/RT600/RT1180 tflm_kws example
    
    Signed-off-by: Xiaodong Xie <xiaodong.xie@nxp.com>

--- _boards/evkmimxrt595/eiq_examples/tflm_label_image/cm33/reconfig.cmake
+++ _boards/evkmimxrt595/eiq_examples/tflm_label_image/cm33/reconfig.cmake
@@ -1,11 +1,11 @@
 mcux_add_include(
   BASE_PATH ${SdkRootDirPath}
-  INCLUDES examples/eiq_examples/tflm_cifar10/pcq/
+  INCLUDES examples/eiq_examples/tflm_label_image/pcq/
   )
 mcux_add_source(
   BASE_PATH ${SdkRootDirPath}
-  SOURCES examples/eiq_examples/tflm_cifar10/pcq/model_data.h
-  examples/eiq_examples/tflm_cifar10/pcq/model_cifarnet_ops_micro.cpp
+  SOURCES examples/eiq_examples/tflm_label_image/pcq/model_data.h
+  examples/eiq_examples/tflm_label_image/pcq/model_mobilenet_ops_micro.cpp
   )
 mcux_remove_mdk_configuration(
   CC "-fshort-wchar"
