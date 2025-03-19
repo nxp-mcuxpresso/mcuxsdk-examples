caebd06fb7d93e7d63657b4b95d74d788fcd22a0
Xiaodong Xie <xiaodong.xie@nxp.com>
2025-03-19 13:33:35 +0800

    [AIR-12782] tflm_examples: update hyperram config for rt1180
      - Update RT1180 hyperram config
      - Correct model path for RT500/RT600/RT1180 tflm_kws example
    
    Signed-off-by: Xiaodong Xie <xiaodong.xie@nxp.com>

--- _boards/evkmimxrt1180/eiq_examples/tflm_cifar10/cm33/reconfig.cmake
+++ _boards/evkmimxrt1180/eiq_examples/tflm_cifar10/cm33/reconfig.cmake
@@ -11,4 +11,38 @@ mcux_remove_mdk_configuration(
   CC "-fshort-wchar"
   CX "-fshort-wchar"
   )
+mcux_add_iar_configuration(
+  LD "--config_def=__heap_size__=0x8000\
+  --config_def=__stack_size__=0x2000"
+  )
+mcux_add_mdk_configuration(
+  LD "--predefine=-D__heap_size__=0x8000\
+  --predefine=-D__stack_size__=0x2000"
+  )
+mcux_add_armgcc_configuration(
+  LD "-Xlinker --defsym=__heap_size__=0x8000\
+  -Xlinker --defsym=__stack_size__=0x2000"
+  )
+mcux_remove_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    CC "-DXIP_BOOT_HEADER_ENABLE=0"
+)
 
+mcux_remove_mdk_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    LD "--predefine=\"-DXIP_BOOT_HEADER_ENABLE=0\""
+)
+
+mcux_add_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    CC "-DXIP_BOOT_HEADER_ENABLE=1"
+)
+
+mcux_add_mdk_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    LD "--predefine=\"-DXIP_BOOT_HEADER_ENABLE=1\""
+)
