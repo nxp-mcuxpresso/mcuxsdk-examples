488b61a52bfbb580cbd76e66cf1675edcc882a57
Allen Zhang <chunfeng.zhang@nxp.com>
2025-03-26 10:24:46 +0800

    [MCUX-77682] Updated the linker file for eeprom_emulation example
    - Shall use the RAM linker file to run the example
    
    Signed-off-by: AllenZhang <chunfeng.zhang@nxp.com>

--- _boards/mcxw71evk/driver_examples/eeprom_emulation/reconfig.cmake
+++ _boards/mcxw71evk/driver_examples/eeprom_emulation/reconfig.cmake
@@ -14,3 +14,26 @@ mcux_remove_armgcc_configuration(
     TARGETS debug
     CC "-O0"
 )
+
+mcux_remove_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/MCX/MCXW/MCXW716C/iar/mcxw716_flash.icf
+)
+mcux_remove_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/MCX/MCXW/MCXW716C/gcc/mcxw716_flash.ld
+)
+
+
+mcux_add_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/MCX/MCXW/MCXW716C/iar/mcxw716_ram.icf
+)
+mcux_add_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/MCX/MCXW/MCXW716C/gcc/mcxw716_ram.ld
+)
