3303de10cfa538cb18e2d20a5799fddd01774eb6
Kate Wang <yumeng.wang@nxp.com>
2025-03-28 16:14:20 +0800

    MCUX-75283 [trdc] [RT1180] Fix wrong multocore configuration and update readme
    Signed-off-by: Kate Wang <yumeng.wang@nxp.com>

--- _boards/evkmimxrt1180/driver_examples/trdc/cm33/reconfig.cmake
+++ _boards/evkmimxrt1180/driver_examples/trdc/cm33/reconfig.cmake
@@ -1,3 +1,22 @@
+mcux_remove_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+    CC "-DXIP_BOOT_HEADER_ENABLE=0"
+)
+
+mcux_remove_mdk_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+    LD "--predefine=\"-DXIP_BOOT_HEADER_ENABLE=0\""
+)
+
+mcux_add_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+    CC "-DXIP_BOOT_HEADER_ENABLE=1"
+)
+
+mcux_add_mdk_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+    LD "--predefine=\"-DXIP_BOOT_HEADER_ENABLE=1\""
+)
 
 mcux_add_include(
     BASE_PATH ${SdkRootDirPath}
--- _boards/evkmimxrt1180/driver_examples/trdc/example_board_readme.md
+++ _boards/evkmimxrt1180/driver_examples/trdc/example_board_readme.md
@@ -17,8 +17,9 @@ Prepare the Demo
     - No parity
     - One stop bit
     - No flow control
-3.  Compile the CM7 then the CM33 project to get the binary trdc_cm33.bin.
-4.  Follow the instruction of SPSDK usage in SDK, to download and run the multicore image combined with edgelock FW.
+3.  Compile the CM7 then the CM33 project to get the executable image.
+4.  Follow the instruction of Bootable image generation section in getting started guide,
+to build and write the image using SPT tool.
 
 Running the demo
 ================
