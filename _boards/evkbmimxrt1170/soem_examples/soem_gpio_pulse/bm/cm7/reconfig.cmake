efe2d3e0788ba1639a67dd366dea356f40ab6bdf
Yves Wang <zhengjia.wang@nxp.com>
2025-04-10 19:10:09 +0900

    MCUX-78278 Fixed invalid cmake syntax which blocks export_app
    Signed-off-by: Yves Wang <zhengjia.wang@nxp.com>

--- _boards/evkbmimxrt1170/soem_examples/soem_gpio_pulse/bm/cm7/reconfig.cmake
+++ _boards/evkbmimxrt1170/soem_examples/soem_gpio_pulse/bm/cm7/reconfig.cmake
@@ -1,15 +1,3 @@
-mcux_add_source(
-    BASE_PATH ${SdkRootDirPath}
-    SOURCES examples/soem_examples/soem_gpio_pulse/bm/soem_gpio_pulse.c
-)
-
-
-mcux_add_include(
-    BASE_PATH ${SdkRootDirPath}
-    INCLUDES examples/soem_examples/soem_gpio_pulse/bm
-)
-
-
 
 
 # Add additional configuration
