90eabbd05b87a7b2a25dbc1e5dbea5bd318f8413
Biwen <biwen.li@nxp.com>
2025-04-23 19:36:39 +0900

    [MCUX-76523][i.MX943] simplify code and correct temperature by formula
    - Correct temperature by formula
    
    Signed-off-by: Biwen <biwen.li@nxp.com>

--- demo_apps/temperature_measurement_1/temperature_measurement.c
+++ demo_apps/temperature_measurement_1/temperature_measurement.c
@@ -1,6 +1,5 @@
 /*
- * Copyright 2024 NXP
- * All rights reserved.
+ * Copyright 2024-2025 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -18,6 +17,25 @@
 #define NUM_SENSOR_DESCS_SHIFT (0U)
 #define NUM_SENSOR_DESCS(x) (((uint32_t)(((uint32_t)(x)) << NUM_SENSOR_DESCS_SHIFT)) & NUM_SENSOR_DESCS_MASK)
 
+/* scmi protocol(from linux: include/linux/scmi_protocol.h) */
+#define TEMPERATURE_C (0x2)
+#define TEMPERATURE_F (0x3)
+#define TEMPERATURE_K (0x4)
+
+/* Common attributes_high macros(from linux: drivers/firmware/arm_scmi/sensors.c) */
+#define SENSOR_SCALE(x) SCMI_SENSOR_ATTR_HIGH_SENSOR_EXP(x)
+#define SENSOR_SCALE_SIGN  (1 << 4)
+#define SENSOR_SCALE_EXTEND  (0xFFFFFFE0)
+
+/* Sign extend to a full s32 */
+#define S32_EXT(v) \
+   ({ \
+       int __v = (v); \
+       if (__v & SENSOR_SCALE_SIGN) \
+           __v |= SENSOR_SCALE_EXTEND; \
+       __v; \
+   })
+
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
@@ -25,27 +43,107 @@
 /*******************************************************************************
  * Variables
  ******************************************************************************/
+scmi_sensor_desc_t desc[SCMI_SENSOR_MAX_DESC];
+scmi_sensor_reading_t readings[SCMI_SENSOR_MAX_READINGS];
 
 /*******************************************************************************
  * Code
  ******************************************************************************/
+static inline uint64_t __pow10(uint8_t x)
+{
+    uint64_t r = 1;
+
+    while (x--)
+    {
+        r *= 10;
+    }
+
+    return r;
+}
+
+void DEMO_GetTemp(int descIdx)
+{
+    uint32_t sensorConfig;
+    int32_t status;
+    bool enabled = false;
+
+    status = SCMI_SensorConfigSet(SM_PLATFORM_A2P, desc[descIdx].sensorId, SCMI_SENSOR_CONFIG_SET_ENABLE(1U));
+    if (SCMI_ERR_SUCCESS != status)
+    {
+        PRINTF("\r\n Sensor configuration failed!");
+	return;
+    }
+
+    status = SCMI_SensorConfigGet(SM_PLATFORM_A2P, desc[descIdx].sensorId, &sensorConfig);
+    if (SCMI_ERR_SUCCESS != status)
+    {
+        enabled = SCMI_SENSOR_CONFIG_GET_ENABLED(sensorConfig);
+        if (!enabled)
+        {
+            PRINTF("\r\n Sensor not enabled!");
+	    return;
+        }
+    }
+    PRINTF("\r\n Sensor is ready to read, press enter key to get the temperature.");
+
+    GETCHAR();
+
+    status = SCMI_SensorReadingGet(SM_PLATFORM_A2P, desc[descIdx].sensorId, SCMI_SENSOR_READ_FLAGS_ASYNC(0U), readings);
+    if (status == SCMI_ERR_SUCCESS)
+    {
+
+        unsigned int sensorType = SCMI_SENSOR_ATTR_HIGH_SENSOR_TYPE(desc[descIdx].sensorAttributesHigh);
+        int scale = S32_EXT(SENSOR_SCALE(desc[descIdx].sensorAttributesHigh));
+        uint64_t temp = 0ULL;
+        uint64_t factor = 0ULL;
+        uint64_t sensorValue = 0;
+
+        sensorValue = readings[0].sensorValueLow | ((uint64_t)readings[0].sensorValueHigh << 32UL);
+        switch (sensorType) {
+        case TEMPERATURE_C:
+            scale += 3;
+            break;
+        default:
+            break;
+        }
+
+        if (sensorType >= TEMPERATURE_C && sensorType <= TEMPERATURE_K)
+        {
+            factor = __pow10(abs(scale));
+            if (scale > 0)
+            {
+                temp = sensorValue * factor / 1000ULL;
+            }
+            else
+            {
+                    temp = sensorValue / factor / 1000ULL;
+            }
+            PRINTF("\r\n Current temperature is %llu degrees Celsius", temp);
+        }
+        else
+        {
+            PRINTF("\r\n The sensor %s is not a temperature sensor, so please select a temperature sensor", desc[descIdx].name);
+        }
+    }
+    else
+    {
+        PRINTF("\r\n Failed to get data from sensor");
+    }
+
+    return;
+}
+
 /*!
  * @brief Main function
  */
 int main(void)
 {
-    bool enabled = false;
     int32_t status;
-    uint32_t sensorConfig;
-    scmi_sensor_desc_t desc[SCMI_SENSOR_MAX_DESC];
-    scmi_sensor_reading_t readings[SCMI_SENSOR_MAX_READINGS];
     uint32_t numSensorFlags = 0U;
     uint32_t numSensorDescs = 0U;
     int digit = 0U;
     char ch;
     int descIdx = 0;
-    uint64_t sensorValue = 0;
-
 
     BOARD_InitHardware();
 
@@ -74,51 +172,18 @@ int main(void)
             digit = ch - '0';
             if (digit >= numSensorDescs)
             {
-                PRINTF("\r\n Valid number is 0-%d\r\n", numSensorDescs - 1);
+                PRINTF("\r\n Valid number is 0-%d", numSensorDescs - 1);
             }
-	    else
-	    {
-		PRINTF("\r\n Sensor(desc index is %d) %s is selected", digit, desc[digit].name);
-		descIdx = digit;
-	    }
+            else
+            {
+                PRINTF("\r\n Sensor(desc index is %d) %s is selected", digit, desc[digit].name);
+                descIdx = digit;
+                DEMO_GetTemp(descIdx);
+            }
         }
         else
         {
             PRINTF("\r\n Pls input a number:");
-            continue;
-        }
-
-        status = SCMI_SensorConfigSet(SM_PLATFORM_A2P, desc[descIdx].sensorId, SCMI_SENSOR_CONFIG_SET_ENABLE(1U));
-        if (SCMI_ERR_SUCCESS != status)
-        {
-            PRINTF("\r\n Sensor configuration failed!");
-            continue;
-        }
-
-        status = SCMI_SensorConfigGet(SM_PLATFORM_A2P, desc[descIdx].sensorId, &sensorConfig);
-        if (SCMI_ERR_SUCCESS != status)
-        {
-            enabled = SCMI_SENSOR_CONFIG_GET_ENABLED(sensorConfig);
-            if (!enabled)
-            {
-                PRINTF("\r\n Sensor not enabled!");
-                continue;
-            }
-        }
-        PRINTF("\r\n Sensor is ready to read, press enter key to get the temperature.");
-
-        GETCHAR();
-        
-        status = SCMI_SensorReadingGet(SM_PLATFORM_A2P, desc[descIdx].sensorId, SCMI_SENSOR_READ_FLAGS_ASYNC(0U), readings);
-        if (status == SCMI_ERR_SUCCESS)
-        {
-            sensorValue = readings[0].sensorValueLow | ((uint64_t)readings[0].sensorValueHigh << 32UL);
-            PRINTF("\r\n Current temperature is %llu degrees Celsius", sensorValue);
-        }
-	else
-        {
-            PRINTF("\r\n Failed to get data from sensor");
-            continue;
         }
     }
 }
