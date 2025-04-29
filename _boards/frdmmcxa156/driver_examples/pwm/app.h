563565dce9f726f499ec3dda7a6d03ee001ff4df
Neil Chen <cheng.chen_1@nxp.com>
2025-04-29 17:13:27 +0800

    [MCUX-79381][MCXA]Update eFlexPWM frequency error in app.h
     - Update eFlexPWM clock source to kCLOCK_MainClk
     - Change PWM clock frequency = fclk/4
    
    Signed-off-by: Neil Chen <cheng.chen_1@nxp.com>

--- _boards/frdmmcxa156/driver_examples/pwm/app.h
+++ _boards/frdmmcxa156/driver_examples/pwm/app.h
@@ -11,8 +11,8 @@
  ******************************************************************************/
 /*${macro:start}*/
 #define BOARD_PWM_BASEADDR     (FLEXPWM0)
-#define PWM_SRC_CLK_FREQ       (CLOCK_GetFreq(kCLOCK_BusClk))
-#define DEMO_PWM_CLOCK_DEVIDER (kPWM_Prescale_Divide_2)
+#define PWM_SRC_CLK_FREQ       (CLOCK_GetFreq(kCLOCK_MainClk))
+#define DEMO_PWM_CLOCK_DEVIDER (kPWM_Prescale_Divide_4)
 #define DEMO_PWM_FAULT_LEVEL   true
 
 #define BOARD_LED_GPIO     BOARD_LED_RED_GPIO
