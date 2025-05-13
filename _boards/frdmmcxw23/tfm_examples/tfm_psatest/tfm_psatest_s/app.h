c26f7f95c2c43d7fdd9847e32279029bb9a4efc9
Waqar Tahir <waqar.tahir@nxp.com>
2025-05-13 18:01:01 +0200

    [IOTDL-2072] TF-M examples configs added for frdmmcxw23
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

---
+++ _boards/frdmmcxw23/tfm_examples/tfm_psatest/tfm_psatest_s/app.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2025 NXP.
+ * NXP Confidential
+ * This software is owned or controlled by NXP and may only be used
+ * strictly in accordance with the applicable license terms. By expressly
+ * accepting such terms or by downloading, installing, activating and/or
+ * otherwise using the software, you are agreeing that you have read, and
+ * that you agree to comply with and are bound by, such license terms. If
+ * you do not agree to be bound by the applicable license terms, then you
+ * may not retain, install, activate or otherwise use the software.
+ */
+#ifndef _APP_H_
+#define _APP_H_
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+
+/*${macro:end}*/
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */
