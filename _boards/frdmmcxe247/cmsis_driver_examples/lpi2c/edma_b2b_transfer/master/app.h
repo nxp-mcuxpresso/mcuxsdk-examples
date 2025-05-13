ef6804a7807ed0b767e9c8f76387397e086158df
Michal Smola <michal.smola@nxp.com>
2025-05-13 16:11:54 +0200

    [MCUX-77351][MCXE247] Add cmsis lpi2c edma driver example
    Cmsis lpi2c edma driver example is not available for
    FRDM-MCXE247. Add it.
    
    Signed-off-by: Michal Smola <michal.smola@nxp.com>

---
+++ _boards/frdmmcxe247/cmsis_driver_examples/lpi2c/edma_b2b_transfer/master/app.h
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _APP_H_
+#define _APP_H_
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define EXAMPLE_I2C_MASTER            Driver_I2C1
+#define EXAMPLE_LPI2C_DMAMUX_BASEADDR DMAMUX
+#define EXAMPLE_LPI2C_DMA_BASEADDR    DMA0
+
+#define DMA0_IRQn DMA0_IRQn
+/*${macro:end}*/
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */
