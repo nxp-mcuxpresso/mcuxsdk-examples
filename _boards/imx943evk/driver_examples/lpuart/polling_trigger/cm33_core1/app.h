nxp-mcuxpresso/mcuxsdk-examples

commit 6261a8e5b470cd1ee5e1891430143be0ee38e9c4

[MCUX-75618][imx943evk] add ipsync examples
Add the IPs example to work with signal trigger from xbar.
Includes LPUART, LPI2C, LPSPI, TPM, LPIT, QTMR, FLEXIO, FLEXPWM, EDMA4,
SAR_ADC, and 1588 timer examples.

Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

_boards/imx943evk/driver_examples/lpuart/polling_trigger/cm33_core1/app.h
@@ -1,0 +1,43 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-12-Clause
+ */
+#ifndef _APP_H_
+#define _APP_H_
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+/* Get source clock for LPIT driver */
+#define DEMO_LPUART          LPUART12
+#define DEMO_LPUART_INSTANCE_IDX (12U)
+#define LPUART_CLOCK_ROOT    hal_clock_lpuart12
+#define DEMO_LPUART_CLK_FREQ HAL_ClockGetRate(LPUART_CLOCK_ROOT)
+/*${macro:end}*/
+
+/* define instance */
+#define BOARD_TPM_BASEADDR TPM6
+#define BOARD_TPM_CHANNEL  kTPM_Chnl_0
+
+/* Interrupt to enable and flag to read; depends on the TPM channel used */
+#define TPM_CHANNEL_INTERRUPT_ENABLE kTPM_Chnl0InterruptEnable
+#define TPM_CHANNEL_FLAG             kTPM_Chnl0Flag
+
+/* Interrupt number and interrupt handler for the TPM instance used */
+#define TPM_INTERRUPT_NUMBER TPM6_IRQn
+#define TPM_LED_HANDLER      TPM6_IRQHandler
+
+/* Get source clock for TPM driver */
+#define LPTPM_CLOCK_ROOT hal_clock_tpm6
+#define TPM_SOURCE_CLOCK HAL_ClockGetRate(LPTPM_CLOCK_ROOT)
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */

_boards/imx943evk/driver_examples/edma4/memory_to_memory_trigger/cm33_core1/app.h
@@ -1,0 +1,46 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _APP_H_
+#define _APP_H_
+
+/*${header:start}*/
+#include "fsl_common.h"
+/*${header:end}*/
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define EXAMPLE_DMA_BASEADDR EDMA4
+#define DEMO_DMA_CHANNEL_0   0U
+#define APP_DMA_IRQ   EDMA4_CH0_IRQn
+#define APP_DMA_IRQ_HANDLER  EDMA4_CH0_IRQHandler
+#define BUFFER_LENGTH 4U
+/*${macro:end}*/
+
+/* define instance */
+#define BOARD_TPM_BASEADDR TPM6
+#define BOARD_TPM_CHANNEL  kTPM_Chnl_0
+
+/* Interrupt to enable and flag to read; depends on the TPM channel used */
+#define TPM_CHANNEL_INTERRUPT_ENABLE kTPM_Chnl0InterruptEnable
+#define TPM_CHANNEL_FLAG             kTPM_Chnl0Flag
+
+/* Interrupt number and interrupt handler for the TPM instance used */
+#define TPM_INTERRUPT_NUMBER TPM6_IRQn
+#define TPM_LED_HANDLER      TPM6_IRQHandler
+
+/* Get source clock for TPM driver */
+#define LPTPM_CLOCK_ROOT hal_clock_tpm6
+#define TPM_SOURCE_CLOCK HAL_ClockGetRate(LPTPM_CLOCK_ROOT)
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */

_boards/imx943evk/driver_examples/flexio/pwm_trgout/cm33_core1/app.h
@@ -1,0 +1,31 @@
+/*
+ * Copyright 2025 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _APP_H_
+#define _APP_H_
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define DEMO_TIME_DELAY_FOR_DUTY_CYCLE_UPDATE 100000U
+#define DEMO_FLEXIO_BASEADDR                  FLEXIO1
+#define DEMO_FLEXIO_OUTPUTPIN                 2U /* Select flexio1_3_1_flexio2 as PWM output */
+#define DEMO_FLEXIO_TIMER_CH                  0U /* Flexio timer0 used */
+#define DEMO_FLEXIO_CLOCK hal_clock_flexio1
+#define DEMO_FLEXIO_CLOCK_FREQUENCY HAL_ClockGetRate(DEMO_FLEXIO_CLOCK)
+#define DEMO_FLEXIO_FREQUENCY       100000U
+/*${macro:end}*/
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */

_boards/imx943evk/driver_examples/flexio/pwm_trigger/cm33_core1/app.h
@@ -1,0 +1,47 @@
+/*
+ * Copyright 2025 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _APP_H_
+#define _APP_H_
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define DEMO_TIME_DELAY_FOR_DUTY_CYCLE_UPDATE 100000U
+#define DEMO_FLEXIO_BASEADDR                  FLEXIO1
+#define DEMO_FLEXIO_OUTPUTPIN                 5U /* Select flexio1_3_1_flexio2 as PWM output */
+#define DEMO_FLEXIO_TIMER_CH                  0U /* Flexio timer0 used */
+#define DEMO_FLEXIO_CLOCK hal_clock_flexio1
+#define DEMO_FLEXIO_CLOCK_FREQUENCY HAL_ClockGetRate(DEMO_FLEXIO_CLOCK)
+#define DEMO_FLEXIO_FREQUENCY       100000U
+/*${macro:end}*/
+
+/* define instance */
+#define BOARD_TPM_BASEADDR TPM6
+#define BOARD_TPM_CHANNEL  kTPM_Chnl_0
+
+/* Interrupt to enable and flag to read; depends on the TPM channel used */
+#define TPM_CHANNEL_INTERRUPT_ENABLE kTPM_Chnl0InterruptEnable
+#define TPM_CHANNEL_FLAG             kTPM_Chnl0Flag
+
+/* Interrupt number and interrupt handler for the TPM instance used */
+#define TPM_INTERRUPT_NUMBER TPM6_IRQn
+#define TPM_LED_HANDLER      TPM6_IRQHandler
+
+/* Get source clock for TPM driver */
+#define LPTPM_CLOCK_ROOT hal_clock_tpm6
+#define TPM_SOURCE_CLOCK HAL_ClockGetRate(LPTPM_CLOCK_ROOT)
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */

_boards/imx943evk/driver_examples/lpi2c/polling_trigger/cm33_core1/app.h
@@ -1,0 +1,49 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _APP_H_
+#define _APP_H_
+
+/*${header:start}*/
+#include "hal_clock.h"
+/*${header:end}*/
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define LPI2C_MASTER_CLOCK_ROOT  hal_clock_lpi2c4
+#define LPI2C_MASTER_CLOCK_FREQUENCY    HAL_ClockGetRate(LPI2C_MASTER_CLOCK_ROOT)
+#define EXAMPLE_I2C_MASTER_BASE LPI2C4
+
+#define LPI2C_SLAVE_CLOCK_ROOT  hal_clock_lpi2c6
+#define LPI2C_SLAVE_CLOCK_FREQUENCY    HAL_ClockGetRate(LPI2C_MASTER_CLOCK_ROOT)
+#define EXAMPLE_I2C_SLAVE_BASE LPI2C6
+
+/* define instance */
+#define BOARD_TPM_BASEADDR TPM6
+#define BOARD_TPM_CHANNEL  kTPM_Chnl_0
+
+/* Interrupt to enable and flag to read; depends on the TPM channel used */
+#define TPM_CHANNEL_INTERRUPT_ENABLE kTPM_Chnl0InterruptEnable
+#define TPM_CHANNEL_FLAG             kTPM_Chnl0Flag
+
+/* Interrupt number and interrupt handler for the TPM instance used */
+#define TPM_INTERRUPT_NUMBER TPM6_IRQn
+#define TPM_LED_HANDLER      TPM6_IRQHandler
+
+/* Get source clock for TPM driver */
+#define LPTPM_CLOCK_ROOT hal_clock_tpm6
+#define TPM_SOURCE_CLOCK HAL_ClockGetRate(LPTPM_CLOCK_ROOT)
+/*${macro:end}*/
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */

_boards/imx943evk/driver_examples/lpit/single_channel_trigger/cm33_core1/app.h
@@ -1,0 +1,46 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _APP_H_
+#define _APP_H_
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define DEMO_LPIT_BASE       LPIT1
+#define DEMO_LPIT_IRQn       LPIT1_IRQn
+#define DEMO_LPIT_IRQHandler LPIT1_IRQHandler
+/* Get source clock for LPIT driver */
+#define LPIT_MASTER_CLOCK_ROOT hal_clock_busaon
+#define LPIT_SOURCECLOCK       HAL_ClockGetRate(LPIT_MASTER_CLOCK_ROOT)
+#define LED_INIT()
+#define LED_TOGGLE()
+/*${macro:end}*/
+
+/* define instance */
+#define BOARD_TPM_BASEADDR TPM6
+#define BOARD_TPM_CHANNEL  kTPM_Chnl_0
+
+/* Interrupt to enable and flag to read; depends on the TPM channel used */
+#define TPM_CHANNEL_INTERRUPT_ENABLE kTPM_Chnl0InterruptEnable
+#define TPM_CHANNEL_FLAG             kTPM_Chnl0Flag
+
+/* Interrupt number and interrupt handler for the TPM instance used */
+#define TPM_INTERRUPT_NUMBER TPM6_IRQn
+#define DEMO_TPM_IRQHandler      TPM6_IRQHandler
+
+/* Get source clock for TPM driver */
+#define LPTPM_CLOCK_ROOT hal_clock_tpm6
+#define TPM_SOURCE_CLOCK HAL_ClockGetRate(LPTPM_CLOCK_ROOT)
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */

_boards/imx943evk/driver_examples/lpspi/interrupt_trigger/cm33_core1/app.h
@@ -1,0 +1,59 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _APP_H_
+#define _APP_H_
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+/* Master related */
+#define EXAMPLE_LPSPI_MASTER_BASEADDR   (LPSPI8)
+#define EXAMPLE_LPSPI_MASTER_IRQN       (LPSPI8_IRQn)
+#define EXAMPLE_LPSPI_MASTER_IRQHandler (LPSPI8_IRQHandler)
+
+#define EXAMPLE_LPSPI_MASTER_PCS_FOR_INIT     (kLPSPI_Pcs0)
+#define EXAMPLE_LPSPI_MASTER_PCS_FOR_TRANSFER (kLPSPI_MasterPcs0)
+
+#define EXAMPLE_LPSPI_MASTER_CLK_ID (hal_clock_lpspi8)
+/* Slave related */
+#define EXAMPLE_LPSPI_SLAVE_BASEADDR   (LPSPI3)
+#define EXAMPLE_LPSPI_SLAVE_IRQN       (LPSPI3_IRQn)
+#define EXAMPLE_LPSPI_SLAVE_IRQHandler (LPSPI3_IRQHandler)
+
+#define EXAMPLE_LPSPI_SLAVE_PCS_FOR_INIT     (kLPSPI_Pcs0)
+#define EXAMPLE_LPSPI_SLAVE_PCS_FOR_TRANSFER (kLPSPI_SlavePcs0)
+
+#define EXAMPLE_LPSPI_SLAVE_CLK_ID (hal_clock_lpspi3)
+
+#define EXAMPLE_LPSPI_MASTER_CLOCK_FREQ HAL_ClockGetRate(EXAMPLE_LPSPI_MASTER_CLK_ID) 
+#define EXAMPLE_LPSPI_SLAVE_CLOCK_FREQ  HAL_ClockGetRate(EXAMPLE_LPSPI_SLAVE_CLK_ID)
+
+/* define instance */
+#define BOARD_TPM_BASEADDR TPM6
+#define BOARD_TPM_CHANNEL  kTPM_Chnl_0
+
+/* Interrupt to enable and flag to read; depends on the TPM channel used */
+#define TPM_CHANNEL_INTERRUPT_ENABLE kTPM_Chnl0InterruptEnable
+#define TPM_CHANNEL_FLAG             kTPM_Chnl0Flag
+
+/* Interrupt number and interrupt handler for the TPM instance used */
+#define TPM_INTERRUPT_NUMBER TPM6_IRQn
+#define TPM_LED_HANDLER      TPM6_IRQHandler
+
+/* Get source clock for TPM driver */
+#define LPTPM_CLOCK_ROOT hal_clock_tpm6
+#define TPM_SOURCE_CLOCK HAL_ClockGetRate(LPTPM_CLOCK_ROOT)
+
+/*${macro:end}*/
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif

_boards/imx943evk/driver_examples/lptmr/lptmr_trigger_out/cm33_core1/app.h
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2025 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _APP_H_
+#define _APP_H_
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define DEMO_LPTMR_BASE   LPTMR1
+#define DEMO_LPTMR_IRQn   LPTMR1_IRQn
+#define LPTMR_LED_HANDLER LPTMR1_IRQHandler
+/* Get source clock for LPTMR driver */
+#define LPTMR_SOURCE_CLOCK 24000000U
+/* Define LPTMR microseconds counts value */
+#define LPTMR_USEC_COUNT 1000U
+/*${macro:end}*/
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */

commit 41d1c0e3d1ce669f6868c95f68b4d0f19638ca0e

[MCUX-79985][License] fix SPDX-License-Identifier issue
The SPDX-License-Identifier should be BSD-3-Clause.

Fixes: 6261a8e5b4("[MCUX-75618][imx943evk] add ipsync examples")
Signed-off-by: Xiaoliang Yang <xiaoliang.yang_1@nxp.com>

_boards/imx943evk/driver_examples/lpuart/polling_trigger/cm33_core1/app.h
@@ -1,7 +1,7 @@
 /*
  * Copyright 2025 NXP
  *
- * SPDX-License-Identifier: BSD-12-Clause
+ * SPDX-License-Identifier: BSD-3-Clause
  */
 #ifndef _APP_H_
 #define _APP_H_