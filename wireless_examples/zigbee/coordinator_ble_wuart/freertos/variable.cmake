a7bdcf6dca012fa012e3085c069c34a585ff2a9d
Anca Jeanina Dragusin <ancajeanina.dragusin@nxp.com>
2025-05-15 12:07:22 +0300

    [MCZB-2815] zigbee: Added zigbee_coordinator_ble_wuart example
    The Bluetooth® LE - Zigbee dynamic mode mode application showcases the Zigbee
    commissioning over Bluetooth LE use case (dedicated for MCXW_SERIES). The
    zigbee application part is based on the Zigbee Coordinator example and the
    Bluetooth LE application part is base on the Wireless UART peripheral demo.
    
    Signed-off-by: Anca Jeanina Dragusin <ancajeanina.dragusin@nxp.com>

---
+++ wireless_examples/zigbee/coordinator_ble_wuart/freertos/variable.cmake
@@ -0,0 +1,5 @@
+mcux_set_variable(NXP_ZB_BASE ${SdkRootDirPath}/middleware/wireless/zigbee)
+mcux_set_variable(PDUMCONFIG  ${NXP_ZB_BASE}/tools/PDUMConfig/Source/PDUMConfig.py)
+mcux_set_variable(ZPSCONFIG   ${NXP_ZB_BASE}/tools/ZPSConfig/Source/ZPSConfig.py)
+mcux_set_variable(ZPSCFG      ${NXP_ZB_BASE}/examples/zigbee_coordinator/src/coordinator.zpscfg)
+mcux_set_variable(MEMSIZE     ${NXP_ZB_BASE}/tools/memsize2/memsize.py)
