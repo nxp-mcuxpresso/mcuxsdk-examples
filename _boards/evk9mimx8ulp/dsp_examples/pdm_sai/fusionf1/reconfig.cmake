5b3503499291becd00cdd0bf8a6ea345393be638
Tomas Barak <tomas.barak@nxp.com>
2025-05-19 08:52:45 +0200

    [MCUX-77500] Fix pdm_sai build fail
    - re-enable cm33 example
    - remove O0 optimization

--- _boards/evk9mimx8ulp/dsp_examples/pdm_sai/fusionf1/reconfig.cmake
+++ _boards/evk9mimx8ulp/dsp_examples/pdm_sai/fusionf1/reconfig.cmake
@@ -21,6 +21,12 @@ mcux_add_include(
 )
 
 # Optimize compiler settings to reduce code size for IRAM memory constraints
+mcux_remove_configuration(
+    TARGETS debug
+    CC "-O0 -g"
+    CX "-O0 -g"
+)
+
 mcux_add_xtensa_configuration(
     TARGETS debug
     CC "-O1 -g"
