f46ffcfc12bf007f076a1c405f8c08e31df60529
Qiankun Li <qiankun.li@nxp.com>
2025-05-28 09:16:43 +0530

    [MCUX-77221] Enable SLIM component macro for FRDM-MCXN947 wifi_cli
    [Description]
    1. Adding SLIM macros in reconfig.cmake is a legacy method to enable
    SLIM feature.
    2. Enable SLIM component in prj.conf
    
    [Reviewers]
    Gannicus Guo, Pankaj Razdan.
    
    Signed-off-by: Qiankun Li <qiankun.li@nxp.com>

--- _boards/frdmmcxn947/wifi_examples/wifi_cli/reconfig.cmake
+++ _boards/frdmmcxn947/wifi_examples/wifi_cli/reconfig.cmake
@@ -1,10 +1,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/${core_id}/reconfig.cmake OPTIONAL)
 include(${SdkRootDirPath}/${board_root}/${board}/wifi_examples/common/reconfig.cmake OPTIONAL)
 mcux_add_macro(
-    CC "-DCONFIG_WIFI_SLIM_ROAM=1\
-       -DCONFIG_WIFI_SLIM_STA=1\
-       -DCONFIG_WIFI_SLIM_UAP=1\
-       -DCONFIG_FREERTOS_LOW_MEMORY_FOOTPRINT=1\
-       -DCONFIG_LWIP_LOW_MEM_FOOTPRINT=1\
-       -DCONFIG_FW_DNLD_ASYNC=1"
+    CC "-DCONFIG_FW_DNLD_ASYNC=1"
 )
