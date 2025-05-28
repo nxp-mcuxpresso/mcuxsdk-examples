c9ec2d9a894e72071974ca7d66463df3cb14f180
Qiankun Li <qiankun.li@nxp.com>
2025-05-28 08:42:38 +0530

    [MCUX-77221] Remove Zero-copy macors for FRDM-MCXN947 wifi_cli.
    [Description]
    1. Zero-copy macros defined in reconfig.camke can bed effective for all
    Wi-Fi SOCs.
    2. Zero-copy feature has not verified yet in IW610.
       Will remove macros temporarily.
    
    [Reviewers]
    Gannicus Guo, Pankaj Razdan
    
    Signed-off-by: Qiankun Li <qiankun.li@nxp.com>

--- _boards/frdmmcxn947/wifi_examples/wifi_cli/reconfig.cmake
+++ _boards/frdmmcxn947/wifi_examples/wifi_cli/reconfig.cmake
@@ -1,9 +1,7 @@
 include(${CMAKE_CURRENT_LIST_DIR}/${core_id}/reconfig.cmake OPTIONAL)
 include(${SdkRootDirPath}/${board_root}/${board}/wifi_examples/common/reconfig.cmake OPTIONAL)
 mcux_add_macro(
-    CC "-DFSL_USDHC_ENABLE_SCATTER_GATHER_TRANSFER=1\
-       -DSDMMCHOST_ENABLE_CACHE_LINE_ALIGN_TRANSFER=1\
-       -DCONFIG_WIFI_SLIM_ROAM=1\
+    CC "-DCONFIG_WIFI_SLIM_ROAM=1\
        -DCONFIG_WIFI_SLIM_STA=1\
        -DCONFIG_WIFI_SLIM_UAP=1\
        -DCONFIG_FREERTOS_LOW_MEMORY_FOOTPRINT=1\
