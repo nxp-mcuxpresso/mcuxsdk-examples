177fa193805f246e7d578a71bcb8055499b10b92
Anca Jeanina Dragusin <ancajeanina.dragusin@nxp.com>
2025-06-05 10:51:10 +0300

    [MCZB-2828] mcxw72evk, mcxw71evk: coordinator_ble_wuart: Added freertos support
    Signed-off-by: Anca Jeanina Dragusin <ancajeanina.dragusin@nxp.com>

---
+++ _boards/mcxw71evk/wireless_examples/zigbee/coordinator_ble_wuart/freertos/reconfig.cmake
@@ -0,0 +1,112 @@
+mcux_set_variable(CONFIG_ZB_PLATFORM "K32W1")
+
+mcux_add_include(
+    BASE_PATH ${SdkRootDirPath}
+    INCLUDES middleware/wireless/framework/boards/kw45_k32w1/K32W1480
+)
+
+mcux_add_source(
+    BASE_PATH ${SdkRootDirPath}/${device_root}/${soc_portfolio}/${soc_series}/${device}
+    SOURCES gcc/startup_${device}.S
+)
+
+mcux_add_source(BASE_PATH ${SdkRootDirPath}/${device_root}/${soc_portfolio}/${soc_series}/${device} SOURCES ${device}.h)
+mcux_add_source(BASE_PATH ${SdkRootDirPath}/${device_root}/${soc_portfolio}/${soc_series}/${device} SOURCES system_${device}.h)
+mcux_add_source(BASE_PATH ${SdkRootDirPath}/${device_root}/${soc_portfolio}/${soc_series}/${device} SOURCES system_${device}.c)
+
+mcux_add_source(BASE_PATH ${SdkRootDirPath}/middleware/wireless/zigbee/platform/${CONFIG_ZB_PLATFORM}
+                SOURCES FreeRTOSConfig.h)
+                
+mcux_add_configuration(
+    CC "-DGENERIC_LIST_LIGHT=0\
+        -DUSE_SMU2_AS_SYSTEM_MEMORY=1\
+        -DTM_ENABLE_TIME_STAMP=1\
+        -DUSE_NBU=1\
+        -DgMemManagerLightGuardsCheckEnable=1\
+        -DgMemManagerLightExtendHeapAreaUsage=1\
+        -DUSE_PB_RAM_AS_SYSTEM_MEMORY=1\
+        -DgHwParamsProdDataPlacement_c=gHwParamsProdDataIfrMode_c\
+        -DFSL_OSA_TASK_ENABLE=1\
+        -DZBPRO_DEVICE_TYPE=ZCR\
+        -DcPWR_FullPowerDownMode=1\
+        -DHEAP_SIZE=4000\
+        -DLITTLE_ENDIAN_PROCESSOR\
+        -Dvector_table_size=512\
+        -D__ram_vector_table__=1\
+        -DosNumberOfSemaphores=0\
+        -DosNumberOfMutexes=0\
+        -DosNumberOfMessageQs=0\
+        -DosNumberOfMessages=0\
+        -DosNumberOfEvents=0\
+        -DgMainThreadStackSize_c=4096\
+        -DFSL_RTOS_FREE_RTOS\
+        -DSDK_OS_FREE_RTOS\
+        -DgMainThreadPriority_c=7\
+        -DgTaskMultipleInstancesManagement_c=0\
+        -DFSL_OSA_BM_TIMER_CONFIG=FSL_OSA_BM_TIMER_SYSTICK\
+        -DENABLE_RAM_VECTOR_TABLE\
+        -DgFlashEraseDuringWrite=0\
+        -DgInvalidateHeaderLength=336\
+        -DgEepromType_d=gEepromDevice_InternalFlash_c\
+        -DgOtaVerifyWrite_d=0\
+        -DgEePromParams_BufferSize_c=512\
+        -DgEepromParams_bufferedWrite_c=1\
+        -DgEepromParams_CurrentOffset=0\
+        -DOM15081\
+        -DZIGBEE_USE_FRAMEWORK=1\
+        -DgSupportBle=0\
+        -DJN518x=5180\
+        -DPDM_NO_RTOS\
+        -D__NEWLIB__\
+        -DMCUXPRESSO_SDK\
+        -DTIMER_PORT_TYPE_LPTMR=1\
+        -DSERIAL_PORT_TYPE_RPMSG=1\
+        -DHAL_UART_ADAPTER_LOWPOWER=1\
+        -DMULTICORE_APP=1\
+        -DDEBUG\
+        -DCR_INTEGER_PRINTF\
+        -D__MCUXPRESSO\
+        -D__USE_CMSIS\
+        -DPRINTF_ADVANCED_ENABLE=1\
+        "
+)
+
+mcux_add_armgcc_configuration(
+    LD "-Xlinker --sort-section=alignment\
+        -Xlinker --defsym=gUseNVMLink_d=1\
+        -Xlinker --defsym=gUseInternalStorageLink_d=1\
+        -Xlinker --defsym=__stack_size__=0x2800\
+        -Xlinker --defsym=__heap_size__=0x8000\
+        -Xlinker --defsym=gUseNVMLink_d=1\
+        -Xlinker --defsym=m_lowpower_flag_start=0x489C007C"
+)
+
+# add BLE configuration
+mcux_add_configuration(
+    CC "-DSERIAL_USE_CONFIGURE_STRUCTURE=1\
+       -DgButtonSupported_d=1\
+       -DSDK_DEBUGCONSOLE=2\
+       -DOSA_USED\
+       -DSDK_DEBUGCONSOLE_UART\
+       -DSDK_COMPONENT_INTEGRATION=1\
+       -DFSL_OSA_TASK_ENABLE=1\
+       -DCR_INTEGER_PRINTF\
+       -DMCXW716C\
+       -DCPU_MCXW716CMFTA\
+       -DCFG_BLE_PRJ=1\
+       -DENABLE_RAM_VECTOR_TABLE=1\
+       -DNXP_SSSAPI\
+       -DNXP_ELE200\
+       -DHAL_FLASH_ROMAPI_DRIVER=1\
+       -DgUseHciTransportDownward_d=1\
+       -DTM_ENABLE_TIME_STAMP=1"
+)
+
+mcux_add_armgcc_configuration(
+    LD "-Xlinker --defsym=gUseNVMLink_d=1\
+        -Xlinker --defsym=gEraseNVMLink_d=1\
+        -Xlinker --defsym=__ram_vector_table__=1\
+        -Xlinker --defsym=gFlashNbuImage_d=1\
+        -Xlinker --defsym=gUseProdInfoLegacyMode_d=1\
+        -Xlinker --defsym=__use_shmem__=1"
+)
