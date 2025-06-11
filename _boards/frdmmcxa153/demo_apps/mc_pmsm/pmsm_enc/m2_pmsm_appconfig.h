bd5bdc183f2bf9dbf91ad82d488878cbd83b57d6
nxa18911 <peter.butko@nxp.com>
2025-06-11 14:03:58 +0200

    [MCROZ-1503] Updateded appconfig on MCXA153

--- _boards/frdmmcxa153/demo_apps/mc_pmsm/pmsm_enc/m2_pmsm_appconfig.h
+++ _boards/frdmmcxa153/demo_apps/mc_pmsm/pmsm_enc/m2_pmsm_appconfig.h
@@ -5,8 +5,8 @@
 */
 
 /*
-    * FILE NAME: ../../../examples/frdmmcxa153/demo_apps/mc_pmsm/pmsm_enc/m2_pmsm_appconfig.h
-    * DATE: Tue Aug 20 2024, 06:17:46
+    * FILE NAME: ../../../boards/frdmmcxa153/demo_apps/mc_pmsm/pmsm_enc/m2_pmsm_appconfig.h
+    * DATE: Wed Jun 11 2025, 13:46:27
 */
 
 /*
@@ -15,12 +15,12 @@
         "parametersNmax": 4400,
         "parametersImax": 8.25,
         "parametersEmax": 15.1,
-        "parametersPP": 4,
-        "parametersRs": 0.72,
-        "parametersLd": 0.000326,
-        "parametersLq": 0.000294,
-        "parametersKe": 0.0393,
-        "parametersJ": 0.000017,
+        "parametersPP": 2,
+        "parametersRs": 0.45,
+        "parametersLd": 0.000375,
+        "parametersLq": 0.000429,
+        "parametersKe": 0.0138,
+        "parametersJ": 0.0000016,
         "parametersIphNom": 2,
         "parametersUphNom": 15,
         "parametersNnom": 4000,
@@ -28,9 +28,9 @@
         "parametersUdcbTrip": 29,
         "parametersUdcbUnder": 16,
         "parametersUdcbOver": 32,
-        "parametersNover": 4399,
-        "parametersNmin": 300,
-        "parametersEblock": 0.7,
+        "parametersNover": 4300,
+        "parametersNmin": 400,
+        "parametersEblock": 1.4,
         "parametersEblockPer": 2000,
         "parametersKt": 0.01217,
         "parametersUdcbIIRf0": 100,
@@ -86,8 +86,8 @@
 
 /*
 {
-    "motorName": "Teknic M-2310P",
-    "motorDescription": "Configuration for the Teknic motor (fixed-point implementation)."
+    "motorName": "Linix 45ZWN 24-40",
+    "motorDescription": "Configuration for LINIX motor."
 }
 */
 
@@ -96,20 +96,20 @@
 
 /* PARAMETERS*/
 #define M1_N_MAX (4400.0F)
-#define M1_FREQ_MAX (293.333F)
+#define M1_FREQ_MAX (146.667F)
 #define M1_I_MAX (8.25F)
 #define M1_U_MAX (35.1029F)
 #define M1_E_MAX (15.1F)
-#define M1_MOTOR_PP (4)
+#define M1_MOTOR_PP (2)
 #define M1_I_PH_NOM FRAC16(0.242424)
 #define M1_N_NOM FRAC16(0.909091)
 #define M1_U_DCB_MAX (60.8F)
 #define M1_U_DCB_TRIP FRAC16(0.476974)
 #define M1_U_DCB_UNDERVOLTAGE FRAC16(0.263158)
 #define M1_U_DCB_OVERVOLTAGE FRAC16(0.526316)
-#define M1_N_OVERSPEED FRAC16(0.999773)
-#define M1_N_MIN FRAC16(0.0681818)
-#define M1_E_BLOCK_TRH FRAC16(0.0463576)
+#define M1_N_OVERSPEED FRAC16(0.977273)
+#define M1_N_MIN FRAC16(0.0909091)
+#define M1_E_BLOCK_TRH FRAC16(0.0927152)
 #define M1_E_BLOCK_PER (2000)
 #define M1_CALIB_DURATION (200)
 #define M1_FAULT_DURATION (6000)
@@ -122,22 +122,22 @@
 #define M1_UDCB_IIR_A1 FRAC32(0.480743)
 #define M1_SCALAR_VHZ_FACTOR_GAIN FRAC16(0.470046)
 #define M1_SCALAR_VHZ_FACTOR_SHIFT (0)
-#define M1_SCALAR_INTEG_GAIN ACC32(0.0366667)
+#define M1_SCALAR_INTEG_GAIN ACC32(0.0183333)
 #define M1_SCALAR_RAMP_UP FRAC32(0.000142045)
 #define M1_SCALAR_RAMP_DOWN FRAC32(0.000142045)
 /* CURRENTLOOP*/
-#define M1_D_KP_GAIN ACC32(0.108071)
-#define M1_D_KI_GAIN ACC32(0.00784013)
-#define M1_Q_KP_GAIN ACC32(0.0808526)
-#define M1_Q_KI_GAIN ACC32(0.00707054)
+#define M1_D_KP_GAIN ACC32(0.213206)
+#define M1_D_KI_GAIN ACC32(0.00901855)
+#define M1_Q_KP_GAIN ACC32(0.259137)
+#define M1_Q_KI_GAIN ACC32(0.0103172)
 #define M1_CLOOP_LIMIT FRAC16(0.9)
 /* SPEEDLOOP*/
 #define M1_SPEED_RAMP_UP FRAC32(0.00227273)
 #define M1_SPEED_RAMP_DOWN FRAC32(0.00227273)
 #define M1_SPEED_LOOP_HIGH_LIMIT FRAC16(0.363636)
 #define M1_SPEED_LOOP_LOW_LIMIT FRAC16(-0.363636)
-#define M1_SPEED_PI_PROP_GAIN ACC32(0.670206)
-#define M1_SPEED_PI_INTEG_GAIN ACC32(0.00201062)
+#define M1_SPEED_PI_PROP_GAIN ACC32(0.335103)
+#define M1_SPEED_PI_INTEG_GAIN ACC32(0.00100531)
 #define M1_SPEED_IIR_B0 FRAC32(0.119529)
 #define M1_SPEED_IIR_B1 FRAC32(0.119529)
 #define M1_SPEED_IIR_A1 FRAC32(0.260943)
@@ -157,18 +157,18 @@
 #define M1_OL_START_RAMP_INC FRAC32(0.0000426136)
 #define M1_OL_START_I FRAC16(0.0727273)
 #define M1_MERG_SPEED_TRH FRAC16(0.113636)
-#define M1_MERG_COEFF FRAC16(0.00312500)
-#define M1_I_SCALE ACC32(0.878706)
-#define M1_U_SCALE ACC32(0.716796)
-#define M1_E_SCALE ACC32(0.308339)
-#define M1_WI_SCALE ACC32(0.0912840)
-#define M1_BEMF_DQ_KP_GAIN ACC32(0.278091)
-#define M1_BEMF_DQ_KI_GAIN ACC32(0.0395528)
-#define M1_TO_KP_SHIFT (-2)
+#define M1_MERG_COEFF FRAC16(0.00156250)
+#define M1_I_SCALE ACC32(0.930233)
+#define M1_U_SCALE ACC32(0.659674)
+#define M1_E_SCALE ACC32(0.283768)
+#define M1_WI_SCALE ACC32(0.0612927)
+#define M1_BEMF_DQ_KP_GAIN ACC32(0.526534)
+#define M1_BEMF_DQ_KI_GAIN ACC32(0.0454978)
+#define M1_TO_KP_SHIFT (-1)
 #define M1_TO_KP_GAIN FRAC16(0.545455)
-#define M1_TO_KI_SHIFT (-10)
+#define M1_TO_KI_SHIFT (-9)
 #define M1_TO_KI_GAIN FRAC16(0.548351)
-#define M1_TO_THETA_SHIFT (-4)
+#define M1_TO_THETA_SHIFT (-5)
 #define M1_TO_THETA_GAIN FRAC16(0.586667)
 #define M1_TO_SPEED_IIR_B0 FRAC32(0.0364103)
 #define M1_TO_SPEED_IIR_B1 FRAC32(0.0364103)
--- _boards/frdmmcxa153/demo_apps/mc_pmsm/pmsm_enc/main.c
+++ _boards/frdmmcxa153/demo_apps/mc_pmsm/pmsm_enc/main.c
@@ -76,8 +76,8 @@ static uint32_t ui32ButtonFilter = 0U;
 
 /* Structure used in FM to get required ID's */
 app_ver_t g_sAppIdFM = {
-    "../../../examples/frdmmcxa153/demo_apps/mc_pmsm/pmsm_enc",     /* User Path 1- the highest priority */
-    "",       /* User Path 2 */
+    "../../../examples/_boards/frdmmcxa153/demo_apps/mc_pmsm/pmsm_enc",     /* User Path 1- the highest priority */
+    "../../../boards/frdmmcxa153/demo_apps/mc_pmsm/pmsm_enc",       /* User Path 2 */
     "frdmmcxa153",    /* board id */
     "pmsm_enc", /* example id */
     MCRSP_VER,      /* sw version */
