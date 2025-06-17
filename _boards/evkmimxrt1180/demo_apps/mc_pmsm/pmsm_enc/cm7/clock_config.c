5ff3dbf6e7b19bcf02c7c94f852c9f70eca2a4d7
Eric Sun <jian.sun@nxp.com>
2025-06-17 16:35:31 +0800

    [MCUX-79984][RT118x] Update Clock Config to Remove OSC 400MHz Gating Operation
    Signed-off-by: Eric Sun <jian.sun@nxp.com>

--- _boards/evkmimxrt1180/demo_apps/mc_pmsm/pmsm_enc/cm7/clock_config.c
+++ _boards/evkmimxrt1180/demo_apps/mc_pmsm/pmsm_enc/cm7/clock_config.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018-2020 NXP
+ * Copyright 2018-2025 NXP
  * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -388,7 +388,6 @@ void BOARD_BootClockRUN(void)
 
     /* Init OSC RC 400M */
     CLOCK_OSC_EnableOscRc400M();
-    CLOCK_OSC_GateOscRc400M(false);
 
     /* Switch both core to OscRC400M first */
 #if (__CORTEX_M == 7)
