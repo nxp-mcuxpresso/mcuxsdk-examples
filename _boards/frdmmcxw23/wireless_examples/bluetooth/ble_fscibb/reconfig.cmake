2ab6abfa68f80a2ea673006676a1565950c39f45
Axel Le Bourhis <axel.lebourhis@nxp.com>
2025-06-18 16:14:12 +0200

    [MCSE-891][mcxw23] Add ble_fscibb application support
    Port the application on mcxw23 boards for both GCC and IAR toolchain.
    
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

---
+++ _boards/frdmmcxw23/wireless_examples/bluetooth/ble_fscibb/reconfig.cmake
@@ -0,0 +1,11 @@
+mcux_add_configuration(
+    CC "-DgButtonSupported_d=1\
+       -DOSA_USED\
+       -DSDK_DEBUGCONSOLE_UART\
+       -DFSL_OSA_TASK_ENABLE=1\
+       -DCR_INTEGER_PRINTF\
+       -DCFG_BLE_PRJ=1\
+       -DHAL_FLASH_ROMAPI_DRIVER=1"
+)
+
+mcux_convert_binary(BINARY ${APPLICATION_BINARY_DIR}/${MCUX_SDK_PROJECT_NAME}.bin)
