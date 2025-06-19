52bab80014ecc0f48285f68920d824918c12f84e
Michal Princ <michal.princ@nxp.com>
2025-06-19 14:43:46 +0200

    [MCUX-79829] Refactor the sema4 driver example code to reduce the cyclomatic complexity and other HIS defects
    Signed-off-by: Michal Princ <michal.princ@nxp.com>

--- driver_examples/sema4/basic/sema4_basic.c
+++ driver_examples/sema4/basic/sema4_basic.c
@@ -13,15 +13,17 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define DEMO_CHECK(x)                                          \
-    if (!(x))                                                  \
-    {                                                          \
-        (void)PRINTF("Example error, line: %d\r\n", __LINE__); \
-        for (;;)                                               \
-        {                                                      \
-        }                                                      \
-    }
+static inline void demo_check(bool condition, int line)
+{
 
+    if (!condition)
+    {
+        (void)PRINTF("Example error, line: %d\r\n", line);
+        for (;;)
+        {
+        }
+    }
+}
 /*******************************************************************************
  * Variables
  ******************************************************************************/
@@ -33,6 +35,9 @@
 static void DEMO_SEMA4_Lock(void);
 static void DEMO_SEMA4_ResetGate(void);
 static void DEMO_SEMA4_ResetAllGate(void);
+static void DEMO_SEMA4_TestLockUnlock(uint8_t gateNum, bool isBlocking);
+static void DEMO_SEMA4_ResetSingleGate(uint8_t gateNum);
+static void DEMO_SEMA4_LockAllAndResetOne(uint8_t gateToReset);
 
 /*******************************************************************************
  * Code
@@ -60,14 +65,44 @@ int main(void)
     }
 }
 
+static void DEMO_SEMA4_TestLockUnlock(uint8_t gateNum, bool isBlocking) {
+    uint8_t i = 0U;
+    const char* method = isBlocking ? "blocking" : "non-blocking";
+    
+    (void)PRINTF("Lock with %s method\r\n", method);
+
+    if (isBlocking) {
+        (void)SEMA4_Lock(DEMO_SEMA4, gateNum, DEMO_PROC_NUM);
+    } else {
+        demo_check(SEMA4_TryLock(DEMO_SEMA4, gateNum, DEMO_PROC_NUM) == kStatus_Success, __LINE__);
+    }
+
+    /* Verify gate is locked */
+    demo_check(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == DEMO_PROC_NUM, __LINE__);
+
+    /* Check other gates are not locked */
+    for (i = 0U; i < gateNum; i++) {
+        demo_check(SEMA4_GetLockProc(DEMO_SEMA4, i) == -1, __LINE__);
+    }
+    for (i = gateNum + 1U; i < FSL_FEATURE_SEMA4_GATE_COUNT; i++) {
+        demo_check(SEMA4_GetLockProc(DEMO_SEMA4, i) == -1, __LINE__);
+    }
+
+    SEMA4_Unlock(DEMO_SEMA4, gateNum);
+
+    /* Verify gate is unlocked */
+    demo_check(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == -1, __LINE__);
+
+    (void)PRINTF("Gate lock&unlock success\r\n");
+}
+
 static void DEMO_SEMA4_Lock(void)
 {
     uint8_t gateNum = 0U;
-    uint8_t i       = 0U;
 
     (void)PRINTF("\r\n\r\nSEMA42 GATE LOCK AND UNLOCK\r\n");
 
-    DEMO_CHECK(kStatus_Success == SEMA4_ResetAllGates(DEMO_SEMA4));
+    demo_check(SEMA4_ResetAllGates(DEMO_SEMA4) == kStatus_Success, __LINE__);
 
     for (gateNum = 0U; gateNum < FSL_FEATURE_SEMA4_GATE_COUNT; gateNum++)
     {
@@ -76,123 +111,84 @@ static void DEMO_SEMA4_Lock(void)
         /*
          * Non-blocking lock.
          */
-        (void)PRINTF("Lock with non-blocking method\r\n");
-
-        DEMO_CHECK(kStatus_Success == SEMA4_TryLock(DEMO_SEMA4, gateNum, DEMO_PROC_NUM));
-
-        /* Now SEMA4 gate is locked. */
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == DEMO_PROC_NUM);
-
-        /* Check other gates are not locked by mistake. */
-        for (i = 0U; i < gateNum; i++)
-        {
-            DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, i) == -1);
-        }
-        for (i++; i < FSL_FEATURE_SEMA4_GATE_COUNT; i++)
-        {
-            DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, i) == -1);
-        }
-
-        SEMA4_Unlock(DEMO_SEMA4, gateNum);
-
-        /* Now SEMA4 gate is unlocked. */
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == -1);
-
-        (void)PRINTF("Gate lock&unlock success\r\n");
+        DEMO_SEMA4_TestLockUnlock(gateNum, false);
 
         /*
          * Blocking lock.
          */
-        (void)PRINTF("Lock with blocking method\r\n");
+        DEMO_SEMA4_TestLockUnlock(gateNum, true);
+    }
+}
 
-        (void)SEMA4_Lock(DEMO_SEMA4, gateNum, DEMO_PROC_NUM);
+static void DEMO_SEMA4_ResetSingleGate(uint8_t gateNum) 
+{
+    (void)PRINTF("Lock gate %d then reset it\r\n", gateNum);
+    (void)SEMA4_Lock(DEMO_SEMA4, gateNum, DEMO_PROC_NUM);
+    
+    /* Now SEMA4 gate is locked */
+    demo_check(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == DEMO_PROC_NUM, __LINE__);
+    
+    /* Reset the gate */
+    demo_check(SEMA4_ResetGate(DEMO_SEMA4, gateNum) == kStatus_Success, __LINE__);
+    
+    /* Now SEMA4 gate is unlocked */
+    demo_check(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == -1, __LINE__);
+}
 
-        /* Now SEMA4 gate is locked. */
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == DEMO_PROC_NUM);
+static void DEMO_SEMA4_LockAllAndResetOne(uint8_t gateToReset) 
+{
+    uint8_t i = 0U;
+    (void)PRINTF("Lock all gates then reset gate %d\r\n", gateToReset);
 
-        /* Check other gates are not locked by mistake. */
-        for (i = 0U; i < gateNum; i++)
-        {
-            DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, i) == -1);
-        }
-        for (i++; i < FSL_FEATURE_SEMA4_GATE_COUNT; i++)
-        {
-            DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, i) == -1);
-        }
+    /* Lock all gates */
+    for (i = 0U; i < FSL_FEATURE_SEMA4_GATE_COUNT; i++)
+    {
+        (void)SEMA4_Lock(DEMO_SEMA4, i, DEMO_PROC_NUM);
+        demo_check(SEMA4_GetLockProc(DEMO_SEMA4, i) == DEMO_PROC_NUM, __LINE__);
+    }
 
-        SEMA4_Unlock(DEMO_SEMA4, gateNum);
+    /* Reset the specified gate */
+    demo_check(SEMA4_ResetGate(DEMO_SEMA4, gateToReset) == kStatus_Success, __LINE__);
+    demo_check(SEMA4_GetLockProc(DEMO_SEMA4, gateToReset) == -1, __LINE__);
 
-        /* Now SEMA4 gate is unlocked. */
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == -1);
-
-        (void)PRINTF("Gate lock&unlock success\r\n");
+    /* Verify other gates remain locked */
+    for (i = 0U; i < gateToReset; i++)
+    {
+        demo_check(SEMA4_GetLockProc(DEMO_SEMA4, i) == DEMO_PROC_NUM, __LINE__);
+    }
+    for (i = gateToReset + 1U; i < FSL_FEATURE_SEMA4_GATE_COUNT; i++)
+    {
+        demo_check(SEMA4_GetLockProc(DEMO_SEMA4, i) == DEMO_PROC_NUM, __LINE__);
     }
 }
 
 static void DEMO_SEMA4_ResetGate(void)
 {
     uint8_t gateNum = 0U;
-    uint8_t i       = 0U;
 
     (void)PRINTF("\r\n\r\nSEMA42 GATE RESET\r\n");
+    demo_check(SEMA4_ResetAllGates(DEMO_SEMA4) == kStatus_Success, __LINE__);
 
-    DEMO_CHECK(kStatus_Success == SEMA4_ResetAllGates(DEMO_SEMA4));
-
+    /* Test resetting individual gates */
     for (gateNum = 0U; gateNum < FSL_FEATURE_SEMA4_GATE_COUNT; gateNum++)
     {
-        (void)PRINTF("Lock gate %d then reset it\r\n", gateNum);
-
-        (void)SEMA4_Lock(DEMO_SEMA4, gateNum, DEMO_PROC_NUM);
-
-        /* Now SEMA4 gate is locked. */
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == DEMO_PROC_NUM);
-
-        /* Reset the gate. */
-        DEMO_CHECK(kStatus_Success == SEMA4_ResetGate(DEMO_SEMA4, gateNum));
-
-        /* Now SEMA4 gate is unlocked. */
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == -1);
+        DEMO_SEMA4_ResetSingleGate(gateNum);
     }
 
+    /* Test locking all gates and resetting one at a time */
     for (gateNum = 0U; gateNum < FSL_FEATURE_SEMA4_GATE_COUNT; gateNum++)
     {
-        (void)PRINTF("Lock all gates then reset gate %d\r\n", gateNum);
-
-        /* Lock all gates. */
-        for (i = 0U; i < FSL_FEATURE_SEMA4_GATE_COUNT; i++)
-        {
-            (void)SEMA4_Lock(DEMO_SEMA4, i, DEMO_PROC_NUM);
-
-            DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, i) == DEMO_PROC_NUM);
-        }
-
-        /* Reset the gate. */
-        DEMO_CHECK(kStatus_Success == SEMA4_ResetGate(DEMO_SEMA4, gateNum));
-
-        /* Now SEMA4 gate is unlocked. */
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == -1);
-
-        /* Check other gate state to make sure no unlock by mistake. */
-        for (i = 0U; i < gateNum; i++)
-        {
-            DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, i) == DEMO_PROC_NUM);
-        }
-        for (i++; i < FSL_FEATURE_SEMA4_GATE_COUNT; i++)
-        {
-            DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, i) == DEMO_PROC_NUM);
-        }
-
-        DEMO_CHECK(kStatus_Success == SEMA4_ResetAllGates(DEMO_SEMA4));
+        DEMO_SEMA4_LockAllAndResetOne(gateNum);
+        demo_check(SEMA4_ResetAllGates(DEMO_SEMA4) == kStatus_Success, __LINE__);
     }
 }
-
 static void DEMO_SEMA4_ResetAllGate(void)
 {
     uint8_t gateNum = 0U;
 
     (void)PRINTF("\r\n\r\nSEMA42 ALL GATE RESET\r\n");
 
-    DEMO_CHECK(kStatus_Success == SEMA4_ResetAllGates(DEMO_SEMA4));
+    demo_check(SEMA4_ResetAllGates(DEMO_SEMA4) == kStatus_Success, __LINE__);
 
     for (gateNum = 0U; gateNum < FSL_FEATURE_SEMA4_GATE_COUNT; gateNum++)
     {
@@ -201,13 +197,13 @@ static void DEMO_SEMA4_ResetAllGate(void)
         (void)SEMA4_Lock(DEMO_SEMA4, gateNum, DEMO_PROC_NUM);
 
         /* Now SEMA4 gate is locked. */
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == DEMO_PROC_NUM);
+        demo_check(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == DEMO_PROC_NUM, __LINE__);
 
         /* Reset all gates. */
-        DEMO_CHECK(kStatus_Success == SEMA4_ResetAllGates(DEMO_SEMA4));
+        demo_check(SEMA4_ResetAllGates(DEMO_SEMA4) == kStatus_Success, __LINE__);
 
         /* Now SEMA4 gate is unlocked. */
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == -1);
+        demo_check(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == -1, __LINE__);
     }
 
     (void)PRINTF("Lock all gates and reset all\r\n");
@@ -217,15 +213,15 @@ static void DEMO_SEMA4_ResetAllGate(void)
         (void)SEMA4_Lock(DEMO_SEMA4, gateNum, DEMO_PROC_NUM);
 
         /* Now SEMA4 gate is locked. */
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == DEMO_PROC_NUM);
+        demo_check(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == DEMO_PROC_NUM, __LINE__);
     }
 
     /* Reset all the gates. */
-    DEMO_CHECK(kStatus_Success == SEMA4_ResetAllGates(DEMO_SEMA4));
+    demo_check(SEMA4_ResetAllGates(DEMO_SEMA4) == kStatus_Success, __LINE__);
 
     /* Check the gate status, should be unlocked. */
     for (gateNum = 0U; gateNum < FSL_FEATURE_SEMA4_GATE_COUNT; gateNum++)
     {
-        DEMO_CHECK(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == -1);
+        demo_check(SEMA4_GetLockProc(DEMO_SEMA4, gateNum) == -1, __LINE__);
     }
 }
