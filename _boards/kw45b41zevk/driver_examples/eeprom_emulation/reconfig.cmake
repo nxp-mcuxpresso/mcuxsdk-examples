c1c80716314a0740f4d0caa6e336aa5b611baa86
Allen Zhang <chunfeng.zhang@nxp.com>
2025-06-26 17:29:07 +0800

    [MCUX-77682] Updated the linker file for eeprom_emulation example
    - Shall use the RAM linker file to run the example
    
    Signed-off-by: AllenZhang <chunfeng.zhang@nxp.com>

--- _boards/kw45b41zevk/driver_examples/eeprom_emulation/reconfig.cmake
+++ _boards/kw45b41zevk/driver_examples/eeprom_emulation/reconfig.cmake
@@ -14,3 +14,26 @@ mcux_remove_armgcc_configuration(
     TARGETS debug
     CC "-O0"
 )
+
+mcux_remove_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/Wireless/KW/KW45B41Z83/iar/KW45B41Z83xxxA_flash.icf
+)
+mcux_remove_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/Wireless/KW/KW45B41Z83/gcc/KW45B41Z83xxxA_flash.ld
+)
+
+
+mcux_add_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/Wireless/KW/KW45B41Z83/iar/KW45B41Z83xxxA_ram.icf
+)
+mcux_add_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${device_root}/Wireless/KW/KW45B41Z83/gcc/KW45B41Z83xxxA_ram.ld
+)
