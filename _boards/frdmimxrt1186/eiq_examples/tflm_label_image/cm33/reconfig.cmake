4c602588e92d8f9a35f1ac3aa7e1661b9a83aedc
Xiaodong Xie <xiaodong.xie@nxp.com>
2025-07-09 20:00:00 +0800

    [AIR-12324] update TFLM examples for frdmimxrt1186
    - Enable tflm_lib.
    - Update memory config.
    - Update the wrong model_data for label_image and kws.
    
    Signed-off-by: Xiaodong Xie <xiaodong.xie@nxp.com>

--- _boards/frdmimxrt1186/eiq_examples/tflm_label_image/cm33/reconfig.cmake
+++ _boards/frdmimxrt1186/eiq_examples/tflm_label_image/cm33/reconfig.cmake
@@ -4,11 +4,48 @@ mcux_add_include(
   )
 mcux_add_source(
   BASE_PATH ${SdkRootDirPath}
-  SOURCES examples/eiq_examples/tflm_cifar10/pcq/model_data.h
-  examples/eiq_examples/tflm_cifar10/pcq/model_cifarnet_ops_micro.cpp
+  SOURCES examples/eiq_examples/tflm_label_image/pcq/model_data.h
+  examples/eiq_examples/tflm_label_image/pcq/model_mobilenet_ops_micro.cpp
+
   )
 mcux_remove_mdk_configuration(
   CC "-fshort-wchar"
   CX "-fshort-wchar"
+  )
+
+mcux_remove_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    CC "-DXIP_BOOT_HEADER_ENABLE=0"
+)
+
+mcux_remove_mdk_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    LD "--predefine=-DXIP_BOOT_HEADER_ENABLE=0"
+)
+
+mcux_add_macro(
+    CC "-DUSE_HYPERRAM=1"
+)
+
+mcux_add_configuration(
+    TARGETS flexspi_nor_debug           flexspi_nor_release
+            flexspi_nor_hyperram_debug  flexspi_nor_hyperram_release
+    CC "-DXIP_BOOT_HEADER_ENABLE=1"
+)
+
+mcux_add_iar_configuration(
+  LD "--config_def=__heap_size__=0x8000\
+  --config_def=__stack_size__=0x2000"
+  )
+mcux_add_mdk_configuration(
+  LD "--predefine=-D__heap_size__=0x8000\
+      --predefine=-DXIP_BOOT_HEADER_ENABLE=1\
+      --predefine=-D__stack_size__=0x2000"
+  )
+mcux_add_armgcc_configuration(
+  LD "-Xlinker --defsym=__heap_size__=0x8000\
+  -Xlinker --defsym=__stack_size__=0x2000"
   )
 
