97a4ddc9e8d4f296eb901b101d329ad96eda08f6
Vit Valek <vit.valek@nxp.com>
2025-07-14 13:48:59 +0200

    Revert "[IECM-1710] Safety code temporary commented to avoid build errors on frdmrw612."
    This reverts commit cec346a2f9ac9f593acca448037cae7af5fec512.

--- _boards/frdmrw612/demo_apps/safety_iec60730b/safety_test_items.c
+++ _boards/frdmrw612/demo_apps/safety_iec60730b/safety_test_items.c
@@ -18,24 +18,24 @@
  *******************************************************************************/
 
 //P0_21 (ARD_D4)    J1-10
-//fs_dio_test_t dio_safety_test_item_0 = {
-//    .gpio          = GPIO0_BASE_NS,
-//    .pinNum        = 21U,
-//    .pinDir        = PIN_DIRECTION_IN,
-//    .pcr           = (uint32_t)PORT0_NS->PCR,
-//};
-//
-////P0_22 (ARD_D7)    J1-16
-//fs_dio_test_t dio_safety_test_item_1 = {
-//    .gpio          = GPIO0_BASE_NS,
-//    .pinNum        = 22U,
-//    .pinDir        = PIN_DIRECTION_IN,
-//    .pcr           = (uint32_t)PORT0_NS->PCR,
-//};
+fs_dio_test_t dio_safety_test_item_0 = {
+    .gpio          = GPIO0_BASE_NS,
+    .pinNum        = 21U,
+    .pinDir        = PIN_DIRECTION_IN,
+    .pcr           = (uint32_t)PORT0_NS->PCR,
+};
+
+//P0_22 (ARD_D7)    J1-16
+fs_dio_test_t dio_safety_test_item_1 = {
+    .gpio          = GPIO0_BASE_NS,
+    .pinNum        = 22U,
+    .pinDir        = PIN_DIRECTION_IN,
+    .pcr           = (uint32_t)PORT0_NS->PCR,
+};
 
 
 /* NULL terminated array of pointers to dio_test_t items for safety DIO test */
-//fs_dio_test_t *g_dio_safety_test_items[] = {&dio_safety_test_item_0, &dio_safety_test_item_1, NULL};
+fs_dio_test_t *g_dio_safety_test_items[] = {&dio_safety_test_item_0, &dio_safety_test_item_1, NULL};
 
 
 /*******************************************************************************
@@ -43,10 +43,6 @@
  *                           STRUCTURE FOR AIO TEST                             *
  *                                                                              *
  *******************************************************************************/
-#define ADC_DEVIATION_PERCENT 1
-#define ADC_RESOLUTION 1
-#define ADC_BANDGAP_LEVEL 1
-#define ADC_REFERENCE 1
 #define ADC_MAX                 ((1 << (ADC_RESOLUTION)) - 1)
 #define ADC_BANDGAP_LEVEL_RAW   (((ADC_BANDGAP_LEVEL) * (ADC_MAX)) / (ADC_REFERENCE))
 #define ADC_MIN_LIMIT(val)      (uint16_t)(((val) * (100 - ADC_DEVIATION_PERCENT)) / 100)
