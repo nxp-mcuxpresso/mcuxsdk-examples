afad6dfb6646e16febedd04eed53cfdb908e7b06
David Jurajda <david.jurajda@nxp.com>
2025-07-23 10:56:49 +0200

    [MCUX-77540][littlefs_shell][evkmimxrt1180] Enable littlefs_shell on evkmimxrt1180. Signed-off-by: David Jurajda <david.jurajda@nxp.com>

--- _boards/evkmimxrt1180/littlefs_examples/littlefs_shell/cm33/reconfig.cmake
+++ _boards/evkmimxrt1180/littlefs_examples/littlefs_shell/cm33/reconfig.cmake
@@ -14,8 +14,3 @@ mcux_add_macro(
     CC "-DLFS_NO_INTRINSICS=1\
        -DSDK_DEBUGCONSOLE_UART"
 )
-mcux_add_macro(
-    TOOLCHAINS armgcc iar
-    TARGETS hyperram_release release sdram_release
-    CC "-DLFS_NO_ASSERT"
-)
