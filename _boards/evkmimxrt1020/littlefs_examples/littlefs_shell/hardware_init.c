f50d0015f60b51713c1cd9b7a2ef421439920dc8
David Jurajda <david.jurajda@nxp.com>
2025-07-23 10:48:11 +0200

    [MCUX-77540][littlefs_shell][evkmimxrt1020] Enable littlefs_shell on evkmimxrt1020. Signed-off-by: David Jurajda <david.jurajda@nxp.com>

--- _boards/evkmimxrt1020/littlefs_examples/littlefs_shell/hardware_init.c
+++ _boards/evkmimxrt1020/littlefs_examples/littlefs_shell/hardware_init.c
@@ -21,13 +21,7 @@ void BOARD_InitHardware(void)
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
-
-    const clock_usb_pll_config_t g_ccmConfigUsbPll = {.loopDivider = 0U};
-
-    CLOCK_InitUsb1Pll(&g_ccmConfigUsbPll);
-    CLOCK_InitUsb1Pfd(kCLOCK_Pfd0, 24);   /* Set PLL3 PFD0 clock 360MHZ. */
-    CLOCK_SetMux(kCLOCK_FlexspiMux, 0x3); /* Choose PLL3 PFD0 clock as flexspi source clock. */
-    CLOCK_SetDiv(kCLOCK_FlexspiDiv, 2);   /* flexspi clock 120M. */
+    flexspi_clock_init();
 }
 
 /*${function:end}*/
--- _boards/evkmimxrt1020/littlefs_examples/littlefs_shell/reconfig.cmake
+++ _boards/evkmimxrt1020/littlefs_examples/littlefs_shell/reconfig.cmake
@@ -8,15 +8,51 @@ mcux_add_macro(
     CC "-DLFS_NO_INTRINSICS=1\
        -DSDK_DEBUGCONSOLE_UART\
        -DSKIP_SYSCLK_INIT\
-       -DXIP_BOOT_HEADER_DCD_ENABLE=1"
+       -DXIP_BOOT_HEADER_DCD_ENABLE=1\
+       -DXIP_EXTERNAL_FLASH=1"
 )
 
 mcux_add_macro(
     TOOLCHAINS armgcc iar
-    TARGETS hyperram_release release sdram_release
+    TARGETS hyperram_release release sdram_release flexspi_nor_release
     CC "-DLFS_NO_ASSERT"
 )
 
 mcux_add_mdk_configuration(
     LD "--diag_suppress=L6329W"
+)
+
+# Add or remove Linker File Configurations
+mcux_remove_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${device_root}/RT/RT1020/MIMXRT1021/iar/MIMXRT1021xxxxx_flexspi_nor.icf
+)
+mcux_remove_mdk_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${device_root}/RT/RT1020/MIMXRT1021/arm/MIMXRT1021xxxxx_flexspi_nor.scf
+)
+mcux_remove_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${device_root}/RT/RT1020/MIMXRT1021/gcc/MIMXRT1021xxxxx_flexspi_nor.ld
+)
+
+
+# Add or remove Linker File Configurations
+mcux_add_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${board_root}/${board}/littlefs_examples/littlefs_shell/linker/MIMXRT1021xxxxx_flexspi_nor.icf
+)
+mcux_add_mdk_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${board_root}/${board}/littlefs_examples/littlefs_shell/linker/MIMXRT1021xxxxx_flexspi_nor.scf
+)
+mcux_add_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${board_root}/${board}/littlefs_examples/littlefs_shell/linker/MIMXRT1021xxxxx_flexspi_nor.ld
 )
