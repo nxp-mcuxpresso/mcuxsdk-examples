63d4f35d22183fae224c60264d253a1166009941
Axel Le Bourhis <axel.lebourhis@nxp.com>
2025-07-24 10:29:05 +0200

    [MCSE-936][mcxw23] Enable IPM HCI transport
    Use the IPM (Inter Process Message) HCI transport implementation.
    Also enable the gUseHciTransportDownward_d macro.
    
    This enables the HCI transport abstracted over the framework PLATFORM
    API, which is a preliminary requirement for Zephyr support.
    
    Signed-off-by: Axel Le Bourhis <axel.lebourhis@nxp.com>

--- _boards/mcxw23evk/wireless_examples/bluetooth/beacon/reconfig.cmake
+++ _boards/mcxw23evk/wireless_examples/bluetooth/beacon/reconfig.cmake
@@ -5,6 +5,7 @@ mcux_add_configuration(
        -DFSL_OSA_TASK_ENABLE=1\
        -DCR_INTEGER_PRINTF\
        -DCFG_BLE_PRJ=1\
+       -DgUseHciTransportDownward_d=1\
        -DHAL_FLASH_ROMAPI_DRIVER=1"
 )
 
