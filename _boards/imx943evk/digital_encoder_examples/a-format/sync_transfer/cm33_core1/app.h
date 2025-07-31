e3b54085d5d12ac7dbdd6b3253673af7bc0f74fb
JianchaoWang <Jianchao.wang_1@nxp.com>
2025-07-31 14:12:20 +0800

    [INDLINUX-4914] Added the A-format example with sync mode for the imx943evk board
    Signed-off-by: JianchaoWang <Jianchao.wang_1@nxp.com>

---
+++ _boards/imx943evk/digital_encoder_examples/a-format/sync_transfer/cm33_core1/app.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _APP_H_
+#define _APP_H_
+
+#include "board.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define BOARD_FLEXIO_BASE      FLEXIO1
+
+#define FLEXIO_A_FORMAT_DR_PIN    1U
+#define FLEXIO_A_FORMAT_TX_PIN    2U
+#define FLEXIO_A_FORMAT_RX_PIN    3U
+
+#define A_FORMAT_TX_SHIFTER_INDEX 0U
+#define A_FORMAT_RX_SHIFTER_INDEX 1U
+#define A_FORMAT_DR_TIMER_INDEX   0U
+#define A_FORMAT_TX_TIMER_INDEX   1U
+#define A_FORMAT_RX_TIMER_INDEX   2U
+#define DEMO_FLEXIO_CLOCK         (kCLOCK_Flexio1)
+#define FLEXIO_CLOCK_FREQUENCY    CLOCK_GetRate(DEMO_FLEXIO_CLOCK)
+/*${macro:end}*/
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */
