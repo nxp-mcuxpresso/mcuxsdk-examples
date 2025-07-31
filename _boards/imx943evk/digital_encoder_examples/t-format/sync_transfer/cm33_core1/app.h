9efd22d96290cff0d0a4cdd675487ca7f6cbc6c6
JianchaoWang <Jianchao.wang_1@nxp.com>
2025-07-31 14:03:33 +0800

    [INDLINUX-4915] Added the T-format example with sync mode for the imx943evk board
    Signed-off-by: JianchaoWang <Jianchao.wang_1@nxp.com>

---
+++ _boards/imx943evk/digital_encoder_examples/t-format/sync_transfer/cm33_core1/app.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+#ifndef _APP_H_
+#define _APP_H_
+
+#include "board.h"
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define BOARD_FLEXIO_BASE      FLEXIO1
+
+#define FLEXIO_T_FORMAT_DR_PIN    1U
+#define FLEXIO_T_FORMAT_TX_PIN    2U
+#define FLEXIO_T_FORMAT_RX_PIN    3U
+
+#define T_FORMAT_TX_SHIFTER_INDEX 0U
+#define T_FORMAT_RX_SHIFTER_INDEX 1U
+#define T_FORMAT_DR_TIMER_INDEX   0U
+#define T_FORMAT_TX_TIMER_INDEX   1U
+#define T_FORMAT_RX_TIMER_INDEX   2U
+#define DEMO_DFS_DIV2_CLOCK       (kCLOCK_Syspll1dfs0div2)
+#define DEMO_FLEXIO_CLOCK         (kCLOCK_Flexio1)
+#define FLEXIO_CLOCK_FREQUENCY    CLOCK_GetRate(DEMO_FLEXIO_CLOCK)
+/*${macro:end}*/
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */
---
+++ _boards/imx943evk/digital_encoder_examples/t-format/sync_transfer/cm33_core1/hardware_init.c
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*${header:start}*/
+#include "app.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+/*${header:end}*/
+
+/*${function:start}*/
+void BOARD_InitHardware(void)
+{
+    /* clang-format off */
+    clk_t flexioClkCfg = {
+        .clkId = DEMO_FLEXIO_CLOCK,
+        .pclkId = kCLOCK_Syspll1dfs1div2, /* 400 MHz */
+        .clkRoundOpt = SCMI_CLOCK_ROUND_AUTO,
+        .rate = 80000000UL, /* 80MHz */
+    };
+    /* clang-format on */
+
+    SystemPlatformInit();
+    BOARD_InitDebugConsolePins();
+    BOARD_InitBootPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+
+    CLOCK_SetParent(&flexioClkCfg);
+    CLOCK_SetRate(&flexioClkCfg);
+    CLOCK_EnableClock(flexioClkCfg.clkId);
+}
+/*${function:end}*/
