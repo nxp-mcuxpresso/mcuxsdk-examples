597f03efcef2b55cef4a5c47adadd1c395bcc8bb
Yangbo Lu <yangbo.lu@nxp.com>
2025-07-31 15:35:29 +0800

    [MCUX-80906] boards: imx943evk: netc_share: fix package build issue
    Fixed package build issue for netc_share.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- _boards/imx943evk/demo_apps/netc_share/reconfig.cmake
+++ _boards/imx943evk/demo_apps/netc_share/reconfig.cmake
@@ -8,19 +8,10 @@ mcux_add_source(
     BASE_PATH ${SdkRootDirPath}
     SOURCES
     ${board_root}/${board}/demo_apps/netc_share/cm33_core1/lwip_netc_port.h
-    ${board_root}/${board}/cm33_core1/app_srtm.c
-    ${board_root}/${board}/cm33_core1/app_srtm.h
-    ${board_root}/${board}/cm33_core1/rpmsg_config.h
-    ${board_root}/${board}/rsc_table.c
-    ${board_root}/${board}/rsc_table.h
-    ${board_root}/${board}/srtm_config.h
-    middleware/multicore/remoteproc/remoteproc.h
 )
 
 mcux_add_include(
     BASE_PATH ${SdkRootDirPath}
     INCLUDES
     ${board_root}/${board}/demo_apps/netc_share/cm33_core1
-    ${board_root}/${board}/
-    middleware/multicore/remoteproc
 )
