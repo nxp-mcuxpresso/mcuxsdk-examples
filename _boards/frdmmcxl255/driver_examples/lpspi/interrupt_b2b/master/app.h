99b217c9c7e5c7efee564e426bd5245cdcda08c4
Michal Chromec <michal.chromec@nxp.com>
2025-08-01 16:52:28 +0200

    [MCUX-81047] Fixed instance of lpspi
    Signed-off-by: Michal Chromec <michal.chromec@nxp.com>

--- _boards/frdmmcxl255/driver_examples/lpspi/interrupt_b2b/master/app.h
+++ _boards/frdmmcxl255/driver_examples/lpspi/interrupt_b2b/master/app.h
@@ -14,7 +14,7 @@
 #define EXAMPLE_LPSPI_MASTER_BASEADDR         (LPSPI1)
 #define EXAMPLE_LPSPI_MASTER_IRQN             (LPSPI1_IRQn)
 #define EXAMPLE_LPSPI_DEALY_COUNT             0xFFFFF
-#define LPSPI_MASTER_CLK_FREQ                 (CLOCK_GetLpspiClkFreq(0))
+#define LPSPI_MASTER_CLK_FREQ                 (CLOCK_GetLpspiClkFreq(1U))
 #define EXAMPLE_LPSPI_MASTER_PCS_FOR_INIT     (kLPSPI_Pcs0)
 #define EXAMPLE_LPSPI_MASTER_PCS_FOR_TRANSFER (kLPSPI_MasterPcs0)
 #define EXAMPLE_LPSPI_MASTER_IRQHandler       (LPSPI1_IRQHandler)
