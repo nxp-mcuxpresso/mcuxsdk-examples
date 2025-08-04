2b6af1feeba12b51f27ff4cc35a0487ff00bb01e
Albort Xue <yao.xue@nxp.com>
2025-08-04 18:30:25 +0800

    [MCUX-81699] [kw47evk][kw47loc][mcxw72evk][frdmmcxw72] Updated hardware_init to achieve lower power consumption.
    - Added spc configurations;
    - Added VBAT configurations;
    - Added RFMC configurations.
    
    Signed-off-by: Albort Xue <yao.xue@nxp.com>

--- _boards/mcxw72evk/demo_apps/power_manager_test/bm/cm33_core0/hardware_init.c
+++ _boards/mcxw72evk/demo_apps/power_manager_test/bm/cm33_core0/hardware_init.c
@@ -19,6 +19,7 @@
 #include "fsl_port.h"
 #include "fsl_cmc.h"
 #include "fsl_spc.h"
+#include "fsl_vbat.h"
 
 /*${header:end}*/
 
@@ -43,6 +44,9 @@ void LPTMR0_IRQHandler(void)
     PM_TriggerWakeSourceService(&g_lptmr0WakeupSource);
 }
 
+#define APP_SPC  (SPC0)
+#define APP_VBAT (VBAT0)
+
 void BOARD_InitHardware(void)
 {
     BOARD_InitPins();
@@ -53,9 +57,89 @@ void BOARD_InitHardware(void)
     {
         /* Close ISO. */
         SPC_ClearPeriphIOIsolationFlag(SPC0);
-    }    
-    
+    }
+
     CMC_EnableDebugOperation(CMC0, false);
+
+    static volatile status_t status = kStatus_Success;
+
+    /* Disable LVDs and HVDs in Active mode. */
+    SPC_EnableActiveModeCoreHighVoltageDetect(APP_SPC, false);
+    SPC_EnableActiveModeCoreLowVoltageDetect(APP_SPC, false);
+    SPC_EnableActiveModeSystemHighVoltageDetect(APP_SPC, false);
+    SPC_EnableActiveModeSystemLowVoltageDetect(APP_SPC, false);
+    SPC_EnableActiveModeIOHighVoltageDetect(APP_SPC, false);
+    SPC_EnableActiveModeIOLowVoltageDetect(APP_SPC, false);
+
+    while (SPC_GetBusyStatusFlag(APP_SPC))
+    {
+    }
+
+    spc_active_mode_regulators_config_t activeModeRegulatorOption;
+    activeModeRegulatorOption.bandgapMode                      = kSPC_BandgapEnabledBufferDisabled;
+    activeModeRegulatorOption.lpBuff                           = false;
+    activeModeRegulatorOption.DCDCOption.DCDCVoltage           = kSPC_DCDC_NormalVoltage; /* DCDC regulate to 1.5V. */
+    activeModeRegulatorOption.DCDCOption.DCDCDriveStrength     = kSPC_DCDC_NormalDriveStrength;
+    activeModeRegulatorOption.SysLDOOption.SysLDOVoltage       = kSPC_SysLDO_NormalVoltage;
+    activeModeRegulatorOption.SysLDOOption.SysLDODriveStrength = kSPC_SysLDO_NormalDriveStrength;
+    activeModeRegulatorOption.CoreLDOOption.CoreLDOVoltage =
+        kSPC_CoreLDO_NormalVoltage; /* Core LDO regulate to 1.1V. */
+#if defined(FSL_FEATURE_SPC_HAS_CORELDO_VDD_DS) && FSL_FEATURE_SPC_HAS_CORELDO_VDD_DS
+    activeModeRegulatorOption.CoreLDOOption.CoreLDODriveStrength = kSPC_CoreLDO_NormalDriveStrength;
+#endif /* FSL_FEATURE_SPC_HAS_CORELDO_VDD_DS */
+    if (SPC_SetActiveModeDCDCRegulatorConfig(APP_SPC, &activeModeRegulatorOption.DCDCOption) != kStatus_Success)
+    {
+        PRINTF("Fail to set DCDC in Active Mode!\r\n");
+    }
+    while (SPC_GetBusyStatusFlag(APP_SPC))
+    {
+    }
+    if (SPC_SetActiveModeSystemLDORegulatorConfig(APP_SPC, &activeModeRegulatorOption.SysLDOOption) != kStatus_Success)
+    {
+        PRINTF("Fail to set System LDO in Active Mode!\r\n ");
+    }
+    if (SPC_SetActiveModeBandgapModeConfig(APP_SPC, activeModeRegulatorOption.bandgapMode) != kStatus_Success)
+    {
+        PRINTF("Fail to set bandgap mode in Active Mode!\r\n");
+    }
+    status = SPC_SetActiveModeCoreLDORegulatorConfig(APP_SPC, &activeModeRegulatorOption.CoreLDOOption);
+    if (status != kStatus_Success)
+    {
+        PRINTF("Fail to set Core LDO in Active mode!\r\n");
+    }
+    SPC_EnableActiveModeCMPBandgapBuffer(APP_SPC, activeModeRegulatorOption.lpBuff);
+
+    spc_lowpower_mode_regulators_config_t lowPowerRegulatorOption;
+
+    lowPowerRegulatorOption.lpIREF                             = false;
+    lowPowerRegulatorOption.bandgapMode                        = kSPC_BandgapDisabled;
+    lowPowerRegulatorOption.lpBuff                             = false;
+    lowPowerRegulatorOption.CoreIVS                            = false;
+    lowPowerRegulatorOption.DCDCOption.DCDCVoltage             = kSPC_DCDC_LowUnderVoltage;
+    lowPowerRegulatorOption.DCDCOption.DCDCDriveStrength       = kSPC_DCDC_LowDriveStrength;
+    lowPowerRegulatorOption.SysLDOOption.SysLDODriveStrength   = kSPC_SysLDO_LowDriveStrength;
+    lowPowerRegulatorOption.CoreLDOOption.CoreLDOVoltage       = kSPC_CoreLDO_MidDriveVoltage;
+    lowPowerRegulatorOption.CoreLDOOption.CoreLDODriveStrength = kSPC_CoreLDO_LowDriveStrength;
+
+    status = SPC_SetLowPowerModeRegulatorsConfig(APP_SPC, &lowPowerRegulatorOption);
+    if (status != kStatus_Success)
+    {
+        PRINTF("Fail to set regulators in Low Power Mode.");
+        return;
+    }
+
+    SPC_SetLowPowerWakeUpDelay(APP_SPC, 0xFFFFU);
+
+    VBAT_EnableBackupSRAMRegulator(APP_VBAT, false);
+    VBAT_EnableFRO16k(APP_VBAT, false);
+    while (VBAT_CheckFRO16kEnabled(APP_VBAT))
+        ;
+
+    /* Isolate some external power domain to save power. */
+    SPC_SetExternalVoltageDomainsConfig(APP_SPC, 0x7U, 0x0U);
+
+    /* Enable CORE VDD Voltage scaling. */
+    SPC_EnableLowPowerModeCoreVDDInternalVoltageScaling(APP_SPC, true);
 }
 
 status_t APP_EccReInitCallback(pm_event_type_t eventType, uint8_t powerState, void *data)
@@ -99,6 +183,11 @@ status_t APP_UartControlCallback(pm_event_type_t eventType, uint8_t powerState, 
          * Debug console TX pin: Don't need to change.
          */
         PORT_SetPinMux(DEBUG_CONSOLE_RX_PORT, DEBUG_CONSOLE_RX_PIN, kPORT_PinDisabledOrAnalog);
+
+        /* Set NBU into Deep Sleep Mode */
+        RFMC->RF2P4GHZ_CTRL =
+            (RFMC->RF2P4GHZ_CTRL & (~RFMC_RF2P4GHZ_CTRL_LP_MODE_MASK)) | RFMC_RF2P4GHZ_CTRL_LP_MODE(0x3);
+        RFMC->RF2P4GHZ_CTRL |= RFMC_RF2P4GHZ_CTRL_LP_ENTER_MASK;
     }
     else
     {
