c7553e18413c6837e8aa94c96e3b1efa48d71524
Yangbo Lu <yangbo.lu@nxp.com>
2025-08-06 16:52:12 +0800

    [MCUX-81940] boards: evkmimxrt595: increase stack size for mmccard_fatfs
    Increased stack size for mmccard_fatfs.
    
    Signed-off-by: Yangbo Lu <yangbo.lu@nxp.com>

--- _boards/evkmimxrt595/sdmmc_examples/mmccard_fatfs/reconfig.cmake
+++ _boards/evkmimxrt595/sdmmc_examples/mmccard_fatfs/reconfig.cmake
@@ -6,3 +6,7 @@ mcux_add_source(
             ${board_root}/${board}/sdmmc_config.c
             ${board_root}/${board}/sdmmc_config.h
 )
+
+mcux_add_linker_symbol(
+      SYMBOLS "__stack_size__=0x1000 __heap_size__=0x1000"
+)
