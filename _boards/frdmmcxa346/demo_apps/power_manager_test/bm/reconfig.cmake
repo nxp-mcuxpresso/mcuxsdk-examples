f11f2dea3450eb5c776a3824a984f52c8d8e2090
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2025-08-13 14:34:57 +0800

    [MCUX-82123] Enable power manager for MCXA346 and MCXA366
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- _boards/frdmmcxa346/demo_apps/power_manager_test/bm/reconfig.cmake
+++ _boards/frdmmcxa346/demo_apps/power_manager_test/bm/reconfig.cmake
@@ -12,47 +12,47 @@ mcux_add_macro(
 mcux_remove_iar_linker_script(
     BASE_PATH ${SdkRootDirPath}
     TARGETS debug release
-    LINKER devices/MCX/MCXA/MCXA366/iar/MCXA366_flash.icf
+    LINKER devices/MCX/MCXA/MCXA346/iar/MCXA346_flash.icf
 )
 
 mcux_remove_armgcc_linker_script(
     BASE_PATH ${SdkRootDirPath}
     TARGETS debug release
-    LINKER devices/MCX/MCXA/MCXA366/gcc/MCXA366_flash.ld
+    LINKER devices/MCX/MCXA/MCXA346/gcc/MCXA346_flash.ld
 )
 
 mcux_remove_mdk_linker_script(
     BASE_PATH ${SdkRootDirPath}
     TARGETS debug release
-    LINKER devices/MCX/MCXA/MCXA366/arm/MCXA366_flash.scf
+    LINKER devices/MCX/MCXA/MCXA346/arm/MCXA346_flash.scf
 )
 
 # Add Linker File Configurations
 mcux_add_iar_linker_script(
     BASE_PATH ${SdkRootDirPath}
     TARGETS debug release
-    LINKER examples/_boards/${board}/demo_apps/power_manager_test/linker/MCXA366_power_manager_flash.icf
+    LINKER examples/_boards/${board}/demo_apps/power_manager_test/linker/MCXA346_power_manager_flash.icf
 )
 mcux_add_armgcc_linker_script(
     BASE_PATH ${SdkRootDirPath}
     TARGETS debug release
-    LINKER examples/_boards/${board}/demo_apps/power_manager_test/linker/MCXA366_power_manager_flash.ld
+    LINKER examples/_boards/${board}/demo_apps/power_manager_test/linker/MCXA346_power_manager_flash.ld
 )
 mcux_add_mdk_linker_script(
     BASE_PATH ${SdkRootDirPath}
     TARGETS debug release
-    LINKER examples/_boards/${board}/demo_apps/power_manager_test/linker/MCXA366_power_manager_flash.scf
+    LINKER examples/_boards/${board}/demo_apps/power_manager_test/linker/MCXA346_power_manager_flash.scf
 )
 
 mcux_project_remove_source(
     BASE_PATH ${SdkRootDirPath}
-    SOURCES devices/MCX/MCXA/MCXA366/startup_MCXA366.c
+    SOURCES devices/MCX/MCXA/MCXA346/startup_MCXA346.c
     TOOLCHAINS armgcc
 )
 
 mcux_add_source(
     BASE_PATH ${SdkRootDirPath}
-    SOURCES devices/MCX/MCXA/MCXA366/startup_MCXA366.c
+    SOURCES devices/MCX/MCXA/MCXA346/startup_MCXA346.c
     EXCLUDE TRUE
     TOOLCHAINS armgcc
 )
