8e9d1d20e08093cec9ed2d058ca2dbd6ac28055f
Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
2025-08-14 15:50:32 +0200

    [MCSE-1053][kw47loc] Revert debug console enablement on kw47loc
    kw47loc board has an issue on the second UART RX line.

--- _boards/kw47loc/wireless_examples/reference_design/lowpower_peripheral/app_preinclude.h
+++ _boards/kw47loc/wireless_examples/reference_design/lowpower_peripheral/app_preinclude.h
@@ -44,7 +44,7 @@
  ***********************************************************************************/
 
 /*! Enable Debug Console (PRINTF) */
-#define gDebugConsoleEnable_d           1
+#define gDebugConsoleEnable_d           0
 
 /*! Enable Panic logs through PRINTF */
 #define PANIC_ENABLE_LOG                1
