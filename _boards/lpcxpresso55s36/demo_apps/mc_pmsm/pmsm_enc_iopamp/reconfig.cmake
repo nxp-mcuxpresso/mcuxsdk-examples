a095c224b871c7aa8561ab8e63fefbf1acfc0c5e
Vit Valek <vit.valek@nxp.com>
2025-08-14 13:42:19 +0200

    [MCROZ-1531] Driver for online update parameters removed in dual motor and iopamp examples.
    Signed-off-by: Vit Valek <vit.valek@nxp.com>

--- _boards/lpcxpresso55s36/demo_apps/mc_pmsm/pmsm_enc_iopamp/reconfig.cmake
+++ _boards/lpcxpresso55s36/demo_apps/mc_pmsm/pmsm_enc_iopamp/reconfig.cmake
@@ -12,7 +12,19 @@ mcux_add_armgcc_configuration(
   CC "-Wno-uninitialized"
   )
 
-  mcux_add_iar_configuration(
-      LD "--config_def=__stack_size__=0x2000\
-          --config_def=__heap_size__=0x1000"
-  )
+mcux_add_iar_configuration(
+  LD "--config_def=__stack_size__=0x2000\
+	  --config_def=__heap_size__=0x1000"
+)
+
+# Remove MC flash driver - not available in pmsm_enc_iopamp motor application
+mcux_project_remove_source(
+  BASE_PATH ${SdkRootDirPath}
+  SOURCES middleware/motor_control/pmsm/pmsm_float/mc_drivers/mcdrv_flash_lpc55s36.h
+          middleware/motor_control/pmsm/pmsm_float/mc_drivers/mcdrv_flash_lpc55s36.c
+)
+
+# Remove MC flash driver macro - not available in pmsm_enc_iopamp motor application
+mcux_remove_macro(
+  CC "-DMCDRV_FLASH_LPC55S36"
+)
