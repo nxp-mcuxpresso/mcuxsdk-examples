956cfe44ee0827551316987ab09da897179216db
Eva Klejmova <eva.klejmova@nxp.com>
2025-08-21 13:34:06 +0200

    [MCUX-77487][evkbmimxrt1060][sai_edma_ping_pong_buffer_half_interrupt] Fixed codec macro
    Signed-off-by: Eva Klejmova <eva.klejmova@nxp.com>

--- _boards/evkbmimxrt1060/driver_examples/sai/edma_ping_pong_buffer_half_interrupt/app.h
+++ _boards/evkbmimxrt1060/driver_examples/sai/edma_ping_pong_buffer_half_interrupt/app.h
@@ -20,10 +20,10 @@
 /*${macro:start}*/
 /* SAI instance and clock */
 #ifndef DEMO_CODEC_WM8960
-#define DEMO_CODEC_WM8960 0
+#define DEMO_CODEC_WM8960 1
 #endif
 #ifndef DEMO_CODEC_CS42448
-#define DEMO_CODEC_CS42448 1
+#define DEMO_CODEC_CS42448 0
 #endif
 #if DEMO_CODEC_WM8960 && DEMO_CODEC_CS42448
 #error "Duplicate codec defined"
