11592d469fce3127a36225343822ee9e223928b7
Alex Porosanu <alexandru.porosanu@nxp.com>
2025-08-26 18:06:45 +0300

    [MCZB-2944] rw612: wireless_examples: zigbee: router: add sysbuild configuration
    For RW612 we need to build mcuboot alongside the example, in order to set
    the proper settings as per the OTA requirements.
    
    Signed-off-by: Alex Porosanu <alexandru.porosanu@nxp.com>

---
+++ wireless_examples/zigbee/router/freertos/sysbuild.cmake
@@ -0,0 +1,17 @@
+
+if ("${board}" STREQUAL "frdmrw612" OR
+    "${board}" STREQUAL "rdrw612bga")
+        ExternalMCUXProject_Add(
+                APPLICATION mcuboot
+                SOURCE_DIR  ${APP_DIR}/../../../../ota_examples/mcuboot_opensource
+	)
+
+	# Let's build the secondary application first
+	add_dependencies(${DEFAULT_IMAGE} mcuboot)
+
+	# Add custom setting for mcuboot
+	set_config_bool(mcuboot CONFIG_BOOT_CUSTOM_DEVICE_SETUP y)
+	set_config_int(mcuboot CONFIG_BOOT_FLASH_ACT_APP_ADDRESS 0x08020000)
+	set_config_int(mcuboot CONFIG_BOOT_FLASH_CAND_APP_ADDRESS 0x08460000)
+	set_config_int(mcuboot CONFIG_MCUBOOT_MAX_IMG_SECTORS 1088)
+endif()
