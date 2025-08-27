ae3e1ae1f291fd6df329f8466bba07f493ff1f49
William Tang <william.tang@nxp.com>
2025-08-27 23:05:27 +0800

    [MCUX-82344]Fix pwm fault example assert error
     - Update PWM frequency to 12kHz to make period in ticks
       will not larger than 65535.
     - Slow down PWM duty cycle update frequency to make wave
       form easier to be measured.
    
    Signed-off-by: William Tang <william.tang@nxp.com>

--- _boards/evkcmimxrt1060/demo_apps/pwm_fault/app.h
+++ _boards/evkcmimxrt1060/demo_apps/pwm_fault/app.h
@@ -19,12 +19,12 @@
 #define DEMO_PWM_CONTROL_SUBMODULE         kPWM_Control_Module_0
 #define DEMO_PWM_CHANNEL                   kPWM_PwmA
 #define DEMO_PWM_DELAY_VAL                 0x0FFFU
-#define DEMO_PWM_CHANNEL_LOCATION_ON_BOARD "J24-6"
+#define DEMO_PWM_CHANNEL_LOCATION_ON_BOARD "J17-6"
 
 #define DEMO_CMP_BASE                        CMP1
 #define DEMO_CMP_USER_CHANNEL                0U
 #define DEMO_CMP_DAC_CHANNEL                 7U
-#define DEMO_CMP_INPUT_PIN_LOCATION_ON_BOARD "J23-5"
+#define DEMO_CMP_INPUT_PIN_LOCATION_ON_BOARD "J33-5"
 
 #define DEMO_DEADTIME_VAL 650U
 #define PWM_SRC_CLK_FREQ  CLOCK_GetFreq(kCLOCK_IpgClk)
