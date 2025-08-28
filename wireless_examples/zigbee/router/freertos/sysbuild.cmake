4f0059e31970276050e31d3062d9c13520b4223b
Alex Porosanu <alexandru.porosanu@nxp.com>
2025-08-28 11:58:04 +0300

    [MCZB-2944] rw612: wireless_exaples: router: enable RSA signature verification for images instead of ECDSA
    Signed-off-by: Alex Porosanu <alexandru.porosanu@nxp.com>

--- wireless_examples/zigbee/router/freertos/sysbuild.cmake
+++ wireless_examples/zigbee/router/freertos/sysbuild.cmake
@@ -4,14 +4,15 @@ if ("${board}" STREQUAL "frdmrw612" OR
         ExternalMCUXProject_Add(
                 APPLICATION mcuboot
                 SOURCE_DIR  ${APP_DIR}/../../../../ota_examples/mcuboot_opensource
-	)
+    )
 
-	# Let's build the secondary application first
-	add_dependencies(${DEFAULT_IMAGE} mcuboot)
+    # Let's build the secondary application first
+    add_dependencies(${DEFAULT_IMAGE} mcuboot)
 
-	# Add custom setting for mcuboot
-	set_config_bool(mcuboot CONFIG_BOOT_CUSTOM_DEVICE_SETUP y)
-	set_config_int(mcuboot CONFIG_BOOT_FLASH_ACT_APP_ADDRESS 0x08020000)
-	set_config_int(mcuboot CONFIG_BOOT_FLASH_CAND_APP_ADDRESS 0x08460000)
-	set_config_int(mcuboot CONFIG_MCUBOOT_MAX_IMG_SECTORS 1088)
+    # Add custom setting for mcuboot
+    set_config_bool(mcuboot CONFIG_BOOT_CUSTOM_DEVICE_SETUP y)
+    set_config_int(mcuboot CONFIG_BOOT_FLASH_ACT_APP_ADDRESS 0x08020000)
+    set_config_int(mcuboot CONFIG_BOOT_FLASH_CAND_APP_ADDRESS 0x08460000)
+    set_config_int(mcuboot CONFIG_MCUBOOT_MAX_IMG_SECTORS 1088)
+    set_config_bool(mcuboot CONFIG_BOOT_SIGNATURE_TYPE_RSA y)
 endif()
