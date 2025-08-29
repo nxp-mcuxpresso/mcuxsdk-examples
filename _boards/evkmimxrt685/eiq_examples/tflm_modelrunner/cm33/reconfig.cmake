b9c4bfb747ee737c323539e4a7959cd566925e84
joy deng <joy.deng@nxp.com>
2025-08-29 14:01:31 +0800

    [AIR-13357]enlarge heap size on RT685 to resolve nnserver malloc issue
    Signed-off-by: joy deng <joy.deng@nxp.com>

--- _boards/evkmimxrt685/eiq_examples/tflm_modelrunner/cm33/reconfig.cmake
+++ _boards/evkmimxrt685/eiq_examples/tflm_modelrunner/cm33/reconfig.cmake
@@ -21,18 +21,19 @@ mcux_add_macro(
             PRINTF_FLOAT_ENABLE=1"
 )
 mcux_add_iar_configuration(
-  LD "--config_def=__heap_size__=0x8000\
+  LD "--config_def=__heap_size__=0x100000\
   --config_def=__stack_size__=0x2000"
   )
 mcux_add_mdk_configuration(
-  LD "--predefine=-D__heap_size__=0x8000\
+  LD "--predefine=-D__heap_size__=0x100000\
   --predefine=-D__stack_size__=0x2000"
   )
 mcux_add_armgcc_configuration(
-  LD "-Xlinker --defsym=__heap_size__=0x8000\
+  LD "-Xlinker --defsym=__heap_size__=0x100000\
   -Xlinker --defsym=__stack_size__=0x2000"
   )
 mcux_add_mcux_configuration(
   LD "-Xlinker --defsym=__heap_size__=0x100000\
       -Xlinker --defsym=__stack_size__=0x2000"
 )
+
