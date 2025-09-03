f813e0e7da5f7c72073663a119b58e61212c7f0d
Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>
2025-09-03 14:06:39 +0800

    [MCUX-82335] Remove unused code
    Signed-off-by: Zhaoxiang Jin <Zhaoxiang.Jin_1@nxp.com>

--- _boards/frdmmcxa366/demo_apps/power_manager_test/bm/reconfig.cmake
+++ _boards/frdmmcxa366/demo_apps/power_manager_test/bm/reconfig.cmake
@@ -44,20 +44,8 @@ mcux_add_mdk_linker_script(
     LINKER examples/_boards/${board}/demo_apps/power_manager_test/linker/MCXA366_power_manager_flash.scf
 )
 
-mcux_project_remove_source(
-    BASE_PATH ${SdkRootDirPath}
-    SOURCES devices/MCX/MCXA/MCXA366/startup_MCXA366.c
-    TOOLCHAINS armgcc
-)
-
-mcux_add_source(
-    BASE_PATH ${SdkRootDirPath}
-    SOURCES devices/MCX/MCXA/MCXA366/startup_MCXA366.c
-    EXCLUDE TRUE
-    TOOLCHAINS armgcc
-)
-
 mcux_add_source(
     BASE_PATH ${SdkRootDirPath}
     SOURCES examples/_boards/${board}/demo_apps/power_manager_test/startup/startup_power_manager.c
+    TOOLCHAINS armgcc iar mdk
 )
