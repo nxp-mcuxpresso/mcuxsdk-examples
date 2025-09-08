e18fc5a1cf711ce436adbc6da751f70257e73c39
VitSolcikNXP <vit.solcik@nxp.com>
2025-09-08 15:34:58 +0200

    [MCUX-80264][MbedTLS3x][ELE_HSEB] Improve linker and cmake settings
    Some MbedTLS3x examples needs liker and cmake file settings adjustment.
    
    Signed-off-by: VitSolcikNXP <vit.solcik@nxp.com>

--- _boards/frdmmcxe31b/mbedtls3x_examples/psa_crypto_examples/reconfig.cmake
+++ _boards/frdmmcxe31b/mbedtls3x_examples/psa_crypto_examples/reconfig.cmake
@@ -1,18 +1,12 @@
 
 mcux_add_macro(
-    CC "-DFRDM_MCXE247\
-        -DFREEDOM"
+    CC -DFREEDOM
 )
 
-mcux_add_iar_configuration(
-    LD "--config_def=__stack_size__=0x4000\
-        --config_def=__heap_size__=0x8000"
+mcux_remove_linker_symbol(
+    SYMBOLS __stack_size__=0x2000
 )
-mcux_add_mdk_configuration(
-    LD "--predefine=\"-D__stack_size__=0x4000\"\
-        --predefine=\"-D__heap_size__=0x8000\""
-)
-mcux_add_armgcc_configuration(
-    LD "-Xlinker --defsym=__stack_size__=0x4000\
-        -Xlinker --defsym=__heap_size__=0x8000"
+
+mcux_add_linker_symbol(
+    SYMBOLS __stack_size__=0x4000
 )
