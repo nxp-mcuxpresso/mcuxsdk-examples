b246137546c90e7fd61154ce42b65b0205f22c1a
VitSolcikNXP <vit.solcik@nxp.com>
2025-09-24 17:06:05 +0200

    [MCUX-70713][FRDMRT1186][MbedTLS3x][PSA][ELE_Crypto] Port examples
    Signed-off-by: VitSolcikNXP <vit.solcik@nxp.com>

--- _boards/frdmimxrt1186/mbedtls3x_examples/mbedtls3x_selftest/cm33/hardware_init.c
+++ _boards/frdmimxrt1186/mbedtls3x_examples/mbedtls3x_selftest/cm33/hardware_init.c
@@ -9,7 +9,11 @@
 #include "pin_mux.h"
 #include "clock_config.h"
 #include "board.h"
+#include "ele_crypto.h"
+#include "fsl_debug_console.h"
 /*${header:end}*/
+
+extern uint8_t ele_fw[];
 
 /*${function:start}*/
 void BOARD_InitHardware(void)
@@ -18,5 +22,11 @@ void BOARD_InitHardware(void)
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
+
+    /* We need FW to be loaded in order to get entropy */
+    if (ELE_LoadFw(MU_RT_S3MUA, ele_fw) != kStatus_Success) {
+        PRINTF("Load FW failed\n");
+    }
+
 }
 /*${function:end}*/
