45a04077676484f9f6df30e755112db7e4626253
Patrik Nemeth (nxf98608) <patrik.nemeth@nxp.com>
2025-09-24 10:24:26 +0200

    [MCUX-83104][frdmmcxl255][mbedtls3x_psatest] Fix optimization reconfig
    Needed to remove the old optimization config before bumping it to
    the -O1 level, in order to correctly translate it to ecosystem.
    
    Signed-off-by: Patrik Nemeth

--- _boards/frdmmcxl255/mbedtls3x_examples/mbedtls3x_psatest/cm33/reconfig.cmake
+++ _boards/frdmmcxl255/mbedtls3x_examples/mbedtls3x_psatest/cm33/reconfig.cmake
@@ -1,4 +1,10 @@
 # ARMGCC debug target does not fit; slightly increase optimization as workaround
+mcux_remove_armgcc_configuration(
+    TARGETS debug
+    CC "-O0"
+    CX "-O0"
+)
+
 mcux_add_armgcc_configuration(
     TARGETS debug
     CC "-O1"
