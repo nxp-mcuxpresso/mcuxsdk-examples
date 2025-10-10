73706dbbe6e4c36fccd4fb26aa8d618d5bf2e03f
Robert Yeung <robert.yeung@nxp.com>
2025-10-10 11:21:17 +0200

    [MCUX-81851] align CC flags between kw47loc frdmmcxw72 and kw47evk for connectivity_test

--- _boards/kw47loc/wireless_examples/genfsk/connectivity_test/reconfig.cmake
+++ _boards/kw47loc/wireless_examples/genfsk/connectivity_test/reconfig.cmake
@@ -1,16 +1,13 @@
 
 # Compiler options for all toolchains
 mcux_add_configuration(
-    CC "        -DCPU_KW45B41Z83AFTA\
-                -DSERIAL_PORT_TYPE_UART=1\
-                -DENABLE_RAM_VECTOR_TABLE\
-                -DHAL_UART_DMA_ENABLE=0\
-                -DSERIAL_PORT_TYPE_UART_DMA=0\
-                -DHAL_UART_ADAPTER_FIFO=1\
-                -DKW45_A0_SUPPORT=0\
-                -DTIMER_PORT_TYPE_LPTMR=1\
-                -DHAL_FLASH_ROMAPI_DRIVER=1\
-                -DSERIAL_MANAGER_NON_BLOCKING_MODE=1"
+    CC "-DSERIAL_PORT_TYPE_UART=1\
+        -DHAL_UART_DMA_ENABLE=0\
+        -DSERIAL_PORT_TYPE_UART_DMA=0\
+        -DHAL_UART_ADAPTER_FIFO=1\
+        -DTIMER_PORT_TYPE_LPTMR=1\
+        -DHAL_FLASH_ROMAPI_DRIVER=1\
+        -DSERIAL_MANAGER_NON_BLOCKING_MODE=1"
 )
 
 # Linker symbols
