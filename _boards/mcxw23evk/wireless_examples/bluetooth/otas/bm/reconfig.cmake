2c3df687f6e2ddb8e0da09681a305669dfe17755
Yassine El Aissaoui <yassine.elaissaoui@nxp.com>
2025-10-14 18:22:10 +0200

    [MCSE-1186][wireless_examples] Fix stack overflow on bm otas on w23
    Removing this since the default value used now is higher

--- _boards/mcxw23evk/wireless_examples/bluetooth/otas/bm/reconfig.cmake
+++ _boards/mcxw23evk/wireless_examples/bluetooth/otas/bm/reconfig.cmake
@@ -1,8 +1,1 @@
 include(${SdkRootDirPath}/${board_root}/${board}/wireless_examples/bluetooth/otas/${multicore_foldername}/reconfig.cmake OPTIONAL)
-
-# the iar debug target requires a bit more stack
-mcux_add_configuration(
-    TOOLCHAINS iar
-    TARGETS debug
-    LD "--config_def=__stack_size__=0xA00"
-)
