d9777df88befd2c97d2c65d0174e448251d253af
Jason Yu <zejiang.yu@nxp.com>
2025-10-24 18:21:29 +0800

    [MCUX-83426][LVGL]Fix LVGL v9.4 run failure
     - Adjust the stack size
    
    Signed-off-by: Jason Yu <zejiang.yu@nxp.com>

--- _boards/rdrw612bga/lvgl_examples/lvgl_guider_bm/reconfig.cmake
+++ _boards/rdrw612bga/lvgl_examples/lvgl_guider_bm/reconfig.cmake
@@ -12,13 +12,6 @@ mcux_add_include(
     INCLUDES ${board_root}/${board}/lvgl_examples
 )
 
-
-mcux_add_iar_configuration(
-    LD "--config_def=__stack_size__=0x1800"
-)
-mcux_add_mdk_configuration(
-    LD "--predefine=\"-D__stack_size__=0x1800\""
-)
-mcux_add_armgcc_configuration(
-    LD "-Xlinker --defsym=__stack_size__=0x1800"
+mcux_add_linker_symbol(
+    SYMBOLS "__stack_size__=0x2000"
 )
