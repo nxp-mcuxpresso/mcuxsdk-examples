63304761e43b7b7a3995e7adeab3805d323d48de
Alison Wang <alison.wang@nxp.com>
2025-10-31 18:44:41 +0900

    [AIR-13612] Add eiq_example executorch_cifarnet_hifi4
    Signed-off-by: Alison Wang <alison.wang@nxp.com>

---
+++ eiq_examples/executorch_cifarnet_hifi4/cm/sysbuild.cmake
@@ -0,0 +1,1 @@
+include(${SdkRootDirPath}/${board_root}/${board}/eiq_examples/executorch_cifarnet_hifi4/${core_id}/sysbuild.cmake)
---
+++ eiq_examples/executorch_cifarnet_hifi4/hifi4/demo_config.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _DEMO_CONFIG_H_
+#define _DEMO_CONFIG_H_
+
+#define EXAMPLE_NAME       "CIFAR-10"
+#define FRAMEWORK_NAME     "Executorch"
+#define DETECTION_TRESHOLD 60
+#define NUM_RESULTS        1
+#define DEMO_VERBOSE       false
+#define EOL                "\r\n"
+
+#endif // _DEMO_CONFIG_H_
