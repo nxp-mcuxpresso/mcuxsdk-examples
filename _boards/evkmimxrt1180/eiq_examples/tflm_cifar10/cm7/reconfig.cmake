a52e6844d4a77a7e6e4e52d4f6b27afa8788ba66
Xiaodong Xie <xiaodong.xie@nxp.com>
2025-11-07 20:32:58 +0800

    [AIR-13614][rt1180] Support tflm_cifar10 on cm7
    Signed-off-by: Xiaodong Xie <xiaodong.xie@nxp.com>

---
+++ _boards/evkmimxrt1180/eiq_examples/tflm_cifar10/cm7/reconfig.cmake
@@ -0,0 +1,14 @@
+mcux_add_include(
+	BASE_PATH ${SdkRootDirPath}
+	INCLUDES examples/eiq_examples/tflm_cifar10/pcq/
+	)
+mcux_add_source(
+	BASE_PATH ${SdkRootDirPath}
+	SOURCES examples/eiq_examples/tflm_cifar10/pcq/model_data.h
+	examples/eiq_examples/tflm_cifar10/pcq/model_cifarnet_ops_micro.cpp
+	)
+mcux_remove_mdk_configuration(
+	CC "-fshort-wchar"
+	CX "-fshort-wchar"
+	)
+
