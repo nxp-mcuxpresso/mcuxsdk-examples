f13e48e3c0bf1041862723fcfde6c06d109e87e1
Biwen Li <biwen.li@nxp.com>
2025-11-11 18:18:22 +0900

    [MCUX-83936][imx943_orangebox] add rgpio_led_output example
    Add rgpio_led_output example.
    
    Signed-off-by: Biwen Li <biwen.li@nxp.com>

---
+++ _boards/imx943_orangebox/driver_examples/rgpio/led_output/cm33_core1/hardware_init.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+/*${header:start}*/
+#include "app.h"
+#include "board.h"
+#include "pin_mux.h"
+/*${header:end}*/
+
+/*${function:start}*/
+void BOARD_InitHardware(void)
+{
+    SystemPlatformInit();
+    BOARD_InitDebugConsolePins();
+    BOARD_InitLEDsPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+    BOARD_ConfigMPU();
+
+    /* Set PCNS register value to 0x0 to prepare the RGPIO initialization */
+    BOARD_LED_RGPIO->PCNS = 0x0;
+}
+/*${function:end}*/
