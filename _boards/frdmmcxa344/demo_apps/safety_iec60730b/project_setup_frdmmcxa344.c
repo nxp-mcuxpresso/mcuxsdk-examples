526103dd2d3129da11cc5b1a5b0b44fac8b5b68a
Martin Martincek <martin.martincek@nxp.com>
2025-11-27 09:40:06 +0100

    [IECM-1766] Enabled safety clock test for board mcxa344. Fixed program counter test address in ram.
    Signed-off-by: Martin Martincek <martin.martincek@nxp.com>

--- _boards/frdmmcxa344/demo_apps/safety_iec60730b/project_setup_frdmmcxa344.c
+++ _boards/frdmmcxa344/demo_apps/safety_iec60730b/project_setup_frdmmcxa344.c
@@ -14,6 +14,7 @@
 #endif
 
 #include "board.h"
+#include "fsl_ctimer.h"
 
 /*******************************************************************************
  * Code
@@ -31,7 +32,26 @@
  */
 void WatchdogEnable(uint32_t wd_setup_value)
 {
+    uint32_t reg_offset         = CLK_GATE_REG_OFFSET(kCLOCK_GateWWDT0);
+    uint32_t bit_shift          = CLK_GATE_BIT_SHIFT(kCLOCK_GateWWDT0);
+    volatile uint32_t *pClkCtrl = (volatile uint32_t *)((uint32_t)(&(MRCC0->MRCC_GLB_CC0_SET)) + reg_offset);
 
+    /* Unlock clock configuration */
+    SYSCON->CLKUNLOCK &= ~SYSCON_CLKUNLOCK_UNLOCK_MASK;
+    
+    *pClkCtrl = (1UL << bit_shift);
+    
+    /* Freeze clock configuration */
+    SYSCON->CLKUNLOCK |= SYSCON_CLKUNLOCK_UNLOCK_MASK;
+
+    USED_WDOG->TC     = WWDT_TC_COUNT(wd_setup_value); /* refresh value */
+    USED_WDOG->MOD    = WWDT_MOD_WDRESET(1) | WWDT_MOD_WDEN(1);
+    USED_WDOG->WINDOW = 0xFFFFFF; /* Disable Window mode */
+
+    __asm("CPSID i");
+    USED_WDOG->FEED = 0xAA; /* Start WDOG */
+    USED_WDOG->FEED = 0x55;
+    __asm("CPSIE i");
 }
 
 /*!
@@ -57,7 +77,14 @@ void WatchdogDisable(void)
  */
 void CTIMER_initialisation(void)
 {
-
+    /* Use FRO HF clock for some of the Ctimers */
+    CLOCK_SetClockDiv(CLOCK_DIVIDER, CLOCK_DIVIDE_VALUE);
+    CLOCK_AttachClk(USED_CLOCK_SOURCE);
+  
+    ctimer_config_t config;
+    CTIMER_GetDefaultConfig(&config);
+    CTIMER_Init(REF_TIMER_USED, &config);
+    CTIMER_StartTimer(REF_TIMER_USED);
 }
 
 /*!
@@ -88,27 +115,27 @@ void ClockInit(void)
     CLOCK_AttachClk(BOARD_DEBUG_UART_CLK_ATTACH);
     RESET_PeripheralReset(BOARD_DEBUG_UART_RST);
     
-//    /* Release peripheral reset */
-//    RESET_ReleasePeripheralReset(kADC0_RST_SHIFT_RSTn);
-//
-//    /* Attach peripheral clock */
-//    CLOCK_SetClockDiv(kCLOCK_DivADC, 1u);
-//    CLOCK_AttachClk(kFRO_LF_DIV_to_ADC);    
-//    
-//    /* GPIO3: Peripheral clock is enabled */
-//    CLOCK_EnableClock(kCLOCK_GateGPIO3);
-//    
-//    /* PORT3: Peripheral clock is enabled */
-//    CLOCK_EnableClock(kCLOCK_GatePORT3);
-//  
-//    /* GPIO3 peripheral is released from reset */
-//    RESET_ReleasePeripheralReset(kGPIO3_RST_SHIFT_RSTn);
-//    
-//    /* PORT3 peripheral is released from reset */
-//    RESET_ReleasePeripheralReset(kPORT3_RST_SHIFT_RSTn);
-//    
-//    /* Release peripheral reset */
-//    RESET_ReleasePeripheralReset(kCRC0_RST_SHIFT_RSTn);
+    /* Release peripheral reset */
+    RESET_ReleasePeripheralReset(kADC0_RST_SHIFT_RSTn);
+
+    /* Attach peripheral clock */
+    CLOCK_SetClockDiv(kCLOCK_DivADC, 1u);
+    CLOCK_AttachClk(kFRO_LF_DIV_to_ADC);    
+    
+    /* GPIO3: Peripheral clock is enabled */
+    CLOCK_EnableClock(kCLOCK_GateGPIO3);
+    
+    /* PORT3: Peripheral clock is enabled */
+    CLOCK_EnableClock(kCLOCK_GatePORT3);
+  
+    /* GPIO3 peripheral is released from reset */
+    RESET_ReleasePeripheralReset(kGPIO3_RST_SHIFT_RSTn);
+    
+    /* PORT3 peripheral is released from reset */
+    RESET_ReleasePeripheralReset(kPORT3_RST_SHIFT_RSTn);
+    
+    /* Release peripheral reset */
+    RESET_ReleasePeripheralReset(kCRC0_RST_SHIFT_RSTn);
 
     /* SDK Initialization */
     BOARD_InitBootClocks();
@@ -156,7 +183,11 @@ void SerialInit()
  */
 void PortSetup(uint32_t gpio, uint8_t pinNum, uint8_t pinDir)
 {
+    /* Port configuration structure */
+    gpio_pin_config_t portConfig = {pinDir ? kGPIO_DigitalOutput : kGPIO_DigitalInput, 0U};
 
+    /* Initialization */
+    GPIO_PinInit((GPIO_Type *)gpio, pinNum, &portConfig);
 }
 
 /*!
@@ -170,5 +201,46 @@ void PortSetup(uint32_t gpio, uint8_t pinNum, uint8_t pinDir)
  */
 void AdcInit(void)
 {
+    /* Release peripheral reset */
+    RESET_ReleasePeripheralReset(kADC1_RST_SHIFT_RSTn);
 
+    /* Attach peripheral clock */
+    CLOCK_SetClockDiv(kCLOCK_DivADC, 1u);
+    CLOCK_AttachClk(kFRO_LF_DIV_to_ADC);
+    CLOCK_EnableClock(kCLOCK_GateADC0);
+    
+    /* enable VREF */
+    SPC0->ACTIVE_CFG1 |= 0x1;
+    
+    /* Disable the module before setting configuration. */
+    TESTED_ADC->CTRL &= ~ADC_CTRL_ADCEN_MASK;
+    
+    /* Reset ADC module */
+    TESTED_ADC->CTRL |= ADC_CTRL_RST_MASK;
+    TESTED_ADC->CTRL &= ~ADC_CTRL_RST_MASK;
+
+    /* Reset FIFO(s) */
+#if (defined(FSL_FEATURE_LPADC_FIFO_COUNT) && (FSL_FEATURE_LPADC_FIFO_COUNT == 2))
+    TESTED_ADC->CTRL |= ADC_CTRL_RSTFIFO0_MASK;
+    TESTED_ADC->CTRL |= ADC_CTRL_RSTFIFO1_MASK;
+#else
+    TESTED_ADC->CTRL |= ADC_CTRL_RSTFIFO_MASK;
+#endif /* FSL_FEATURE_LPADC_FIFO_COUNT */
+
+    /* Disable ADC */
+    TESTED_ADC->CTRL &= ~ADC_CTRL_ADCEN_MASK;
+
+    /* Conf ADC CFG register */
+    TESTED_ADC->CFG |= ADC_CFG_PUDLY(0x80)    /* Power up delay. */
+                       | ADC_CFG_REFSEL(0U)   /* Reference voltage. */
+                       | ADC_CFG_TPRICTRL(0U) /* Trigger priority policy. */
+                       | ADC_CFG_PWREN(1U);   /* LPADC Analog Pre-Enable */
+
+    /* Disable PAUSE between conversions */
+    TESTED_ADC->PAUSE = 0U;
+
+    TESTED_ADC->TCTRL[0] = ADC_TCTRL_TCMD(1);
+
+    /* Enable ADC */
+    TESTED_ADC->CTRL |= ADC_CTRL_ADCEN_MASK;
 }
