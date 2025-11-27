78ab23c33a655976f3b52b1a2f3c82151944cd34
Stanislav Poboril <stanislav.poboril@nxp.com>
2025-11-27 14:24:28 +0100

    [MCUX-84366] lwip_httpssrv_mbedTLS_freertos: increased memory on some platforms
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

--- _boards/evkcmimxrt1060/lwip_examples/lwip_httpssrv_mbedTLS/freertos/reconfig.cmake
+++ _boards/evkcmimxrt1060/lwip_examples/lwip_httpssrv_mbedTLS/freertos/reconfig.cmake
@@ -1,4 +1,10 @@
-# Add additional configuration
+mcux_remove_linker_symbol(
+    SYMBOLS "__heap_size__=110000"
+)
+mcux_add_linker_symbol(
+    SYMBOLS "__heap_size__=120000"
+)
+
 mcux_add_macro(
     CC "-DFSL_FEATURE_PHYKSZ8081_USE_RMII50M_MODE\
         -DFSL_SDK_ENABLE_DRIVER_CACHE_CONTROL=1"
