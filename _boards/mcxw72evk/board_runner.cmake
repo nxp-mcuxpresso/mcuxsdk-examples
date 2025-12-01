70b0748451eb958631da9bc0cb0cf02754aa1b17
Allen Zhang <chunfeng.zhang@nxp.com>
2025-12-01 15:44:20 +0800

    [MCUX-84319][KW47][MCXW72] Correct the device and core info in board_runner.cmake
    1. Added the missing --core information for linkserver
    2. Correct the --device information for jlink
    
    Signed-off-by: Allen Zhang <chunfeng.zhang@nxp.com>

--- _boards/mcxw72evk/board_runner.cmake
+++ _boards/mcxw72evk/board_runner.cmake
@@ -1,15 +1,14 @@
-# Copyright 2024 NXP
+# Copyright 2024-2025 NXP
 #
 # SPDX-License-Identifier: BSD-3-Clause
 if(${core_id} STREQUAL cm33_core0)
-    board_runner_args(jlink "--device=${CONFIG_MCUX_HW_DEVICE_ID}_M33_0")
+    board_runner_args(jlink "--device=${CONFIG_MCUX_TOOLCHAIN_JLINK_CPU_IDENTIFIER}_M33_0")
 elseif(${core_id} STREQUAL cm33_core1)
-    board_runner_args(jlink "--device=${CONFIG_MCUX_HW_DEVICE_ID}_M33_1")
+    board_runner_args(jlink "--device=${CONFIG_MCUX_TOOLCHAIN_JLINK_CPU_IDENTIFIER}_M33_1")
 endif()
 
-board_runner_args(jlink "--device=${CONFIG_MCUX_TOOLCHAIN_JLINK_CPU_IDENTIFIER}")
-
 board_runner_args(linkserver  "--device=${CONFIG_MCUX_HW_DEVICE_ID}:MCX-W72-EVK")
+board_runner_args(linkserver  "--core=${core_id}")
 
 include(${SdkRootDirPath}/cmake/extension/runner/linkserver.board.cmake)
 include(${SdkRootDirPath}/cmake/extension/runner/jlink.board.cmake)
