9b4105b25b7a0ce40c4a84bd768644514830cb3f
Samuel Slesar <samuel.slesar@nxp.com>
2025-12-02 15:58:30 +0100

    [MCUX-84057][MCXL20][trustzone_examples] Port hello_world to MCXL255
    Port the hello_world trustzone example to the FRDM-MCXL255 platform.
    
    Signed-off-by: Samuel Slesar <samuel.slesar@nxp.com>

---
+++ _boards/frdmmcxl255/trustzone_examples/hello_world/hello_world_ns/cm33/hardware_init.c
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2025
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*${header:start}*/
+#include "app.h"
+/*${header:end}*/
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${function:start}*/
+void BOARD_InitHardware(void) {}
+void SystemInit(void) {}
+/*${function:end}*/
---
+++ _boards/frdmmcxl255/trustzone_examples/hello_world/hello_world_s/cm33/hardware_init.c
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2025 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*${header:start}*/
+#include "pin_mux.h"
+#include "board.h"
+#include "app.h"
+#include "tzm_config.h"
+/*${header:end}*/
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${function:start}*/
+void BOARD_InitHardware(void)
+{
+    BOARD_InitBootClocks();
+    BOARD_InitBootPins();
+    BOARD_InitDebugConsole();
+}
+/*${function:end}*/
