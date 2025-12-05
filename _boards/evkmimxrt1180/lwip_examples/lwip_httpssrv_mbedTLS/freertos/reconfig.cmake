125bbef416b417e0b0462e91ce76cd05143de606
Stanislav Poboril <stanislav.poboril@nxp.com>
2025-12-05 14:45:19 +0100

    [MCUX-84362] lwip_httpssrv_mbedTLS examples: increased memory on frdmimxrt1186 and evkmimxrt1180.
    Signed-off-by: Stanislav Poboril <stanislav.poboril@nxp.com>

--- _boards/evkmimxrt1180/lwip_examples/lwip_httpssrv_mbedTLS/freertos/reconfig.cmake
+++ _boards/evkmimxrt1180/lwip_examples/lwip_httpssrv_mbedTLS/freertos/reconfig.cmake
@@ -1,1 +1,8 @@
+mcux_remove_linker_symbol(
+    SYMBOLS "__heap_size__=110000"
+)
+mcux_add_linker_symbol(
+    SYMBOLS "__heap_size__=120000"
+)
+
 include(${CMAKE_CURRENT_LIST_DIR}/${core_id}/reconfig.cmake OPTIONAL)
--- _boards/frdmimxrt1186/lwip_examples/lwip_httpssrv_mbedTLS/bm/reconfig.cmake
+++ _boards/frdmimxrt1186/lwip_examples/lwip_httpssrv_mbedTLS/bm/reconfig.cmake
@@ -10,7 +10,7 @@ mcux_remove_linker_symbol(
     SYMBOLS "__stack_size__=1000"
 )
 mcux_add_linker_symbol(
-    SYMBOLS "__stack_size__=4000"
+    SYMBOLS "__stack_size__=6000"
 )
 
 include(${CMAKE_CURRENT_LIST_DIR}/${core_id}/reconfig.cmake OPTIONAL)
