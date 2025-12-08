21a0eff2851227065c30c260e42830cffc253cd2
Xiaodong Xie <xiaodong.xie@nxp.com>
2025-12-08 16:29:41 +0800

    [AIR-13755] Fix xtensa IDE build issue
    Signed-off-by: Xiaodong Xie <xiaodong.xie@nxp.com>

--- _boards/mimxrt700evk/eiq_examples/tflm_cifar10_hifi4_neutron/hifi4/reconfig.cmake
+++ _boards/mimxrt700evk/eiq_examples/tflm_cifar10_hifi4_neutron/hifi4/reconfig.cmake
@@ -1,7 +1,6 @@
 mcux_add_xtensa_configuration(
   CC "-std=c99"
   CX "-stdlib=libc++ \
-    -DTENSORARENA_NONCACHE=1\
     -std=c++17"
 )
 
@@ -17,8 +16,3 @@ mcux_add_source(
   SOURCES model_cifarnet_ops_npu.cpp
   model_data.h
   )
-
-mcux_add_xtensa_configuration(
-    TARGETS release
-    LD "-mlsp=${SdkRootDirPath}/${board_root}/mimxrt700evk/eiq_examples/tflm_cifar10_hifi4_neutron/linker/hifi4/min-rt"
-)
