85370706ce59f1b3b26c89d21ca60e2008423f77
Marian Chereji <marian.chereji@nxp.com>
2025-12-11 16:53:22 +0200

    [MATTER-3504][mcxw72] Update configuration of NBU firmware flavor for Matter apps
    Updated the configuration of the project that generates
    an optimized NBU firmware flavor for Matter applications.
    
    Signed-off-by: George Stefan <george.stefan@nxp.com>
    Signed-off-by: Marian Chereji <marian.chereji@nxp.com>

--- _boards/mcxw72evk/wireless_examples/ieee-802.15.4/nbu_ble_15_4_dyn/phy_split_matter/reconfig.cmake
+++ _boards/mcxw72evk/wireless_examples/ieee-802.15.4/nbu_ble_15_4_dyn/phy_split_matter/reconfig.cmake
@@ -2,7 +2,18 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
+mcux_remove_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER ${board_root}/${board}/wireless_examples/linker/iar/${core_id}/${iar_wireless_linker_file}
+)
+mcux_add_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS debug release
+    LINKER examples/_boards/${board}/wireless_examples/linker/iar/nbu_flash_connectivity_matter.icf
+)
+
 mcux_convert_binary(
     TOOLCHAINS armgcc mdk iar
-    BINARY ${APPLICATION_BINARY_DIR}/${CONFIG_TOOLCHAIN}/mcxw72_nbu_ble_full_15_4_dyn.bin
+    BINARY ${APPLICATION_BINARY_DIR}/${CONFIG_TOOLCHAIN}/mcxw72_nbu_ble_15_4_dyn_matter.bin
 )
