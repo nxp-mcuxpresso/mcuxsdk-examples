1d09b728ef7461cd91f346b0f201b78ba1f03b1f
Waqar Tahir <waqar.tahir@nxp.com>
2025-12-22 13:12:04 +0100

    [IOTDL-2465] TF-M Platform Linker file configuration updated as per SDK-generator requirements. Now, device variable is passed and used in code for different phantoms during pre-build command.
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

--- _boards/frdmmcxw23/tfm_examples/tfm_demo/tfm_demo_ns/reconfig.cmake
+++ _boards/frdmmcxw23/tfm_examples/tfm_demo/tfm_demo_ns/reconfig.cmake
@@ -5,6 +5,3 @@
 
 include(${SdkRootDirPath}/${board_root}/${board}/tfm_examples/reconfig.cmake OPTIONAL)
 
-if(CONFIG_MCUX_HW_DEVICE_ID_MCXW235)
-    set(TFM_PLATFORM_LINKER_DEFINES "-DMCXW235_SERIES")
-endif()
