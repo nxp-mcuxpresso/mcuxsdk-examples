704fe510f062d87a228177ec424d57f93ff3c119
Michal Smola <michal.smola@nxp.com>
2026-01-07 15:59:04 +0100

    [MCUX-82171][MCXL25] Fix missing core runner arg for linkserver
    core_id argument is missing for linkserver in generated runners.yml.
    Add the argument definition in board_runner.cmake
    
    Signed-off-by: Michal Smola <michal.smola@nxp.com>

--- _boards/frdmmcxl255/board_runner.cmake
+++ _boards/frdmmcxl255/board_runner.cmake
@@ -5,6 +5,7 @@
 board_runner_args(jlink "--device=${CONFIG_MCUX_TOOLCHAIN_JLINK_CPU_IDENTIFIER}")
 
 board_runner_args(linkserver  "--device=${CONFIG_MCUX_HW_DEVICE_ID}:FRDM-MCXL255")
+board_runner_args(linkserver  "--core=${core_id}")
 
 include(${SdkRootDirPath}/cmake/extension/runner/linkserver.board.cmake)
 include(${SdkRootDirPath}/cmake/extension/runner/jlink.board.cmake)
