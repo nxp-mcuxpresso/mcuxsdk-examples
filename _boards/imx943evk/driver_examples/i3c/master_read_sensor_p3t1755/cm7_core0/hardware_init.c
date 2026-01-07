7b5b35c7eaa2a0087cc6d019f088d9d8648029fc
Biwen Li <biwen.li@nxp.com>
2026-01-07 16:34:01 +0900

    [MCUX-85135][imx943evk] add i3c demo
    Signed-off-by: Biwen Li <biwen.li@nxp.com>

---
+++ _boards/imx943evk/driver_examples/i3c/master_read_sensor_p3t1755/cm7_core0/hardware_init.c
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+/*${header:start}*/
+#include "board.h"
+#include "pin_mux.h"
+#include "app.h"
+/*${header:end}*/
+
+/*${function:start}*/
+void BOARD_InitHardware(void)
+{
+    /* clang-format off */
+    clk_t i3cClkCfg = {
+        .clkId = I3C_MASTER_CLOCK_ROOT,
+        .pclkId = kCLOCK_Osc24m,
+        .rate = 24000000UL,
+        .clkRoundOpt = SCMI_CLOCK_ROUND_AUTO,
+    };
+    /* clang-format on */
+
+    SystemPlatformInit();
+    BOARD_InitDebugConsolePins();
+    BOARD_InitBootPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+    BOARD_ConfigMPU();
+
+    CLOCK_SetRate(&i3cClkCfg);
+    CLOCK_EnableClock(i3cClkCfg.clkId);
+}
+/*${function:end}*/
---
+++ _boards/imx943evk/driver_examples/i3c/master_read_sensor_p3t1755/cm7_core1/app.h
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#ifndef _APP_H_
+#define _APP_H_
+
+/*${header:start}*/
+#include "fsl_clock.h"
+/*${header:end}*/
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+/*${macro:start}*/
+#define EXAMPLE_MASTER             I3C2
+#define I3C_MASTER_CLOCK_ROOT      kCLOCK_I3c2slow
+#define I3C_MASTER_CLOCK_FREQUENCY CLOCK_GetRate(I3C_MASTER_CLOCK_ROOT)
+#define SENSOR_SLAVE_ADDR          0x49U
+
+static inline uint64_t CLOCK_GetCoreSysClkFreq(void)
+{
+    return CLOCK_GetRate(kCLOCK_M71);
+}
+/*${macro:end}*/
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+/*${prototype:start}*/
+void BOARD_InitHardware(void);
+/*${prototype:end}*/
+
+#endif /* _APP_H_ */
