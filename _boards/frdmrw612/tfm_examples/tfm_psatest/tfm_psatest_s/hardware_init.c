fed3345e7786fd4efeb53ea39dcbc6d66eb24f4b
Waqar Tahir <waqar.tahir@nxp.com>
2026-01-14 16:02:27 +0100

    [IOTDL-2487] hardware_init files updated for tfm examples for RW612 to initialize flexcomm14 clock.
    Signed-off-by: Waqar Tahir <waqar.tahir@nxp.com>

--- _boards/frdmrw612/tfm_examples/tfm_psatest/tfm_psatest_s/hardware_init.c
+++ _boards/frdmrw612/tfm_examples/tfm_psatest/tfm_psatest_s/hardware_init.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 NXP
+ * Copyright 2024, 2026 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -16,5 +16,20 @@ void BOARD_InitHardware(void)
     BOARD_InitDEBUG_UARTPins();
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
+    
+    // ---------------  flexcomm14 clock---------------
+    /* Configure FRG14 (choose source & fractional divider) */
+    CLOCK_SetFRGClock(&(const clock_frg_clk_config_t){14, kCLOCK_FrgPllDiv, 255, 0});
+    
+    /* Attach FRG14 output to FLEXCOMM14 function clock */
+    RESET_PeripheralReset(kFC14_RST_SHIFT_RSTn);
+    RESET_ClearPeripheralReset(kFC14_RST_SHIFT_RSTn);
+    
+    /* Read back/verify the clock frequency for FC14 */
+    CLOCK_AttachClk(kFRG_to_FLEXCOMM14);
+    
+    /* Gate on the FLEXCOMM14 peripheral clock */
+    CLOCK_EnableClock(kCLOCK_Flexcomm14);
+    // --------------- flexcomm14 end---------------
 }
 /*${function:end}*/
