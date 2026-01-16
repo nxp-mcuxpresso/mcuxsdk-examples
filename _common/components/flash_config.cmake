f2647677274e3d3889c56b6fa695ee6a2ec659eb
Chay Guo <changyi.guo@nxp.com>
2026-01-16 17:09:54 +0800

    [MCUX-84317][flash_config]Add support for custom flash config component configuration
    Signed-off-by: Chay Guo <changyi.guo@nxp.com>

--- _common/components/flash_config.cmake
+++ _common/components/flash_config.cmake
@@ -1,9 +1,27 @@
+# Copyright 2024-2026 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
 if(CONFIG_MCUX_COMPONENT_driver.flash_config)
 
     mcux_add_source(BASE_PATH ${SdkRootDirPath}/${board_root}/${board}/flash_config SOURCES flash_config.c
                     flash_config.h)
 
     mcux_add_include(BASE_PATH ${SdkRootDirPath}/${board_root}/${board}/flash_config INCLUDES .)
+
+    mcux_add_configuration(
+        CC "-DBOOT_HEADER_ENABLE=1"
+        CX "-DBOOT_HEADER_ENABLE=1"
+    )
+
+endif()
+
+if(CONFIG_MCUX_COMPONENT_driver.flash_config_custom)
+
+    mcux_add_source(BASE_PATH ${SdkRootDirPath}/${board_root}/${board}/flash_config_custom SOURCES flash_config.c
+                    flash_config.h)
+
+    mcux_add_include(BASE_PATH ${SdkRootDirPath}/${board_root}/${board}/flash_config_custom INCLUDES .)
 
     mcux_add_configuration(
         CC "-DBOOT_HEADER_ENABLE=1"
