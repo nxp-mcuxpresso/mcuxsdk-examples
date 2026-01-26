d9e5f1652530563c5e84dd90298ec2c222b8bcc5
Aiden Hu <weiwei.hu@nxp.com>
2026-01-26 14:59:36 +0800

    [MCUX-72174] enable usb examples for frdmmcxa577
    Signed-off-by: Aiden Hu <weiwei.hu@nxp.com>

---
+++ _boards/frdmmcxa577/usb_examples/usb_host_cdc_ecm/bm/reconfig.cmake
@@ -0,0 +1,34 @@
+mcux_add_armgcc_configuration(
+  LD "-Xlinker --defsym=__heap_size__=0x2000"
+)
+
+mcux_add_armgcc_configuration(
+  LD "-Xlinker --defsym=__stack_size__=0x2000"
+)
+
+mcux_add_iar_configuration(
+  LD "--config_def=__heap_size__=0x2000"
+)
+
+mcux_add_iar_configuration(
+  LD "--config_def=__stack_size__=0x2000"
+)
+
+mcux_add_mdk_configuration(
+  LD "--predefine=\"-D__heap_size__=0x2000\""
+)
+
+mcux_add_mdk_configuration(
+  LD "--predefine=\"-D__stack_size__=0x2000\""
+)
+
+mcux_add_include(
+  BASE_PATH "${SdkRootDirPath}"
+  INCLUDES "${board_root}/${board}/usb_examples/usb_host_cdc_ecm/bm"
+)
+
+mcux_add_source(
+  BASE_PATH "${SdkRootDirPath}"
+  SOURCES
+    "${board_root}/${board}/usb_examples/usb_host_cdc_ecm/bm/hardware_init.c"
+)
