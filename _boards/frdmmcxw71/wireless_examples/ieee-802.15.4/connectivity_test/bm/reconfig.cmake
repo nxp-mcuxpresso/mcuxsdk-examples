807fb8de91381725f8380b8461c58b02991fbbec
Radu Alexe <radu.alexe@nxp.com>
2026-02-05 16:12:38 +0200

    [MCZB-3150] conn_test: process Framework packages in interrupt context
    This will eliminate the existing deadlock due to the new NBU architecture
    for MCXW71/FRDMMCXW71 boards.

--- _boards/frdmmcxw71/wireless_examples/ieee-802.15.4/connectivity_test/bm/reconfig.cmake
+++ _boards/frdmmcxw71/wireless_examples/ieee-802.15.4/connectivity_test/bm/reconfig.cmake
@@ -9,3 +9,8 @@ mcux_set_variable(NXP_IEEE_802_15_4_CONN_TEST_USE_NBU 1 CACHE PATH "NXP MCUXPRES
 
 # HDI_MODE = 0
 mcux_set_variable(NXP_IEEE_802_15_4_CONN_TEST_HDI_MODE 0 CACHE PATH "NXP MCUXPRESSO ieee-802.15.4 connectivity_test HDI_MODE")
+
+# The NBU will send during initialization more messages than the existing pool size so connectivity_test application will deadlock
+# waiting for the last packet. To fix this issue we set the Framework Platform to process the incomming RPMSGs in IRQ freeing the
+# in-flight packages and removing the dead-lock.
+mcux_add_configuration(CC "-DgPlatformIcsUseWorkqueueRxProcessing_d=0")
