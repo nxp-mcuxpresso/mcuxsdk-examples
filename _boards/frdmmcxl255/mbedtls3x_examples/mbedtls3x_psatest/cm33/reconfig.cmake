a547fd2599bb45ee5d0ce1647768caf1a04481b7
Vit Chmelar <vit.chmelar@nxp.com>
2026-02-11 10:41:42 +0100

    [MCUX-84411] Turn on PKC PSA driver in relevant crypto examples
    Signed-off-by: Vit Chmelar <vit.chmelar@nxp.com>

--- _boards/frdmmcxl255/mbedtls3x_examples/mbedtls3x_psatest/cm33/reconfig.cmake
+++ _boards/frdmmcxl255/mbedtls3x_examples/mbedtls3x_psatest/cm33/reconfig.cmake
@@ -1,4 +1,4 @@
-# ARMGCC debug target does not fit; slightly increase optimization as workaround
+# ARMGCC debug, IAR debug and release target does not fit; slightly increase optimization as workaround
 mcux_remove_armgcc_configuration(
     TARGETS debug
     CC "-O0"
@@ -7,18 +7,30 @@ mcux_remove_armgcc_configuration(
 
 mcux_add_armgcc_configuration(
     TARGETS debug
-    CC "-O1"
-    CX "-O1"
+    CC "-Os"
+    CX "-Os"
 )
 
 mcux_remove_iar_configuration(
-    TARGETS debug
+    TARGETS debug release
     CC "-On"
     CX "-On"
 )
 
 mcux_add_iar_configuration(
+    TARGETS debug release
+    CC "-Ohz"
+    CX "-Ohz"
+)
+
+mcux_remove_mdk_configuration(
     TARGETS debug
-    CC "-Om"
-    CX "-Om"
+    CC "-O1"
+    CX "-O1"
+)
+
+mcux_add_mdk_configuration(
+    TARGETS debug
+    CC "-Oz"
+    CX "-Oz"
 )
