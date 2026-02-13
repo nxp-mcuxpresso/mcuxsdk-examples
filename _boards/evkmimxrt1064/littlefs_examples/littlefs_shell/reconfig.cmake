fe971058daf579364a848b8b902fbc3e1e1acaa2
David Jurajda <david.jurajda@nxp.com>
2026-02-13 09:20:22 +0100

    [MCUX-81212] Fix littlefs_shell on evkmimxrt1064.
    Signed-off-by: David Jurajda <david.jurajda@nxp.com>

--- _boards/evkmimxrt1064/littlefs_examples/littlefs_shell/reconfig.cmake
+++ _boards/evkmimxrt1064/littlefs_examples/littlefs_shell/reconfig.cmake
@@ -1,10 +1,36 @@
 
 mcux_add_macro(
     CC "-DLFS_NO_INTRINSICS=1\
-       -DSDK_DEBUGCONSOLE_UART"
+        -DSDK_DEBUGCONSOLE_UART\
+        -DSKIP_SYSCLK_INIT\
+        -DXIP_BOOT_HEADER_DCD_ENABLE=1\
+        -DFSL_SDK_ENABLE_DRIVER_CACHE_CONTROL=1"
 )
+
 mcux_add_macro(
     TOOLCHAINS armgcc iar
-    TARGETS hyperram_release release sdram_release
+    TARGETS flexspi_nor_release release sdram_release
     CC "-DLFS_NO_ASSERT"
+)
+
+mcux_remove_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${device_root}/RT/RT1064/MIMXRT1064/iar/MIMXRT1064xxxxx_flexspi_nor.icf
+)
+mcux_remove_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${device_root}/RT/RT1064/MIMXRT1064/gcc/MIMXRT1064xxxxx_flexspi_nor.ld
+)
+
+mcux_add_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${board_root}/${board}/littlefs_examples/littlefs_shell/linker/iar/MIMXRT1064xxxxx_flexspi_nor.icf
+)
+mcux_add_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${board_root}/${board}/littlefs_examples/littlefs_shell/linker/gcc/MIMXRT1064xxxxx_flexspi_nor.ld
 )
