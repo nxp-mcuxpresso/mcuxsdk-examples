e44605c7fe38433da99c1b350cec8c8f8e305611
Vit Solcik <vit.solcik@nxp.com>
2026-03-10 12:47:41 +0100

    [MCUX-86439][TDET][MCXA5xx] Fix UART output
    MCXA5xx board needs new pin mux files.
    
    Signed-off-by: Vit Solcik <vit.solcik@nxp.com>

--- _boards/frdmmcxa577/driver_examples/tdet/hardware_init.c
+++ _boards/frdmmcxa577/driver_examples/tdet/hardware_init.c
@@ -1,29 +1,24 @@
 /*
- * Copyright 2023, 2025 NXP
- * All rights reserved.
+ * Copyright 2026 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
-
 /*${header:start}*/
 #include "pin_mux.h"
-#include "clock_config.h"
-#include "board.h"
 #include "fsl_clock.h"
+#include "fsl_reset.h"
+#include "board.h"
+#include <stdbool.h>
 /*${header:end}*/
 
-/*${variable:start}*/
-
-/*${variable:end}*/
 /*${function:start}*/
-
 void BOARD_InitHardware(void)
 {
     /* TDET module is clocked from VBAT module */
     /* So for proper function of active tampers clocks FRO16k or OSC32k must be enabled */
     //CLOCK_SetupClk16KClocking(kCLKE_16K_VBAT);
 
-    BOARD_InitPins();
+    BOARD_InitDEBUG_UARTPins();
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
 }
