a4c0f70aa4369142f76531bf08ba7e870faa1c90
Qiang Zhao <qiang.zhao@nxp.com>
2026-03-10 11:53:37 +0530

    [MCUX-86566][imx95] Change LPI2C EDMA channel allocation to avoid conflict with V2X
    SM has reserved EDMA5_2 channels 0 and 1 for V2X usage,
    update LPI2C examples to use alternative channels 2 and 3 instead.
    
    Signed-off-by: Qiang Zhao <qiang.zhao@nxp.com>

--- _boards/imx95lp4xevk15/cmsis_driver_examples/lpi2c/edma_b2b_transfer/slave/RTE_Device.h
+++ _boards/imx95lp4xevk15/cmsis_driver_examples/lpi2c/edma_b2b_transfer/slave/RTE_Device.h
@@ -20,10 +20,10 @@ extern void LPI2C6_DeinitPins();
 
 #define RTE_I2C6_PIN_INIT        LPI2C6_InitPins
 #define RTE_I2C6_PIN_DEINIT      LPI2C6_DeinitPins
-#define RTE_I2C6_DMA_TX_CH       0
+#define RTE_I2C6_DMA_TX_CH       2
 #define RTE_I2C6_DMA_TX_PERI_SEL (uint8_t) Dma5RequestMuxLPI2C6Tx
 #define RTE_I2C6_DMA_TX_DMA_BASE EDMA5_2
-#define RTE_I2C6_DMA_RX_CH       1
+#define RTE_I2C6_DMA_RX_CH       3
 #define RTE_I2C6_DMA_RX_PERI_SEL (uint8_t) Dma5RequestMuxLPI2C6Rx
 #define RTE_I2C6_DMA_RX_DMA_BASE EDMA5_2
 
