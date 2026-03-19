9343a6c5120f07e3c4b81446657a338c4bebf318
nxa18911 <peter.butko@nxp.com>
2026-03-19 20:33:29 +0100

    [MCROZ-1517] Added missing ENC macro to servo ecat examples

--- _boards/frdmimxrt1186/ecat_examples/servo_motor/cm7/mc_periph_init.h
+++ _boards/frdmimxrt1186/ecat_examples/servo_motor/cm7/mc_periph_init.h
@@ -126,6 +126,10 @@ typedef struct _clock_setup
 #define M1_MCDRV_ENC_GET(par) \
         MCDRV_QdEncGetPosition(par); \
         MCDRV_QdEncGetSpeed(par); 
+#define M1_MCDRV_ENC_GET_DATA_FAST(par) \
+        MCDRV_QdEncGetPosition(par); \
+        MCDRV_QdEncGetSpeed(par); 
+#define M1_MCDRV_ENC_GET_DATA_SLOW(par)	
 #define M1_MCDRV_ENC_SET_DIRECTION(par) (MCDRV_QdEncSetDirection(par)) 
 #define M1_MCDRV_ENC_SET_PULSES(par) \
         MCDRV_QdEncSetPulses(par);  \
