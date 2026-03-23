03a7a24cf52db3a1ec0880301f39cb14ca5c5f31
Mike Li <mike.gang.li@nxp.com>
2026-03-23 11:56:54 +0800

    [MCUX-86639] Set SEMC memory region non-executable by default for i.MXRT CM7 core
    The setting is intended to prevent CM7 Speculative Instruction Fetch
    from accessing SEMC when SEMC has not been initialized.
    
    Signed-off-by: Mike Li <mike.gang.li@nxp.com>

--- _boards/evkbimxrt1050/board.c
+++ _boards/evkbimxrt1050/board.c
@@ -332,7 +332,7 @@ void BOARD_ConfigMPU(void)
 
     /* Region 1 setting: Memory with Device type, not shareable, non-cacheable. */
     MPU->RBAR = ARM_MPU_RBAR(1, 0x80000000U);
-    MPU->RASR = ARM_MPU_RASR(0, ARM_MPU_AP_FULL, 2, 0, 0, 0, 0, ARM_MPU_REGION_SIZE_512MB);
+    MPU->RASR = ARM_MPU_RASR(1, ARM_MPU_AP_FULL, 2, 0, 0, 0, 0, ARM_MPU_REGION_SIZE_512MB);
 
     /* Region 2 setting: Memory with Device type, not shareable,  non-cacheable. */
     MPU->RBAR = ARM_MPU_RBAR(2, 0x60000000U);
