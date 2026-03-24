1d0178c1a73c5bc6ab2d3d73f9c1d77e52bc379f
Allen Zhang <chunfeng.zhang@nxp.com>
2026-03-24 15:33:05 +0800

    [MCUX-86724][MW30] Enable ADC driver examples for FRDM-KW43
    Signed-off-by: Allen Zhang <chunfeng.zhang@nxp.com>

---
+++ _boards/frdmkw43/driver_examples/lpadc/interrupt/hardware_init.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*${header:start}*/
+#include "fsl_common.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "board.h"
+#include "app.h"
+/*${header:end}*/
+
+/*${function:start}*/
+
+void BOARD_InitHardware(void)
+{
+    BOARD_InitBootPins();
+    BOARD_InitLIGHT_SENSORPins();
+    BOARD_BootClockRUN();
+    BOARD_InitDebugConsole();
+
+    CLOCK_SetIpSrc(kCLOCK_Lpadc0, kCLOCK_IpSrcFro192M);
+    CLOCK_SetIpSrcDiv(kCLOCK_Lpadc0, 2U);
+}
+
+/*${function:end}*/
