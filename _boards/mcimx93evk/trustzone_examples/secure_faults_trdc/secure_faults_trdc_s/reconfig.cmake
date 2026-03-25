bb329fa791fb931e998801cc91e58fedd96b7ba4
Yongxu Wang <yongxu.wang@nxp.com>
2026-03-25 09:42:14 +0100

    [MCUX-86856][i.MX93] correct trustzone examples board name
    Signed-off-by: Yongxu Wang <yongxu.wang@nxp.com>

--- _boards/mcimx93evk/trustzone_examples/secure_faults_trdc/secure_faults_trdc_s/reconfig.cmake
+++ _boards/mcimx93evk/trustzone_examples/secure_faults_trdc/secure_faults_trdc_s/reconfig.cmake
@@ -1,18 +1,18 @@
 #
-# Copyright 2024 NXP
+# Copyright 2024,2026 NXP
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
 mcux_add_armgcc_linker_script(
         TARGETS debug release
         BASE_PATH ${SdkRootDirPath}
-        LINKER ${device_root}/${soc_portfolio}/${soc_series}/${device}/gcc/${CONFIG_MCUX_TOOLCHAIN_LINKER_DEVICE_PREFIX}_${core_id}_ram_s.ld
+        LINKER ${device_root}/${soc_portfolio}/${soc_series}/${device}/gcc/${CONFIG_MCUX_TOOLCHAIN_LINKER_DEVICE_PREFIX}_ram_s.ld
 )
 
 mcux_add_iar_linker_script(
         TARGETS debug release
         BASE_PATH ${SdkRootDirPath}
-        LINKER ${device_root}/${soc_portfolio}/${soc_series}/${device}/iar/${CONFIG_MCUX_TOOLCHAIN_LINKER_DEVICE_PREFIX}_${core_id}_ram_s.icf
+        LINKER ${device_root}/${soc_portfolio}/${soc_series}/${device}/iar/${CONFIG_MCUX_TOOLCHAIN_LINKER_DEVICE_PREFIX}_ram_s.icf
 )
 
 mcux_add_source(
