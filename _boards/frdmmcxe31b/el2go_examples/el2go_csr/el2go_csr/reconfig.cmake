7e72ddcc9c6ac004dc22d4e046aaacfc50ddad6a
Fikret Eren <fikret.eren@nxp.com>
2026-03-25 15:16:45 +0100

    [IOTDL-2619] adding diff linker with el2go csr section. Signed-off-by: Fikret Eren <fikret.eren@nxp.com>

--- _boards/frdmmcxe31b/el2go_examples/el2go_csr/el2go_csr/reconfig.cmake
+++ _boards/frdmmcxe31b/el2go_examples/el2go_csr/el2go_csr/reconfig.cmake
@@ -12,3 +12,15 @@ mcux_add_macro(
     CC "PSA_WANT_ALG_GCM\
         PSA_WANT_ALG_SHA_1"
 )
+
+# -------- adjust linker script  for armgcc -----------
+mcux_remove_armgcc_linker_script(
+    TARGETS debug release
+    LINKER ${SdkRootDirPath}/devices/MCX/MCXE/MCXE31B/gcc/MCXE31B_flash.ld
+)
+mcux_add_armgcc_linker_script(
+            TARGETS debug release
+            BASE_PATH ${SdkRootDirPath}
+            LINKER ${board_root}/${board}/el2go_examples/el2go_csr/linker/armgcc/MCXE31B_flash.ld
+)
+
