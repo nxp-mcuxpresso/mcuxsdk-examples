c6938f1540bf5a0377168703ef545c240718f811
Qiang Zhao <qiang.zhao@nxp.com>
2026-04-07 13:15:36 +0530

    [MCUX-87149][frdmimx95] Add sai_mqs_edma_transfer support
    Add mqs_edma_transfer example
    
    Signed-off-by: Qiang Zhao <qiang.zhao@nxp.com>

---
+++ driver_examples/sai/mqs_edma_transfer/sai_mqs_edma_transfer.c
@@ -0,0 +1,171 @@
+/*
+ * Copyright (c) 2015, Freescale Semiconductor, Inc.
+ * Copyright 2016-2020 NXP
+ * All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "app.h"
+#include "board.h"
+#include "music.h"
+#if defined(FSL_FEATURE_SOC_DMAMUX_COUNT) && FSL_FEATURE_SOC_DMAMUX_COUNT
+#include "fsl_dmamux.h"
+#endif
+#include "fsl_debug_console.h"
+#include "fsl_sai_edma.h"
+
+#if !defined(DEMO_BOARD_MQS_INIT)
+#error "This example requires MQS output. Please define DEMO_BOARD_MQS_INIT in board app.h."
+#endif
+
+/*******************************************************************************
+ * Definitions
+ ******************************************************************************/
+#define BUFFER_NUM (2U)
+#ifndef DEMO_XFER_BUFFER_SIZE
+#define DEMO_XFER_BUFFER_SIZE (1600U)
+#endif
+
+/*******************************************************************************
+ * Prototypes
+ ******************************************************************************/
+static void callback(I2S_Type *base, sai_edma_handle_t *handle, status_t status, void *userData);
+extern void BOARD_SAI_RXConfig(sai_transceiver_t *config, sai_sync_mode_t sync);
+
+/*******************************************************************************
+ * Variables
+ ******************************************************************************/
+#if defined(DEMO_QUICKACCESS_SECTION_CACHEABLE) && DEMO_QUICKACCESS_SECTION_CACHEABLE
+AT_NONCACHEABLE_SECTION_INIT(sai_edma_handle_t txHandle);
+#else
+AT_QUICKACCESS_SECTION_DATA(sai_edma_handle_t txHandle);
+#endif
+edma_handle_t g_dmaHandle = {0};
+AT_NONCACHEABLE_SECTION_ALIGN(static uint8_t buffer[BUFFER_NUM * DEMO_XFER_BUFFER_SIZE], 4);
+volatile bool isFinished      = false;
+volatile uint32_t finishIndex = 0U;
+volatile uint32_t emptyBlock  = BUFFER_NUM;
+#if (defined(DEMO_EDMA_HAS_CHANNEL_CONFIG) && DEMO_EDMA_HAS_CHANNEL_CONFIG)
+extern edma_config_t dmaConfig;
+#else
+edma_config_t dmaConfig = {0};
+#endif
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+static void callback(I2S_Type *base, sai_edma_handle_t *handle, status_t status, void *userData)
+{
+    if (kStatus_SAI_RxError == status)
+    {
+    }
+    else
+    {
+        finishIndex++;
+        emptyBlock++;
+        /* Judge whether the music array is completely transfered. */
+        if (MUSIC_LEN / DEMO_XFER_BUFFER_SIZE == finishIndex)
+        {
+            isFinished = true;
+        }
+    }
+}
+
+/*!\
+ * @brief Main function
+ */
+int main(void)
+{
+    sai_transfer_t xfer;
+    uint32_t cpy_index = 0U, tx_index = 0U;
+    sai_transceiver_t saiConfig;
+
+    BOARD_InitHardware();
+
+    PRINTF("MCUX SDK version: %s\r\n", MCUXSDK_VERSION_FULL_STR);
+
+    PRINTF("SAI MQS EDMA example started!\n\r");
+
+#if (!defined(DEMO_EDMA_HAS_CHANNEL_CONFIG) || (defined(DEMO_EDMA_HAS_CHANNEL_CONFIG) && !DEMO_EDMA_HAS_CHANNEL_CONFIG))
+    EDMA_GetDefaultConfig(&dmaConfig);
+#endif
+    EDMA_Init(DEMO_DMA, &dmaConfig);
+    EDMA_CreateHandle(&g_dmaHandle, DEMO_DMA, DEMO_EDMA_CHANNEL);
+#if defined(FSL_FEATURE_EDMA_HAS_CHANNEL_MUX) && FSL_FEATURE_EDMA_HAS_CHANNEL_MUX
+    EDMA_SetChannelMux(DEMO_DMA, DEMO_EDMA_CHANNEL, DEMO_SAI_EDMA_CHANNEL);
+#endif
+
+    /* SAI init */
+    SAI_Init(DEMO_SAI);
+    SAI_TransferTxCreateHandleEDMA(DEMO_SAI, &txHandle, callback, NULL, &g_dmaHandle);
+
+    /* MQS needs left-justified signal */
+    SAI_GetLeftJustifiedConfig(&saiConfig, DEMO_AUDIO_BIT_WIDTH, kSAI_Stereo, 1U << DEMO_SAI_CHANNEL);
+    saiConfig.syncMode    = DEMO_SAI_TX_SYNC_MODE;
+    saiConfig.masterSlave = DEMO_SAI_MASTER_SLAVE;
+    SAI_TransferTxSetConfigEDMA(DEMO_SAI, &txHandle, &saiConfig);
+
+    /* set bit clock divider */
+    SAI_TxSetBitClockRate(DEMO_SAI, DEMO_AUDIO_MASTER_CLOCK, DEMO_AUDIO_SAMPLE_RATE, DEMO_AUDIO_BIT_WIDTH,
+                          DEMO_AUDIO_DATA_CHANNEL);
+
+    /* sai rx configurations */
+    BOARD_SAI_RXCONFIG(&saiConfig, DEMO_SAI_RX_SYNC_MODE);
+
+    /* master clock configurations */
+    BOARD_MASTER_CLOCK_CONFIG();
+
+    /* MQS init */
+    DEMO_BOARD_MQS_INIT();
+
+    /* If need to handle audio error, enable sai interrupt */
+#if defined(DEMO_SAI_IRQ)
+    EnableIRQ(DEMO_SAI_IRQ);
+    SAI_TxEnableInterrupts(DEMO_SAI, kSAI_FIFOErrorInterruptEnable);
+#endif
+
+    /* Waiting until finished. */
+    while (!isFinished)
+    {
+        if ((emptyBlock > 0U) && (cpy_index < MUSIC_LEN / DEMO_XFER_BUFFER_SIZE))
+        {
+            /* Fill in the buffers. */
+            memcpy((uint8_t *)&buffer[DEMO_XFER_BUFFER_SIZE * (cpy_index % BUFFER_NUM)],
+                   (uint8_t *)&music[cpy_index * DEMO_XFER_BUFFER_SIZE], sizeof(uint8_t) * DEMO_XFER_BUFFER_SIZE);
+            emptyBlock--;
+            cpy_index++;
+        }
+        if (emptyBlock < BUFFER_NUM)
+        {
+            /* xfer structure */
+            xfer.data     = (uint8_t *)&buffer[DEMO_XFER_BUFFER_SIZE * (tx_index % BUFFER_NUM)];
+            xfer.dataSize = DEMO_XFER_BUFFER_SIZE;
+            /* Wait for available queue. */
+            if (kStatus_Success == SAI_TransferSendEDMA(DEMO_SAI, &txHandle, &xfer))
+            {
+                tx_index++;
+            }
+        }
+    }
+
+    /* Once transfer finish, disable SAI instance. */
+    SAI_TransferAbortSendEDMA(DEMO_SAI, &txHandle);
+    SAI_Deinit(DEMO_SAI);
+    PRINTF("\n\r SAI MQS EDMA example finished!\n\r ");
+    while (1)
+    {
+    }
+}
+
+#if defined(DEMO_SAITxIRQHandler)
+void DEMO_SAITxIRQHandler(void)
+{
+    /* Clear the FIFO error flag */
+    SAI_TxClearStatusFlags(DEMO_SAI, kSAI_FIFOErrorFlag);
+
+    /* Reset FIFO */
+    SAI_TxSoftwareReset(DEMO_SAI, kSAI_ResetTypeFIFO);
+    SDK_ISR_EXIT_BARRIER;
+}
+#endif
