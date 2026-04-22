bf83bc47d86dd3c4622f969fa805b6a1092580ca
Bojan Simoneta <bojan.simoneta@nxp.com>
2026-04-22 08:57:17 +0200

    [IOTDL-2623]Fix the main logic of the CSR application
    Signed-off-by: Bojan Simoneta <bojan.simoneta@nxp.com>

--- _boards/frdmmcxe31b/el2go_examples/el2go_csr/el2go_csr/hardware_init.c
+++ _boards/frdmmcxe31b/el2go_examples/el2go_csr/el2go_csr/hardware_init.c
@@ -9,7 +9,14 @@
 #include "pin_mux.h"
 #include "board.h"
 #include "fsl_cache.h"
+#include "hse_default_config.h"
+#include "hse_interface.h"
+#include "hse_host_format_key_catalogs.h"
+#include "hse_keys_allocator.h"
 /*${header:end}*/
+
+const hseKeyGroupCfgEntry_t nvmKeyCatalog[] = {HSE_NVM_KEY_CATALOG_CFG};
+const hseKeyGroupCfgEntry_t ramKeyCatalog[] = {HSE_RAM_KEY_CATALOG_CFG};
 
 /*${function:start}*/
 void BOARD_InitHardware(void)
@@ -20,5 +27,10 @@ void BOARD_InitHardware(void)
     BOARD_InitBootClocks();
 
     BOARD_InitDebugConsole();
+    /* This example requires the user to format the key catalogs and initialize
+     * the key management allocator.
+     */
+    FormatKeyCatalogs(nvmKeyCatalog, ramKeyCatalog);
+    HKF_Init(nvmKeyCatalog, ramKeyCatalog);
 }
 /*${function:end}*/
