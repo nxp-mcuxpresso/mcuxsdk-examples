792e11e915fda0a48679961a86e1887bd0d18dd5
Kate Wang <yumeng.wang@nxp.com>
2026-04-27 14:53:42 +0800

    [MCUX-58516][demo_apps][lin_stack] Move the lin_cfg.c/.h files
    The lin_cfg.c/.h files contains the example common configurations and
    should not be placed in board layer.
    
    Signed-off-by: Kate Wang <yumeng.wang@nxp.com>

--- _boards/kw45b41zevk/demo_apps/lin_stack/master/reconfig.cmake
+++ _boards/kw45b41zevk/demo_apps/lin_stack/master/reconfig.cmake
@@ -1,10 +1,4 @@
 
-mcux_add_source(
-    BASE_PATH ${SdkRootDirPath}
-    SOURCES ${board_root}/${board}/demo_apps/lin_stack/master/lin_cfg.c
-            ${board_root}/${board}/demo_apps/lin_stack/master/lin_cfg.h
-)
-
 mcux_add_macro(
     CC "-DFSL_SDK_LIN_STACK_ENABLE=1"
 )
--- _boards/kw47evk/demo_apps/lin_stack/slave/reconfig.cmake
+++ _boards/kw47evk/demo_apps/lin_stack/slave/reconfig.cmake
@@ -1,15 +1,3 @@
-
-mcux_add_source(
-    BASE_PATH ${SdkRootDirPath}
-    SOURCES ${board_root}/${board}/demo_apps/lin_stack/slave/lin_cfg.c
-            ${board_root}/${board}/demo_apps/lin_stack/slave/lin_cfg.h
-)
-
-mcux_add_include(
-    BASE_PATH ${SdkRootDirPath}
-    INCLUDES ${board_root}/${board}/demo_apps/lin_stack/slave/${core_id}
-)
-
 mcux_add_macro(
     CC "-DFSL_SDK_LIN_STACK_ENABLE=1"
 )
