bec354d0386a2c65ee085e747e3fc27c5123364b
Patrik Nemeth (nxf98608) <patrik.nemeth@nxp.com>
2026-05-04 15:13:27 +0200

    [MCUX-87525][frdmmcxe31b] Remove dependency on key catalog formatting
    Key catalog formatting has been moved to the PSA initialization layer.
    This is now the default behavior, which may be disabled by the new
    optional Kconfig options. For more information, see the PSA readme
    for ELE HSEB.
    
    Signed-off-by: Patrik Nemeth

--- _boards/frdmmcxe31b/mbedtls3x_examples/mbedtls3x_psatest/hardware_init.c
+++ _boards/frdmmcxe31b/mbedtls3x_examples/mbedtls3x_psatest/hardware_init.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 NXP
+ * Copyright 2025-2026 NXP
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
@@ -9,29 +9,15 @@
 #include "pin_mux.h"
 #include "board.h"
 #include "fsl_cache.h"
-#include "hse_default_config.h"
-#include "hse_interface.h"
-#include "hse_host_format_key_catalogs.h"
-#include "hse_keys_allocator.h"
 /*${header:end}*/
-
-const hseKeyGroupCfgEntry_t nvmKeyCatalog[] = {HSE_NVM_KEY_CATALOG_CFG};
-const hseKeyGroupCfgEntry_t ramKeyCatalog[] = {HSE_RAM_KEY_CATALOG_CFG};
 
 /*${function:start}*/
 void BOARD_InitHardware(void)
 {
-    BOARD_ConfigMPU(); // This API enables the CACHE
-    L1CACHE_DisableDCache();
+    BOARD_ConfigMPU();       // This API enables the DCache...
+    L1CACHE_DisableDCache(); // ...but HSEB does not like that
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
-
     BOARD_InitDebugConsole();
-
-    /* This example requires the user to format the key catalogs and initialize
-     * the key management allocator.
-     */
-    FormatKeyCatalogs(nvmKeyCatalog, ramKeyCatalog);
-    HKF_Init(nvmKeyCatalog, ramKeyCatalog);
 }
 /*${function:end}*/
--- _boards/frdmmcxe31b/ota_examples/mcuboot_opensource/psa_alt_files/hardware_init.c
+++ _boards/frdmmcxe31b/ota_examples/mcuboot_opensource/psa_alt_files/hardware_init.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2025 NXP
+ * Copyright 2025-2026 NXP
  * All rights reserved.
  *
  * SPDX-License-Identifier: BSD-3-Clause
@@ -9,15 +9,7 @@
 #include "clock_config.h"
 #include "board.h"
 #include "fsl_cache.h"
-#include "hse_default_config.h"
-#include "hse_interface.h"
-#include "hse_host_format_key_catalogs.h"
-#include "hse_keys_allocator.h"
 /*${header:end}*/
-
-const hseKeyGroupCfgEntry_t nvmKeyCatalog[] = {HSE_NVM_KEY_CATALOG_CFG};
-const hseKeyGroupCfgEntry_t ramKeyCatalog[] = {HSE_RAM_KEY_CATALOG_CFG};
-
 
 /*${function:start}*/
 void BOARD_InitHardware(void)
@@ -34,9 +26,5 @@ void BOARD_InitHardware(void)
      */
 
     BOARD_InitDebugConsole();
-
-    /* crypto-backend init calls */
-    FormatKeyCatalogs(nvmKeyCatalog, ramKeyCatalog);
-    HKF_Init(nvmKeyCatalog, ramKeyCatalog);
 }
 /*${function:end}*/
