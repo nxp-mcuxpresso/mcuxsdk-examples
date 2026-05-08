c4d73fefd82ae61676907f94b18d914d4e375cd2
WBI\nxa14931 <dragos.musoiu@nxp.com>
2026-05-08 12:38:14 +0300

    [BLUETOOTH-17516][LOC][KW43] Add LCE support for KW43

--- _common/project_segments/wireless/wireless_mcu/app_common/app_lce_init.c
+++ _common/project_segments/wireless/wireless_mcu/app_common/app_lce_init.c
@@ -12,12 +12,18 @@
 #include "fsl_component_lce.h"
 #include "app_lce_init.h"
 
+/************************************************************************************
+*************************************************************************************
+* Private macros
+*************************************************************************************
+************************************************************************************/
+
 /* -------------------------------------------------------------------------- */
 /*                              Public memory declarations                    */
 /* -------------------------------------------------------------------------- */
 uint8_t      g_ceHeap_id = 0U;
 /* -------------------------------------------------------------------------- */
-/*                              Private functions                             */
+/*                             Private memory declarations                    */
 /* -------------------------------------------------------------------------- */
 /* CE buffer definition */
 #if defined(__IAR_SYSTEMS_ICC__)
@@ -68,6 +74,7 @@ static memAreaCfg_t mCeHeap = {
     .flags         = AREA_FLAGS_POOL_NOT_SHARED,
 };
 
+#if (defined(KW47_core0_SERIES) || defined(MCXW72_core0_SERIES))
 #ifndef gMinExtendedHeapSize_c
 #define gMinExtendedHeapSize_c (24U*1024U)
 #endif
@@ -82,6 +89,7 @@ static uint32_t maExtHeap[gMinExtendedHeapSize_c / sizeof(uint32_t)] __attribute
 #else
 #error "Compiler unknown!"
 #endif
+
 extern uint32_t __EXT_HEAP_end__[];
 
 static memAreaCfg_t mExtHeapCfg = {
@@ -90,28 +98,49 @@ static memAreaCfg_t mExtHeapCfg = {
     .end_address   = __EXT_HEAP_end__,
     .flags         = 0U,
 };
+/* CE image */
+static const char ceImage[] = {
+#include "ce_kw47_mcxw72.txt"
+};
+#elif (defined(KW43_core0_SERIES) || defined(MCXW70_core0_SERIES))
+/* CE image */
+#if defined(__IAR_SYSTEMS_ICC__)
+#pragma data_alignment = 128
+static const uint8_t ceImage[] = {
+#elif defined(__GNUC__)
+static const uint8_t ceImage[] __attribute__((aligned(128))) = {
+#elif defined(__CC_ARM)
+__align(128) static const uint8_t ceImage[] = {
+#else
+#error "Compiler unknown!"
+#endif
+#include "ce_kw43_mcxw70.txt"
+};
+#endif /* (defined(KW47_core0_SERIES) || defined(MCXW72_core0_SERIES)) */
+
+/* -------------------------------------------------------------------------- */
+/*                              Private functions                             */
+/* -------------------------------------------------------------------------- */
 
 static mem_status_t CE_MEM_Init(void)
 {
     (void)MEM_Init();
     mem_status_t st = kStatus_MemSuccess;
-    uint8_t      memHeap_id;
     if (st == kStatus_MemSuccess)
     {
         st = MEM_RegisterExtendedArea(&mCeHeap, &g_ceHeap_id, AREA_FLAGS_POOL_NOT_SHARED);
     }
+#if (defined(KW47_core0_SERIES) || defined(MCXW72_core0_SERIES))
     if (st == kStatus_MemSuccess)
     {
+        uint8_t      memHeap_id;
         st = MEM_RegisterExtendedArea(&mExtHeapCfg, &memHeap_id, 0);
     }
+#endif /* (defined(KW47_core0_SERIES) || defined(MCXW72_core0_SERIES)) */
     return st;
 }
 
-/* CE image */
-static const char ceImage[] = {
-#include "ce_kw47_mcxw72.txt"
-};
-
+#if (defined(KW47_core0_SERIES) || defined(MCXW72_core0_SERIES))
 static void CE_ImageLoad(volatile char *ceImageBase, char const *ceImage, uint32_t ceImageSize)
 {
     for (int i = 0; i < ceImageSize; i++)
@@ -119,11 +148,12 @@ static void CE_ImageLoad(volatile char *ceImageBase, char const *ceImage, uint32
         ceImageBase[i] = ceImage[i];
     }
 }
-
+#endif /* (defined(KW47_core0_SERIES) || defined(MCXW72_core0_SERIES)) */
 /* -------------------------------------------------------------------------- */
 /*                              Public functions                              */
 /* -------------------------------------------------------------------------- */
 
+#if (defined(KW47_core0_SERIES) || defined(MCXW72_core0_SERIES))
 void APP_InitLce(void)
 {
     /* Load CE image to CE code SRAM */
@@ -144,3 +174,43 @@ void APP_InitLce(void)
     (void)LCE_Init();
     return;
 }
+#elif (defined(KW43_core0_SERIES) || defined(MCXW70_core0_SERIES))
+void APP_InitLce(void)
+{
+    union
+    {
+        const uint8_t *pI;
+        uint32_t  address;
+    }ceImageSource = {.pI = ceImage};
+    /* Enable DSP RAM clock */
+    CLOCK_EnableClockLPMode(kCLOCK_Dsp_ramc0, kCLOCK_IpClkControl_fun2);
+    /* Enable LCE clock */
+    CLOCK_EnableClockLPMode(kCLOCK_Zenv_core, kCLOCK_IpClkControl_fun2); 
+    /*Enable MU clock */
+    CLOCK_EnableClockLPMode(kCLOCK_Mu1, kCLOCK_IpClkControl_fun2); 
+    /* CE MU initialization */
+    MU_Init(MU1_MUA);
+    /* By default, TRDC configuration does not enable the DSP's master permissions.
+     * This statement configures the DSP's DID (Domain ID) and its Secure/Non-secure
+     * attributes. Additionally, since there is PRESET data, the ROM has already
+     * configured SRAM access permissions, but these permissions are mapped to a
+     * specific DID. Therefore, it's necessary to configure the DSP's DID in TRDC
+     * to match the DID that has SRAM access permissions. */
+    TRDC_0_MGR->MDA_DFMT0[6].MDA_W_DFMT0[0] = 0x80000011U;
+    /* Configure MBC3 GLBAC1: read and execute allowed for all security/privilege levels on Flash. */
+    TRDC_0_MGR->MBC_INDEX[3].MBC_MEMN_GLBAC[1] = 0x5555U;
+    /* Unlock SYSCON */
+    SYSCON->AUTHENTICATE = SYSCON_UNLOCK_CODE;
+    /* Set ZENV vector start address */
+    SYSCON->ZENV_CTRL = ceImageSource.address;
+    /* Release reset of ZENV CORE */
+    SYSCON->ZENV_RESET_CTRL = 0UL;
+    /* Lock SYSCON again */
+    SYSCON->AUTHENTICATE = 0UL;
+    (void)CE_MEM_Init();
+    (void)CE_CmdInitBuffer(&cpu_ce_cmdbuffer, ce_buffers.ce_cmd_buffer, ce_buffers.ce_status_buffer,
+                           kCE_CmdModeOneNonBlocking);
+    /* Initialization for LCE APIs */
+    (void)LCE_Init();
+}
+#endif /* (defined(KW47_core0_SERIES) || defined(MCXW72_core0_SERIES)) */
