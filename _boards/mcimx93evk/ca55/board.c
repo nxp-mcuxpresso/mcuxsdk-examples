c08b8c66691ddb128ad6018fa61f829e9af1fbee
Hou Zhiqiang <Zhiqiang.Hou@nxp.com>
2026-05-20 14:22:35 +0800

    [MCUX-87112] mcimx93evk: add board file for CA55
    The board.[ch] under mcimx93evk have many CM33 specific
    code, so add CA55 specific board file under the ca55
    directory.
    
    Signed-off-by: Hou Zhiqiang <Zhiqiang.Hou@nxp.com>

---
+++ _boards/mcimx93evk/ca55/board.c
@@ -0,0 +1,28 @@
+/*
+ * Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "board.h"
+
+/*******************************************************************************
+ * Code
+ ******************************************************************************/
+
+/* Initialize debug console. */
+void BOARD_InitDebugConsole(void)
+{
+    /* clang-format off */
+    const clock_root_config_t uartClkCfg = {
+        .clockOff = false,
+	.mux = 0, // 24MHz oscillator source
+	.div = 1
+    };
+    /* clang-format on */
+
+    CLOCK_SetRootClock(BOARD_DEBUG_UART_CLOCK_ROOT, &uartClkCfg);
+    CLOCK_EnableClock(BOARD_DEBUG_UART_CLOCK_GATE);
+    DbgConsole_Init(BOARD_DEBUG_UART_INSTANCE, BOARD_DEBUG_UART_BAUDRATE, BOARD_DEBUG_UART_TYPE,
+                    BOARD_DEBUG_UART_CLK_FREQ);
+}
