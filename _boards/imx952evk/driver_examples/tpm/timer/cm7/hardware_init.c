ab8c5d4ebd0db8e63e68ba399cf7ffbc0aef8acd
Qiang Zhao <qiang.zhao@nxp.com>
2026-05-26 13:56:56 +0530

    [MCUX-88036] [TPM][imx952evk] Add BOARD_InitTPMPins() call in hardware_init and fix pin function name
    [Fix]
    1. Add BOARD_InitTPMPins() call after BOARD_InitBootPins() in all
       TPM example hardware_init.c files for imx952evk.
    2. Rename BOARD_InitITPMPins to BOARD_InitTPMPins in pin_mux.c.
    
    Signed-off-by: Qiang Zhao <qiang.zhao@nxp.com>

--- _boards/imx952evk/driver_examples/tpm/timer/cm7/hardware_init.c
+++ _boards/imx952evk/driver_examples/tpm/timer/cm7/hardware_init.c
@@ -28,6 +28,7 @@ void BOARD_InitHardware(void)
     SystemPlatformInit();
     /* clang-format on */
     BOARD_InitBootPins();
+    BOARD_InitTPMPins();
     BOARD_BootClockRUN();
     BOARD_InitDebugConsole();
     BOARD_ConfigMPU();
