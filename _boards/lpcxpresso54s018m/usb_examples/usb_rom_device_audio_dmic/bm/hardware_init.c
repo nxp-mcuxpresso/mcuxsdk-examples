c87ad5ac771702e188e3e871d75c73f06e28b82a
Aiden Hu <weiwei.hu@nxp.com>
2026-06-04 17:24:26 +0800

    [MCUX-88213] fix periodic noise for usb rom audio.
    wrong clock divider for rom audio dmic case under
    lpc54xxx board.
    
    Signed-off-by: Aiden Hu <weiwei.hu@nxp.com>

--- _boards/lpcxpresso54s018m/usb_examples/usb_rom_device_audio_dmic/bm/hardware_init.c
+++ _boards/lpcxpresso54s018m/usb_examples/usb_rom_device_audio_dmic/bm/hardware_init.c
@@ -39,7 +39,7 @@ void BOARD_InitHardware(void)
     CLOCK_AttachClk(kFRO12M_to_DMIC);
     /*12MHz divided by 5 = 2.4MHz PDM clock --> gives 48kHz sample rate */
     /*12MHz divided by 15 = 800 KHz PDM clock --> gives 16kHz sample rate */
-    CLOCK_SetClkDiv(kCLOCK_DivDmicClk, 14, false);
+    CLOCK_SetClkDiv(kCLOCK_DivDmicClk, 15, false);
     BOARD_InitBootPins();
     BOARD_InitBootClocks();
     BOARD_InitDebugConsole();
