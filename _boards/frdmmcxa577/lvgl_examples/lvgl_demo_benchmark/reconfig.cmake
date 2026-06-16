db0235f52fc5cf97642c714af5a3f78f7d1fc9fc
Ruoshan Shi <ruoshan.shi@nxp.com>
2026-06-16 16:50:43 +0800

    [MCUX-72644][FRDM-MCXA577]Support LVGL examples
    Add lvgl examples on frdmmcxa577 platform
    
    Signed-off-by: Ruoshan Shi <ruoshan.shi@nxp.com>

---
+++ _boards/frdmmcxa577/lvgl_examples/lvgl_demo_benchmark/reconfig.cmake
@@ -0,0 +1,29 @@
+
+mcux_add_source(
+    BASE_PATH ${SdkRootDirPath}
+    SOURCES ${board_root}/${board}/lvgl_examples/hardware_init.c
+            ${board_root}/${board}/lvgl_examples/app.h
+            examples/lvgl_examples/lvgl_freertos.c
+            examples/lvgl_examples/lvgl_freertos.h
+)
+
+mcux_add_include(
+    BASE_PATH ${SdkRootDirPath}
+    INCLUDES ${board_root}/${board}/lvgl_examples/lvgl_demo_benchmark
+             ${board_root}/${board}/lvgl_examples
+             examples/lvgl_examples
+)
+
+mcux_add_macro(
+    CC "-DBOARD_LCD_S035=1"
+)
+
+mcux_add_iar_configuration(
+    LD "--config_def=__stack_size__=0x1800"
+)
+mcux_add_mdk_configuration(
+    LD "--predefine=\"-D__stack_size__=0x1800\""
+)
+mcux_add_armgcc_configuration(
+    LD "-Xlinker --defsym=__stack_size__=0x1800"
+)
