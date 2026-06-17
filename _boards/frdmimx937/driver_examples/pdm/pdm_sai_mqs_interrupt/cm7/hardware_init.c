f2ee780852d0532f2a1765628653576934bf44fe
Qiang Zhao <qiang.zhao@nxp.com>
2026-06-17 14:48:17 +0530

    [MCUX-87220][FRDMIMX937] Update pdm_sai_mqs_interrupt hardware_init.c
    Sync with frdmimx952: add CLOCK_SetParent for pdmClkCfg,
    fix formatting and remove unused commented-out code.
    
    Signed-off-by: Qiang Zhao <qiang.zhao@nxp.com>

--- _boards/frdmimx937/driver_examples/pdm/pdm_sai_mqs_interrupt/cm7/hardware_init.c
+++ _boards/frdmimx937/driver_examples/pdm/pdm_sai_mqs_interrupt/cm7/hardware_init.c
@@ -13,15 +13,12 @@
 #include "fsl_sai.h"
 /*${header:end}*/
 
-/*${variable:start}*/
-/*${variable:end}*/
-
 /*${function:start}*/
 void BOARD_MQS_Init(void)
 {
-    uint32_t mqsDiv = 1;
+    uint32_t mqsDiv = 1U;
 
-    mqsDiv = HMCLK_FREQ / (64 * 16000 * 6);
+    mqsDiv = HMCLK_FREQ / (64U * 16000U * 6);
     BOARD_InitMQS(mqsDiv - 1U);
 }
 
@@ -38,45 +35,45 @@ void BOARD_InitHardware(void)
         .clkRoundOpt = SCMI_CLOCK_ROUND_AUTO,
         .rate = 393216000,
     };
+    clk_t saiCLKCfg = {
+        .clkId = kCLOCK_sai1,
+        .pclkId = kCLOCK_audiopll1, /* select audiopll1out source(393216000 Hz) */
+        .rate = 12288000UL,         /* output 12288000 Hz */
+        .clkRoundOpt = SCMI_CLOCK_ROUND_AUTO,
+    };
     clk_t pdmClkCfg = {
         .clkId = kCLOCK_pdm,
         .pclkId = kCLOCK_audiopll1,
         .rate = 196608000UL,
-        //.enable_clk = true,
-        .clkRoundOpt = SCMI_CLOCK_ROUND_AUTO,
-    };
-    clk_t saiCLKCfg = {
-        .clkId = kCLOCK_sai1,
-        .pclkId = kCLOCK_audiopll1, /* select audiopll1out source(393216000 Hz) */
-        .rate = 12288000UL, /* output 12288000 Hz */
-        //.enable_clk = true,
         .clkRoundOpt = SCMI_CLOCK_ROUND_AUTO,
     };
     sai_master_clock_t saiMasterCfg = {
         .mclkOutputEnable = true,
-     };
-
+    };
     /* clang-format on */
 
     SystemPlatformInit();
     BOARD_InitBootPins();
-    BOARD_InitMQSPins();
     BOARD_InitPDMPins();
+    BOARD_InitMQSPins();
     BOARD_BootClockRUN();
     BOARD_InitDebugConsole();
+    BOARD_ConfigMPU();
+
     CLOCK_SetRate(&audiopll1vcoCLKCfg);
     CLOCK_EnableClock(audiopll1vcoCLKCfg.clkId);
     CLOCK_SetRate(&audiopll1CLKCfg);
     CLOCK_EnableClock(audiopll1CLKCfg.clkId);
+    CLOCK_SetParent(&pdmClkCfg);
     CLOCK_SetRate(&pdmClkCfg);
     CLOCK_EnableClock(pdmClkCfg.clkId);
     CLOCK_SetParent(&saiCLKCfg);
     CLOCK_SetRate(&saiCLKCfg);
     CLOCK_EnableClock(saiCLKCfg.clkId);
-    BOARD_ConfigMPU();
+
     /* select MCLK direction(Enable MCLK clock) */
-    saiMasterCfg.mclkSourceClkHz = DEMO_SAI_CLK_FREQ;            /* setup source clock for MCLK */
-    saiMasterCfg.mclkHz          = saiMasterCfg.mclkSourceClkHz; /* setup target clock of MCLK */
+    saiMasterCfg.mclkSourceClkHz = DEMO_SAI_CLK_FREQ;
+    saiMasterCfg.mclkHz          = saiMasterCfg.mclkSourceClkHz;
     SAI_SetMasterClockConfig(DEMO_SAI, &saiMasterCfg);
 }
 /*${function:end}*/
