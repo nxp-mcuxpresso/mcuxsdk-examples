3197962223c0fe76a88294a2730549e2263fc818
Wei Feng <wei.feng_5@nxp.com>
2026-06-23 14:22:28 +0800

    [MCUX-87985][evkmimxrt1180] Add mdk flexspi_nor target for trustzone examples
    Signed-off-by: Wei Feng <wei.feng_5@nxp.com>

--- _boards/evkmimxrt1180/trustzone_examples/hello_world/hello_world_s/reconfig.cmake
+++ _boards/evkmimxrt1180/trustzone_examples/hello_world/hello_world_s/reconfig.cmake
@@ -51,6 +51,12 @@ mcux_add_armgcc_linker_script(
     LINKER ${device_root}/${soc_portfolio}/${soc_series}/${device}/gcc/${CONFIG_MCUX_TOOLCHAIN_LINKER_DEVICE_PREFIX}_flexspi_nor_s.ld
 )
 
+mcux_add_mdk_linker_script(
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    BASE_PATH ${SdkRootDirPath}
+    LINKER ${device_root}/${soc_portfolio}/${soc_series}/${device}/arm/${CONFIG_MCUX_TOOLCHAIN_LINKER_DEVICE_PREFIX}_flexspi_nor_s.scf
+)
+
 mcux_add_iar_linker_script(
     TARGETS flexspi_nor_debug flexspi_nor_release
     BASE_PATH ${SdkRootDirPath}
--- _boards/evkmimxrt1180/trustzone_examples/reconfig.cmake
+++ _boards/evkmimxrt1180/trustzone_examples/reconfig.cmake
@@ -45,3 +45,9 @@ mcux_remove_iar_linker_script(
     BASE_PATH ${SdkRootDirPath}
     LINKER ${device_root}/${soc_portfolio}/${soc_series}/${device}/iar/${CONFIG_MCUX_TOOLCHAIN_LINKER_DEVICE_PREFIX}_flexspi_nor.icf
 )
+
+mcux_remove_mdk_linker_script(
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    BASE_PATH ${SdkRootDirPath}
+    LINKER ${device_root}/${soc_portfolio}/${soc_series}/${device}/arm/${CONFIG_MCUX_TOOLCHAIN_LINKER_DEVICE_PREFIX}_flexspi_nor.scf
+)
