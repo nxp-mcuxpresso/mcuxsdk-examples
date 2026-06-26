4146e54175cb2bf4a327f5bb933ba3bb47d33b61
Keval Rajendra Thakker <keval.thakker@nxp.com>
2026-06-26 09:54:10 +0000

    [MCUX-83883] frdmimxrt700: add uart_wifi_bridge example support
    Add uart_wifi_bridge example support for frdmimxrt700 board with IW612
    module:
    
    - Add board-specific prj.conf with required components
    - Add reconfig.cmake with board-specific sources and includes
    - Also fix missing debug_console dependency in wifi_cli and
      wifi_wpa_supplicant prj.conf files

---
+++ _boards/frdmimxrt700/wifi_examples/uart_wifi_bridge/cm33_core0/reconfig.cmake
@@ -0,0 +1,58 @@
+#
+# Copyright 2026 NXP
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+mcux_add_source(
+    BASE_PATH ${SdkRootDirPath}
+    SOURCES middleware/wifi_nxp/example/uart_wifi_bridge/freertos_config/FreeRTOSConfig.h
+            middleware/wifi_nxp/example/uart_wifi_bridge/lwip_config/lwipopts.h
+            middleware/wifi_nxp/example/uart_wifi_bridge/lwip_config/lwippools.h
+            middleware/wifi_nxp/example/uart_wifi_bridge/mfg_wifi_bt_firmware/mfg_wlan_bt_fw.h
+            middleware/wifi_nxp/example/uart_wifi_bridge/mfg_wifi_bt_firmware/sduart8987_mfg_wlan_bt.h
+            middleware/wifi_nxp/example/uart_wifi_bridge/mfg_wifi_bt_firmware/sduartIW416_mfg_wlan_bt.h
+            middleware/wifi_nxp/example/uart_wifi_bridge/mfg_wifi_bt_firmware/sduart_nw61x_mfg_se.h
+            middleware/wifi_nxp/example/uart_wifi_bridge/wifi_config/wifi_config.h
+            ${board_root}/${board}/wifi_examples/common/cm33_core0/hardware_init.c
+            ${board_root}/${board}/wifi_examples/common/cm33_core0/app.h
+            ${board_root}/${board}/sdmmc_config.c
+            ${board_root}/${board}/sdmmc_config.h
+            ${board_root}/${board}/wifi_bt_config.c
+            ${board_root}/${board}/wifi_bt_config.h
+            ${board_root}/${board}/FreeRTOSConfigBoard.h
+)
+
+mcux_add_include(
+    BASE_PATH ${SdkRootDirPath}
+    INCLUDES ${board_root}/${board}/wifi_examples/common
+             ${board_root}/${board}/wifi_examples/common/cm33_core0
+             middleware/wifi_nxp/example/uart_wifi_bridge/wifi_config
+             middleware/wifi_nxp/example/uart_wifi_bridge/freertos_config
+             middleware/wifi_nxp/example/uart_wifi_bridge/lwip_config
+             middleware/wifi_nxp/example/uart_wifi_bridge/mfg_wifi_bt_firmware
+)
+
+mcux_add_iar_configuration(
+    CX "--no_clustering"
+)
+
+mcux_add_macro(
+    CC "-DUSE_RTOS=1\
+       -DPRINTF_ADVANCED_ENABLE=1\
+       -DFSL_SDK_ENABLE_DRIVER_CACHE_CONTROL=1"
+)
+
+mcux_add_iar_configuration(
+    LD "--config_def=__stack_size__=0x400\
+        --config_def=__heap_size__=0x400"
+)
+mcux_add_armgcc_configuration(
+    LD "-Xlinker --defsym=__stack_size__=0x400\
+        -Xlinker --defsym=__heap_size__=0x400"
+)
+mcux_add_mdk_configuration(
+    LD "--predefine=\"-D__stack_size__=0x400\"\
+        --predefine=\"-D__heap_size__=0x400\""
+)
+
+include(${SdkRootDirPath}/${board_root}/${board}/wifi_examples/common/cm33_core0/reconfig.cmake)
