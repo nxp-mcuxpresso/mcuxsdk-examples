56ec3d9d0bd99b0142cb7608e9b81c19c26bb56c
Mohit Agrawal <mohit.agrawal@nxp.com>
2026-07-08 18:47:40 +0530

    WSW-71923: evkbmimxrt1170: add coex_wifi_edgefast_open board support
    Add board-specific files for coex_wifi_edgefast_open example on
    evkbmimxrt1170 (CM7) with IW612 (Murata 2EL) combo module. Also
    supports IW416 (Murata 1XK) by changing module selection in prj.conf.
    
    Includes custom linker script (expanded m_data2 for Wi-Fi), custom
    FreeRTOSConfig (180KB heap), and board prj.conf with IW612 module
    selection.
    
    Update example.yml to add evkbmimxrt1170@cm7 board entry with
    appropriate build exclusions (only armgcc flexspi_nor builds enabled).
    
    Build command:
    west build -b evkbmimxrt1170 examples/coex_examples/coex_wifi_edgefast_open \
      -p always -d build_coex_edgefast_open --config flexspi_nor_release \
      -- -Dcore_id=cm7 -DCONFIG_TOOLCHAIN=armgcc
    
    Signed-off-by: Mohit Agrawal <mohit.agrawal@nxp.com>

---
+++ _boards/evkbmimxrt1170/coex_examples/coex_wifi_edgefast_open/cm7/edgefast_open_app.h
@@ -0,0 +1,26 @@
+/*
+ *  Copyright 2026 NXP
+ *  All rights reserved.
+ *
+ *  SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include "edgefast_open_config.h"
+
+#define CONTROLLER_INIT_ESCAPE 1
+
+#define FLASH_ADAPTER_SIZE 0x10000
+#define OSA_USED           1
+
+#if (defined(WIFI_IW416_BOARD_MURATA_1XK_M2) || defined(WIFI_88W8987_BOARD_MURATA_1ZM_M2) || \
+     defined(WIFI_IW612_BOARD_MURATA_2EL_M2) || defined(WIFI_IW610_BOARD_MURATA_2LL_M2))
+#include "wifi_bt_module_config.h"
+#include "wifi_config.h"
+#else
+#error The transceiver module is unsupported
+#endif
+
+#if defined(WIFI_IW612_BOARD_MURATA_2EL_M2)
+#undef SD_TIMING_MAX
+#define SD_TIMING_MAX kSD_TimingDDR50Mode
+#endif /*#define WIFI_IW612_BOARD_MURATA_2EL_M2*/
