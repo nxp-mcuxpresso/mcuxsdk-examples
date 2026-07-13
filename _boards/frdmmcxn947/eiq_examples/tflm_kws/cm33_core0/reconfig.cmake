bd6baab430a4d100c268f2712b77bfb2c013b9e4
Miles Ma <miles.ma@nxp.com>
2026-07-13 11:04:05 +0200

    [AIR-14466] fix diag_suppress flag in MCXN board eiq tflm_kws IAR configs

--- _boards/frdmmcxn947/eiq_examples/tflm_kws/cm33_core0/reconfig.cmake
+++ _boards/frdmmcxn947/eiq_examples/tflm_kws/cm33_core0/reconfig.cmake
@@ -44,8 +44,7 @@ mcux_add_armgcc_linker_script(
 
 
 mcux_add_iar_configuration(
-  # CC "--use_cmsis_dsp\
-  "--diag_suppress=Pe167\
+  CC "--diag_suppress=Pe167\
   --diag_suppress=Pe260,Pe1031"
   )
 mcux_remove_iar_configuration(
