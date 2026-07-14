0a8d2ddb370bbb0080e534715d646060cc723be3
Andrei Catalin Ilie <andreicatalin.ilie@nxp.com>
2026-07-14 18:09:18 +0300

    [MCUX-83882] examples: add frdmimxrt700 cube_freertos
    Add the FRDM RT700 board overlay for cube_freertos.
    Port the RT700 EVK board files, add project-local pin
    mux files, and adapt the board init and project config
    for the FRDM bring-up path. This enables the FRDM RT700
    cube_freertos example to build with armgcc, IAR, and
    MDK.

---
+++ _boards/frdmimxrt700/vglite_examples/cube_freertos/cm33_core0/hardware_init.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2026 NXP
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+/*${header:start}*/
+#include "pin_mux.h"
+#include "fsl_gpio.h"
+#include "app.h"
+#include "clock_config.h"
+#include "board.h"
+#include "display_support.h"
+#include "pmic_support.h"
+/*${header:end}*/
+
+/*${function:start}*/
+void BOARD_InitHardware(void)
+{
+    BOARD_ConfigMPU();
+    BOARD_InitAHBSC();
+    BOARD_InitBootPins();
+    BOARD_InitPsRamPins_Xspi2();
+
+    /* Disable LDO */
+    POWER_SetVddnSupplySrc(kVddSrc_PMIC);
+    POWER_SetVdd1SupplySrc(kVddSrc_PMIC);
+    POWER_SetVdd2SupplySrc(kVddSrc_PMIC);
+    POWER_ApplyPD();
+
+    BOARD_InitPmicPins();
+    BOARD_InitPmic();
+    BOARD_SetPmicVdd2Voltage(1100000U); /* 1.1v for 325MHz clock. */
+
+    BOARD_BootClockHSRUN();
+    BOARD_InitDebugConsole();
+    BOARD_Init16bitsPsRam(XSPI2);
+
+#if (DEMO_PANEL_RASPI_7INCH == DEMO_PANEL)
+    BOARD_InitI2cPins();
+#endif
+}
+/*${function:end}*/
