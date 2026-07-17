75f8a727b90cd44ab940762746db9a9d7bee89c1
David Jurajda <david.jurajda@nxp.com>
2026-07-17 16:09:16 +0200

    [MCUX-89248] Enable littlefs_shell for frdmimxrt1152
    Signed-off-by: David Jurajda <david.jurajda@nxp.com>

--- _boards/frdmimxrt1152/littlefs_examples/littlefs_shell/reconfig.cmake
+++ _boards/frdmimxrt1152/littlefs_examples/littlefs_shell/reconfig.cmake
@@ -8,8 +8,59 @@ mcux_add_macro(
     CC "-DLFS_NO_INTRINSICS=1\
        -DSDK_DEBUGCONSOLE_UART"
 )
+
 mcux_add_macro(
     TOOLCHAINS armgcc iar
-    TARGETS hyperram_release release
+    TARGETS hyperram_release
+            release
+            flexspi_nor_release
     CC "-DLFS_NO_ASSERT"
+)
+
+mcux_add_macro(
+    TOOLCHAINS iar
+    CC "-DFSL_DRIVER_TRANSFER_DOUBLE_WEAK_IRQ=0"
+)
+
+mcux_remove_macro(
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    AS "-D__STARTUP_INITIALIZE_RAMFUNCTION"
+)
+
+# Add or remove Linker File Configurations
+mcux_remove_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${device_root}/RT/RT1150/MIMXRT1152/iar/MIMXRT1152xxxxx_flexspi_nor.icf
+)
+
+mcux_remove_mdk_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${device_root}/RT/RT1150/MIMXRT1152/arm/MIMXRT1152xxxxx_flexspi_nor.scf
+)
+
+mcux_remove_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${device_root}/RT/RT1150/MIMXRT1152/gcc/MIMXRT1152xxxxx_flexspi_nor.ld
+)
+
+# Add or remove Linker File Configurations
+mcux_add_iar_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${board_root}/${board}/littlefs_examples/littlefs_shell/linker/MIMXRT1152xxxxx_flexspi_nor.icf
+)
+
+mcux_add_mdk_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${board_root}/${board}/littlefs_examples/littlefs_shell/linker/MIMXRT1152xxxxx_flexspi_nor.scf
+)
+
+mcux_add_armgcc_linker_script(
+    BASE_PATH ${SdkRootDirPath}
+    TARGETS flexspi_nor_debug flexspi_nor_release
+    LINKER ${board_root}/${board}/littlefs_examples/littlefs_shell/linker/MIMXRT1152xxxxx_flexspi_nor.ld
 )
